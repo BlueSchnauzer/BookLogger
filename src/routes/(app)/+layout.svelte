ed1f02e009d246d9a6ac6db4b55aac08b64f690e
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-12 00:39:46 +0900

    add each page files temporaly

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { LayoutData } from '../$types';
+    
+    export let data: LayoutData;
+</script>
+
+<nav>
+    <a href="/dashboard">ダッシュボード</a>
+    <a href="/books">登録した本</a>
+    <a href="/books/wish">読みたい本</a>
+    <a href="/books/reading">読んでいる本</a>
+    <a href="/books/complete">読み終わった本</a>
+</nav>
+
+<slot></slot>
