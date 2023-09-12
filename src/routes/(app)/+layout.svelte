cf03829835f28cac7e914db0cade4af80b752dbd
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-12 23:02:10 +0900

    Change layout by using each statement

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,14 +2,34 @@
     import type { LayoutData } from '../$types';
     
     export let data: LayoutData;
+
+    interface listItem {
+        ref: string,
+        name: string
+    }
+
+    const listItems: listItem[] = [
+        { ref: "/dashboard", name: "ダッシュボード" },
+        { ref: "/books", name: "登録した本" },
+        { ref: "/books/wish", name: "読みたい本"},
+        { ref: "/books/reading", name: "読んでいる本"},
+        { ref: "/books/complete", name: "読み終わった本"},
+        { ref: "/shelfs", name: "本棚"}
+    ];
+
 </script>
 
 <nav>
-    <a href="/dashboard">ダッシュボード</a>
-    <a href="/books">登録した本</a>
-    <a href="/books/wish">読みたい本</a>
-    <a href="/books/reading">読んでいる本</a>
-    <a href="/books/complete">読み終わった本</a>
+    <div>
+        <p>アプリ名</p>
+    </div>
+    <ul>
+        {#each listItems as item}
+            <li>
+                <a href="{item.ref}">{item.name}</a>
+            </li>
+        {/each}
+    </ul>
 </nav>
 
 <slot></slot>
