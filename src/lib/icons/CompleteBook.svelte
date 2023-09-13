144176f11a7ccfbba8da8990f775e15e058f30b1
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-13 22:52:51 +0900

    Add some svg components

---
+++ src/lib/icons/CompleteBook.svelte
@@ -0,0 +1,28 @@
+<svg
+	id="Layer_1"
+	data-name="Layer 1"
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	width="24"
+	height="24"
+	color="#000000"
+	><defs
+		><style>
+			.cls-637b83faf95e86b59c57a0d6-1 {
+				fill: none;
+				stroke: currentColor;
+				stroke-miterlimit: 10;
+			}
+		</style></defs
+	><path
+		class="cls-637b83faf95e86b59c57a0d6-1"
+		d="M20.59,22.5H5.32A1.91,1.91,0,0,1,4,19.24a1.89,1.89,0,0,1,1.35-.56H20.59"
+	/><line class="cls-637b83faf95e86b59c57a0d6-1" x1="19.64" y1="18.68" x2="19.64" y2="22.5" /><path
+		class="cls-637b83faf95e86b59c57a0d6-1"
+		d="M20.59,1.5V18.68H5.32a1.91,1.91,0,0,0-1.91,1.91V3.41A1.92,1.92,0,0,1,5.32,1.5Z"
+	/><polygon
+		class="cls-637b83faf95e86b59c57a0d6-1"
+		points="12 13.91 10.07 12.96 8.18 13.91 8.18 1.5 12 1.5 12 13.91"
+	/></svg
+>
---
+++ src/lib/icons/Eraser.svelte
@@ -0,0 +1,29 @@
+<svg
+	id="Layer_1"
+	data-name="Layer 1"
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	width="24"
+	height="24"
+	color="#000000"
+	><defs
+		><style>
+			.cls-637b83faf95e86b59c57a0de-1 {
+				fill: none;
+				stroke: currentColor;
+				stroke-miterlimit: 10;
+			}
+		</style></defs
+	><path
+		class="cls-637b83faf95e86b59c57a0de-1"
+		d="M13.59,19l-2.82,2.83a2.33,2.33,0,0,1-1.63.67h0a2.31,2.31,0,0,1-1.63-.67L2.17,16.49a2.31,2.31,0,0,1-.67-1.63h0a2.33,2.33,0,0,1,.67-1.63L5,10.41Z"
+	/><rect
+		class="cls-637b83faf95e86b59c57a0de-1"
+		x="7.45"
+		y="4.17"
+		width="12.59"
+		height="12.15"
+		transform="translate(-3.22 12.73) rotate(-45)"
+	/></svg
+>
