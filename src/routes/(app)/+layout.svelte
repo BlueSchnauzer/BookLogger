0ee0a52dec1b3352fbea65bfd246e6d9265b3a29
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-13 22:53:23 +0900

    Use svg components in side menu

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
 	import type { LayoutData } from '../$types';
+	import type { ComponentType } from 'svelte';
     import Hamburger from '$lib/icons/Hamburger.svelte';
+    import Dashboard from '$lib/icons/Dashboard.svelte';
+    import AllBooks from '$lib/icons/AllBooks.svelte';
+    import PileOfBooks from '$lib/icons/PileOfBooks.svelte';
+    import Openingbook from '$lib/icons/OpeningBook.svelte';
+    import CompleteBook from '$lib/icons/CompleteBook.svelte';
+    import Shelf from '$lib/icons/Shelf.svelte';
 
 	//export let data: LayoutData;
 
 	interface listItem {
-		ref: string;
-		name: string;
+        icon: ComponentType,
+		ref: string,
+		name: string
 	}
 
 	const listItems: listItem[] = [
-		{ ref: '/dashboard', name: 'ダッシュボード' },
-		{ ref: '/books', name: '登録した本' },
-		{ ref: '/books/wish', name: '読みたい本' },
-		{ ref: '/books/reading', name: '読んでいる本' },
-		{ ref: '/books/complete', name: '読み終わった本' },
-		{ ref: '/shelfs', name: '本棚' }
+		{ icon: Dashboard, ref: '/dashboard', name: 'ダッシュボード' },
+		{ icon: AllBooks, ref: '/books', name: '登録した本' },
+		{ icon: PileOfBooks, ref: '/books/wish', name: '読みたい本' },
+		{ icon: Openingbook, ref: '/books/reading', name: '読んでいる本' },
+		{ icon: CompleteBook, ref: '/books/complete', name: '読み終わった本' },
+		{ icon: Shelf, ref: '/shelfs', name: '本棚' }
 	];
 </script>
 
@@ -31,13 +39,14 @@
         </div>
     </header>    
     <div class="flex">
-        <nav class=" bg-lime-200">
+        <nav class="fixed w-[250px] bg-teal-200 overflow-y-auto">
             <div>
                 <p>アプリ名</p>
             </div>
             <ul>
                 {#each listItems as item}
                     <li>
+                        <svelte:component this={item.icon}/>
                         <a href={item.ref}>{item.name}</a>
                     </li>
                 {/each}
