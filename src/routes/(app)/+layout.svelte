dbc3781c124865a768f54f25e49cf3ee02f16801
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-14 02:02:01 +0900

    Separate list contents in sidemenu into component

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
 	import type { LayoutData } from '../$types';
-	import type { ComponentType } from 'svelte';
-    import Hamburger from '$lib/icons/Hamburger.svelte';
-    import LeanBooks from '$lib/icons/LeanBooks.svelte';
-    import Dashboard from '$lib/icons/Dashboard.svelte';
-    import AllBooks from '$lib/icons/AllBooks.svelte';
-    import PileOfBooks from '$lib/icons/PileOfBooks.svelte';
-    import Openingbook from '$lib/icons/OpeningBook.svelte';
-    import CompleteBook from '$lib/icons/CompleteBook.svelte';
-    import Shelf from '$lib/icons/Shelf.svelte';
-    import ArrowDown from '$lib/icons/ArrowDown.svelte';
+	import ListItem from './ListItem.svelte';
+	import type { ListItemData } from '$lib/customTypes';
+	import Hamburger from '$lib/icons/Hamburger.svelte';
+	import LeanBooks from '$lib/icons/LeanBooks.svelte';
+	import Dashboard from '$lib/icons/Dashboard.svelte';
+	import AllBooks from '$lib/icons/AllBooks.svelte';
+	import PileOfBooks from '$lib/icons/PileOfBooks.svelte';
+	import Openingbook from '$lib/icons/OpeningBook.svelte';
+	import CompleteBook from '$lib/icons/CompleteBook.svelte';
+	import Shelf from '$lib/icons/Shelf.svelte';
 
 	//export let data: LayoutData;
-
-	interface ListItemData {
-        icon: ComponentType,
-		ref: string,
-		name: string
-	}
 
 	const listItemDatas: ListItemData[] = [
 		{ icon: Dashboard, ref: '/dashboard', name: 'ダッシュボード' },
@@ -30,49 +24,31 @@
 </script>
 
 <div class="flex flex-col w-screen h-screen">
-    <header class="hidden max-md:block w-full bg-orange-200">
-        <div class="flex justify-between items-center max-w-5xl mx-auto">
-            <button class="ml-5">
-                <LeanBooks/>
-            </button>
-            <button class="mr-5">
-                <Hamburger/>
-            </button>    
-        </div>
-    </header>    
-    <div class="flex w-full h-full">
-        <nav class=" w-[250px] bg-teal-500 overflow-y-auto">
-            <div class="flex p-3">
-                <LeanBooks/>
-                <p class="ml-2.5 text-xl text-white">BookLogger</p>
-            </div>
-            <div class="mx-3 my-2 bg-white h-[1px]"></div>
-            <ul>
-                {#each listItemDatas as data (data.name)}
-                    {#if data.icon === Shelf}
-                        <div class="mx-3 my-0 bg-white h-[1px]"></div>
-                        <li class="p-2">
-                            <a href={data.ref} class="p-2.5 flex items-center rounded-md duration-200 hover:bg-teal-800">
-                                <svelte:component this={data.icon}/>
-                                <div class="ml-2.5 flex flex-1 justify-between">
-                                    <span class=" text-white">{data.name}</span>
-                                    <ArrowDown/>
-                                </div>
-                            </a>
-                        </li>
-                    {:else}
-                        <li class="p-2">
-                            <a href={data.ref} class="p-2.5 flex items-center rounded-md duration-200 hover:bg-teal-800">
-                                <svelte:component this={data.icon}/>
-                                <span class="ml-2.5 text-white">{data.name}</span>
-                            </a>
-                        </li>
-                    {/if}
-                {/each}
-            </ul>
-        </nav>
-        <div class="flex-1">
-            <slot />
-        </div>    
-    </div>
+	<header class="hidden max-md:block w-full bg-orange-200">
+		<div class="flex justify-between items-center max-w-5xl mx-auto">
+			<button class="ml-5">
+				<LeanBooks />
+			</button>
+			<button class="mr-5">
+				<Hamburger />
+			</button>
+		</div>
+	</header>
+	<div class="flex w-full h-full">
+		<nav class=" w-[250px] bg-teal-500 overflow-y-auto">
+			<div class="flex p-3">
+				<LeanBooks />
+				<p class="ml-2.5 text-xl text-white">BookLogger</p>
+			</div>
+			<div class="mx-3 my-2 bg-white h-[1px]" />
+			<ul>
+				{#each listItemDatas as data (data.name)}
+					<ListItem listItemData={data} />
+				{/each}
+			</ul>
+		</nav>
+		<div class="flex-1">
+			<slot />
+		</div>
+	</div>
 </div>
---
+++ src/routes/(app)/ListItem.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import ArrowDown from '$lib/icons/ArrowDown.svelte';
+	import Shelf from '$lib/icons/Shelf.svelte';
+	import type { ListItemData } from '$lib/customTypes';
+
+	export let listItemData: ListItemData;
+</script>
+
+{#if listItemData.icon === Shelf}
+	<div class="mx-3 my-0 bg-white h-[1px]" />
+{/if}
+
+<li class="p-2">
+	<a
+		href={listItemData.ref}
+		class="p-2.5 flex items-center rounded-md duration-200 hover:bg-teal-800"
+	>
+		<svelte:component this={listItemData.icon} />
+		{#if listItemData.icon === Shelf}
+			<div class="ml-2.5 flex flex-1 justify-between">
+				<span class=" text-white">{listItemData.name}</span>
+				<ArrowDown />
+			</div>
+		{:else}
+			<span class="ml-2.5 text-white">{listItemData.name}</span>
+		{/if}
+	</a>
+</li>
