086b7f6f946dd1cac49a82d5d752662edb6f93a2
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-21 01:55:44 +0900

    Create BookInfo Grid tags and Dummy data

---
+++ src/routes/(app)/books/+layout.server.ts
@@ -0,0 +1,193 @@
+import type { LayoutServerLoad } from './$types';
+import type { BookInfo } from '$lib/customTypes';
+
+//保存した書籍の情報を取得する(APIへのリクエストはフロントで実行)
+export const load = (async () => {
+    //DBへアクセスして、ユーザーの書誌情報を取得する。
+    const bookInfos : BookInfo[] = [
+        {
+            isbn_13: '978-4-15-031316-6',
+            title: 'エピローグ',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: false,
+            memorandum: 'メモです1'
+        },
+        {
+            isbn_13: '978-4-16-791019-8',
+            title: 'プロローグ',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです2'
+        },
+        {
+            isbn_13: '978-4-10-240171-2',
+            title: 'クイーンズ・ギャンビット',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 100
+            }],
+            isCompleted: false,
+            memorandum: 'メモです3'
+        },
+        {
+            isbn_13: '978-4-15-120051-9',
+            title: 'わたしを離さないで',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです4'
+        },
+        {
+            isbn_13: '978-4-15-010224-1',
+            title: '地球の長い午後',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 100
+            }],
+            isCompleted: false,
+            memorandum: 'メモです5'
+        },
+        {
+            isbn_13: '978-4-06-274936-7',
+            title: '煙か土か食い物',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです6'
+        },
+        {
+            isbn_13: '',
+            title: 'ダミー',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです6'
+        },
+        {
+            isbn_13: '',
+            title: 'ダミー',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです6'
+        },
+        {
+            isbn_13: '',
+            title: 'ダミー',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです6'
+        },
+        {
+            isbn_13: '',
+            title: 'ダミー',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです6'
+        },
+        {
+            isbn_13: '',
+            title: 'ダミー',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです6'
+        },
+        {
+            isbn_13: '',
+            title: 'ダミー',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです6'
+        },
+        {
+            isbn_13: '',
+            title: 'ダミー',
+            imageUrl: '',
+            registrationDate: new Date,
+            updateDate: new Date,
+            pageCount: -1,
+            history: [{
+                date: new Date,
+                currentPage: 0
+            }],
+            isCompleted: true,
+            memorandum: 'メモです6'
+        }
+    ];
+
+    return { BookInfos: bookInfos};
+}) satisfies LayoutServerLoad;
--- src/routes/(app)/books/+page.svelte
+++ src/routes/(app)/books/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
     import type { PageData } from './$types';
     
-    //export let data: PageData;
+    export let data: PageData;
+
+    console.log('page');
+    console.log(data);
+
 </script>
 
-<h1>全ての本</h1>
+<ul class="grid gap-2 grid-cols-BookContentAutoFill max-sm:grid-cols-smBookContentAutoFit max-sm:place-items-center">
+    {#each data.BookInfos as bookInfo}
+        <li class="grid max-sm:w-[128px] max-sm:h-[182px] bg-gray-100 rounded shadow-xl">
+            <div class="justify-self-center self-center w-[128px] h-[182px] bg-slate-300">No Image</div>
+            <span class="max-sm:hidden ">{bookInfo.title}</span>
+            <span class="justify-self-start max-sm:hidden">登録日</span>
+            <span class="justify-self-end max-sm:hidden">2023年9月20日</span>
+        </li>        
+    {/each}
+</ul>
