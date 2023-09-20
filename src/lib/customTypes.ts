535af0186b497a443465fd82554481358f7951a3
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-20 22:28:50 +0900

    Add BookInfo ts type

--- src/lib/customTypes.ts
+++ src/lib/customTypes.ts
@@ -1,8 +1,24 @@
 import type { ComponentType } from "svelte"
 
-export interface MenuItemData {
+export type MenuItemData = {
     icon: ComponentType,
     ref: string,
     jpName: string,
     enName: string
+}
+
+export type BookInfo = {
+    isbn_13: string,
+    isbn_10?: string,
+    title: string,
+    imageUrl: string,
+    registrationDate: Date,
+    updateDate: Date,
+    pageCount: number,
+    history: [{
+        date: Date,
+        currentPage: number
+    }],
+    isCompleted: boolean,
+    memorandum: string
 }
