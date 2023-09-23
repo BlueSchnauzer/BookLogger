04ba9c191fd4c946d781183f0ef5948a8cfc8bbd
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-23 20:43:13 +0900

    Fix toggle switch focus color to prevent mixing focus and checked color

--- src/lib/parts/ToggleSwitch.svelte
+++ src/lib/parts/ToggleSwitch.svelte
@@ -6,8 +6,8 @@
 
 <div class="flex">
     <input type="checkbox" id="toggle-{id}" class="peer opacity-0 absolute left-0" checked={isChecked}>
-    <label for="toggle-{id}" class="h-8 px-2.5 py-1 mx-0.5 flex items-center text-stone-700 bg-stone-300 border border-stone-700 rounded-full 
-        duration-150 hover:bg-stone-200 peer-focus:bg-stone-200 peer-checked:text-stone-200 peer-checked:bg-stone-700"
+    <label for="toggle-{id}" class="h-8 px-2.5 py-1 mx-0.5 whitespace-nowrap flex items-center text-stone-700 bg-stone-300 border border-stone-700 rounded-full 
+        duration-150 hover:bg-stone-200 peer-focus:bg-stone-200 peer-checked:text-stone-200 peer-focus:peer-checked:bg-stone-700 peer-checked:bg-stone-700"
     >
         {text}
     </label>
