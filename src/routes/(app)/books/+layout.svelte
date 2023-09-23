e1126fcc0c8cf29e59cb1851dc49c8a8804fc1e9
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-23 18:46:59 +0900

    Make space on book info grid

--- src/routes/(app)/books/+layout.svelte
+++ src/routes/(app)/books/+layout.svelte
@@ -23,7 +23,7 @@
 <main class="flex-1 my-2 max-md:pb-16 flexWidth">
     <slot />
     <div class="mx-2 my-1 bg-stone-400 h-[1px]" />
-    <div id="mainContent" class="contentHeight">
+    <div id="mainContent" class="p-1 contentHeight">
         <ul class="grid gap-2 grid-cols-BookContentAutoFill max-sm:grid-cols-smBookContentAutoFit max-sm:place-items-center">
             {#each data.BookInfos as bookInfo}
                 <li class="grid h-80 max-sm:w-[128px] max-sm:h-[182px] bg-gray-100 rounded shadow-md "
