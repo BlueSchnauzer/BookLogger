a48ae75eaa0357d23d751f824542c1d002609a6b
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-26 01:24:42 +0900

    Create cover component

---
+++ src/lib/parts/FullCoverZindex10.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let isDisplay = false;
+	export let isHiddenByOnclick = false;
+	export let isUseBackGroundColor = false;
+
+	const changeDisplay = () => {
+		if (isHiddenByOnclick) {
+			isDisplay = !isDisplay;
+		}
+	};
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+	class="z-10 absolute top-0 left-0 w-full h-full opacity-30
+    {isDisplay ? '' : 'hidden'} {isUseBackGroundColor ? 'bg-black' : ''}"
+	on:click={changeDisplay}
+/>
