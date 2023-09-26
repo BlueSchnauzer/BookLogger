3d4f1cdd0bc7860c03864754d2ce544864ae8821
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-27 01:49:12 +0900

    Format custom types

--- src/lib/customTypes.ts
+++ src/lib/customTypes.ts
@@ -1,37 +1,44 @@
-import type { ComponentType } from "svelte"
+import type { ComponentType } from 'svelte';
 
 /**メニューの項目データ */
 export type menuItemData = {
-    icon: ComponentType,
-    ref: string,
-    jpName: string,
-    enName: string
-}
+	icon: ComponentType;
+	ref: string;
+	jpName: string;
+	enName: string;
+};
 
 /**ラベルフィルター(トグル) */
 export type toggleFilterItem = {
-    id: number,
-    text: string,
-    type: 'favorite'|'status',
-    isChecked: boolean,
-    isVisible: boolean
-}
+	id: number;
+	text: string;
+	type: 'favorite' | 'status';
+	isChecked: boolean;
+	isVisible: boolean;
+};
+
+export type selectFilterItem = { 
+    id: number;
+    text: string; 
+};
 
 /**書誌情報 */
 export type bookInfo = {
-    id: number,
-    isVisible: boolean,
-    isbn_13: string,
-    isbn_10?: string,
-    title: string,
-    imageUrl: string,
-    registrationDate: Date,
-    updateDate: Date,
-    pageCount: number,
-    history: [{
-        date: Date,
-        currentPage: number
-    }],
-    isCompleted: boolean,
-    memorandum: string
-}
+	id: number;
+	isVisible: boolean;
+	isbn_13: string;
+	isbn_10?: string;
+	title: string;
+	imageUrl: string;
+	registrationDate: Date;
+	updateDate: Date;
+	pageCount: number;
+	history: [
+		{
+			date: Date;
+			currentPage: number;
+		}
+	];
+	isCompleted: boolean;
+	memorandum: string;
+};
