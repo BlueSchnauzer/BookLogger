f45cb19b94727da38658a6c5e9ee3faf2a89ac8b
BlueSchnauzer <kbrucke08@gmail.com>
2023-09-28 01:05:57 +0900

    Create bookinfo schema and model

---
+++ src/lib/server/models/BookInfo.ts
@@ -0,0 +1,50 @@
+import { ObjectId } from 'mongodb';
+import { Schema, model } from 'mongoose';
+
+/**書誌情報 */
+export interface IBookInfo {
+	//userId: ObjectId; //User用のスキーマが完成した際に入れ替える。
+	userId: number;
+	isbn_13: string;
+	title: string;
+	imageUrl: string;
+	createDate: Date;
+	updateDate: Date;
+	pageCount: number;
+	history: [
+		{
+			date: Date;
+			currentPage: number;
+		}
+	];
+	isCompleted: boolean;
+    isFavorite: boolean;
+	memorandum: string;
+	isVisible: boolean; //DBでは管理しない
+	isbn_10?: string;
+}
+
+const bookInfoSchema = new Schema<IBookInfo>({
+	//userId: { type: ObjectId, required: true, ref: User }, //User用のモデルが完成した際に入れ替える。
+	userId: {type: Number, required: true},
+	isbn_13: { type: String, required: true },
+	title: { type: String, required: true },
+	imageUrl: { type: String, default: '' },
+	createDate: { type: Date, required: true },
+	updateDate: { type: Date, required: true },
+	pageCount: { type: Number, default: -1 },
+	history: [
+		{
+			required: false,
+			date: { type: Date, required: true },
+			currentPage: { type: Number, required: true }
+		}
+	],
+	isCompleted: { type: Boolean, required: true },
+    isFavorite: { type: Boolean, required: true, default: false},
+	memorandum: { type: String, default: '' },
+	isbn_10: { type: String, default: '' }
+});
+
+/**書誌情報のモデル */
+export const BookInfo = model<IBookInfo>('BookInfo', bookInfoSchema);
