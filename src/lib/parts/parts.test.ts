9e6d125773802280388e8cbc15a658cd4d03a592
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-05 20:57:56 +0900

    Add props change event test code

--- src/lib/parts/parts.test.ts
+++ src/lib/parts/parts.test.ts
@@ -1,4 +1,4 @@
-import {render, fireEvent, screen} from '@testing-library/svelte';
+import { render, fireEvent, screen } from '@testing-library/svelte';
 import { describe, expect, it } from 'vitest';
 import type * as customTypes from '$lib/customTypes';
 import ToggleSwitch from '$lib/parts/ToggleSwitch.svelte';
@@ -11,18 +11,6 @@ describe('ToggleSwitch', () => {
 	];
 
 	it('レンダリング', () => {
-		const {container} = render(ToggleSwitch, {
-			id: toggleFilterItems[0].id,
-			text: toggleFilterItems[0].text,
-			isChecked: toggleFilterItems[0].isChecked,
-			isVisible: toggleFilterItems[0].isVisible
-		});
-
-        expect(container.querySelector('input')).toBeInTheDocument();
-        expect(screen.getByText(toggleFilterItems[0].text)).toBeInTheDocument();
-	});
-
-    it('クリック時にinputタグがcheckedになること', () => {
 		const { container } = render(ToggleSwitch, {
 			id: toggleFilterItems[0].id,
 			text: toggleFilterItems[0].text,
@@ -30,20 +18,41 @@ describe('ToggleSwitch', () => {
 			isVisible: toggleFilterItems[0].isVisible
 		});
 
-        fireEvent.click(screen.getByText(toggleFilterItems[0].text));
+		expect(container.querySelector('input')).toBeInTheDocument();
+		expect(screen.getByText(toggleFilterItems[0].text)).toBeInTheDocument();
+	});
 
-        expect(container.querySelector('input')).toBeChecked();
-    });
+	it('クリック時にinputタグがcheckedになること', async () => {
+		const { container } = render(ToggleSwitch, {
+			id: toggleFilterItems[0].id,
+			text: toggleFilterItems[0].text,
+			isChecked: toggleFilterItems[0].isChecked,
+			isVisible: toggleFilterItems[0].isVisible
+		});
 
-    it('Props変更時にタグが非表示になること', () => {
-        toggleFilterItems[0].isVisible = false;
-        const { container } = render(ToggleSwitch, {
+		await fireEvent.click(screen.getByText(toggleFilterItems[0].text));
+
+		expect(container.querySelector('input')).toBeChecked();
+	});
+
+	it('Props変更時にタグが非表示になること', async () => {
+		const { container, component } = render(ToggleSwitch, {
 			id: toggleFilterItems[0].id,
 			text: toggleFilterItems[0].text,
 			isChecked: toggleFilterItems[0].isChecked,
 			isVisible: toggleFilterItems[0].isVisible
 		})
 
-        expect(container.querySelector('div.hidden')).toBeInTheDocument();
-    });
+		expect(container.querySelector('div.flex')).toBeInTheDocument();
+
+		toggleFilterItems[0].isVisible = false;
+		await component.$set({
+			id: toggleFilterItems[0].id,
+			text: toggleFilterItems[0].text,
+			isChecked: toggleFilterItems[0].isChecked,
+			isVisible: toggleFilterItems[0].isVisible
+		});
+
+		expect(container.querySelector('div.hidden')).toBeInTheDocument();
+	});
 });
