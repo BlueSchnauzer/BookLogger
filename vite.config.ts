08e9e40b8a5591848e1edd0dbafdc1478864fc39
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-05 20:57:06 +0900

    Fix regx error in vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test}.{js,ts}'],
+		include: ['src/**/*.test.{js,ts}'],
 		setupFiles: './src/lib/vitest-setup.ts',
 		globals: true,
 		environment: 'jsdom'
