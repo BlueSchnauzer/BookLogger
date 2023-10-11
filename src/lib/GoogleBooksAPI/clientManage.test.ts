4f2510142c894f749bf8832320479c445e954357
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-12 01:35:08 +0900

    Add clientManage test

---
+++ src/lib/GoogleBooksAPI/clientManage.test.ts
@@ -0,0 +1,79 @@
+import { describe, it, expect } from "vitest";
+import { ClientBooksGAPI } from "./clientManage";
+import type { IBookInfo } from "$lib/server/models/BookInfo";
+import { ObjectId } from "mongodb";
+
+
+describe('requestBookInfo', () => {
+  const booksGapi = new ClientBooksGAPI();
+  const bookInfo: IBookInfo = {
+    _id: new ObjectId('651451ed67241f439ce8a1af'),
+    userId: 1,
+    isVisible: true,
+    isbn_13: '978-4-15-120051-9',
+    title: 'わたしを離さないで',
+    imageUrl: '',
+    createDate: new Date,
+    updateDate: new Date,
+    pageCount: -1,
+    history: [{
+        date: new Date,
+        currentPage: 0
+    }],
+    isFavorite: false,
+    isCompleted: false,
+    memorandum: 'メモです1'
+  }
+
+  it('ISBNを条件にして一致した書誌データを取得できるか', async () => {
+    const result = await booksGapi.requestBookInfo(`isbn:${bookInfo.isbn_13}`);
+
+    expect(result.items).toBeDefined();
+    expect(result.items![0].volumeInfo?.title).toEqual(bookInfo.title);
+  });
+  
+  it('タイトルを条件にして一致した書誌データを取得できるか',async () => {
+    const result = await booksGapi.requestBookInfo(`intitle:${bookInfo.title}`);
+
+    //タイトル指定は複数取れるので、一致させずに1件でもあればOK
+    expect(result.items).toBeDefined();
+  });
+
+  it('著者名を条件にして一致した書誌データを取得できるか', async () => {
+    const result = await booksGapi.requestBookInfo('inauthor:イシグロカズオ');
+
+    //著者指定は複数取れるので、一致させずに1件でもあればOK
+    expect(result.items).toBeDefined();
+  });
+
+});
+
+describe('setBookInfoByISBN', () => {
+  const booksGapi = new ClientBooksGAPI();
+  const bookInfo: IBookInfo = {
+    _id: new ObjectId('651451ed67241f439ce8a1af'),
+    userId: 1,
+    isVisible: true,
+    isbn_13: '978-4-15-120051-9',
+    title: 'わたしを離さないで',
+    imageUrl: '',
+    createDate: new Date,
+    updateDate: new Date,
+    pageCount: -1,
+    history: [{
+        date: new Date,
+        currentPage: 0
+    }],
+    isFavorite: false,
+    isCompleted: false,
+    memorandum: 'メモです1'
+  }
+
+  it('bookInfoが持つISBNを条件にして書誌データの取得と、設定ができること', async () => {
+    await booksGapi.setBookInfoByISBN(bookInfo);
+
+    expect(bookInfo.imageUrl).toBeTruthy();
+    expect(bookInfo.pageCount).not.toBe(-1);
+  });
+
+});
