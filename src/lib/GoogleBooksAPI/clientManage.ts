bbfa5763afa57021ec88f2e9f2e0890414c497d6
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-11 20:55:59 +0900

    Remove gapi and changed to request by fetch API

--- src/lib/GoogleBooksAPI/clientManage.ts
+++ src/lib/GoogleBooksAPI/clientManage.ts
@@ -1,39 +1,29 @@
-import { PUBLIC_BOOKSAPI_DISCOVERY_URL, PUBLIC_BOOKSAPI_LIST } from "$env/static/public";
+import { PUBLIC_BOOKSAPI_LIST } from "$env/static/public";
 import type { IBookInfo } from "$lib/server/models/BookInfo";
 import type { books_v1 } from "googleapis";
 
-/**クライアント側GoogleBooksAPIのリクエスト用データを取得する */
-export async function preparingGapi() {
-    //JavaScript用のGoogleAPIライブラリをロードする
-    await gapi.load('client', () => {});
-    //GoogleBooksAPI v1を使用する
-    await gapi.client.init({'discoveryDocs': [PUBLIC_BOOKSAPI_DISCOVERY_URL]});
-}
-
+/**指定した検索条件でGoogleBooksAPIにリクエストする */
 async function requestBookInfo(query: string): Promise<books_v1.Schema$Volumes> {
-    const response = await gapi.client.request({
-        path: PUBLIC_BOOKSAPI_LIST,
-        params: {
-            q: query
-        }
-    });
-    // レスポンスのボディをbooks_v1.Schema$Volumes型として返す
-    return response.result as books_v1.Schema$Volumes;
+  const response = await fetch(`${PUBLIC_BOOKSAPI_LIST}?q=${query}`);
+  const volumes: books_v1.Schema$Volumes = await response.json();
+  
+  return volumes;
 }
 
 /**GoogleBooksAPIに書影をリクエストする*/
 export async function getBookInfo(bookInfo: IBookInfo): Promise<string> {
-    if (bookInfo.imageUrl) { return bookInfo.imageUrl; }
-    if (!bookInfo.isbn_13) { throw new Error('ISBN is empty'); }
+  if (bookInfo.imageUrl) { return bookInfo.imageUrl; }
+  if (!bookInfo.isbn_13) { throw new Error('ISBN is empty'); }
 
-    const result = await requestBookInfo(`isbn:${bookInfo.isbn_13}`);
-    if (result.items?.length === 0 || !result.items) { throw new Error('This books image was not found in GoogleBooksAPI'); }
+  //isbnで検索する
+  const result = await requestBookInfo(`isbn:${bookInfo.isbn_13}`);
+  if (result.items?.length === 0 || !result.items) { throw new Error('This books image was not found in GoogleBooksAPI'); }
 
-    const volumeInfo = result.items[0].volumeInfo;
-    bookInfo.imageUrl = volumeInfo?.imageLinks?.thumbnail!;
-    bookInfo.pageCount = volumeInfo?.pageCount!;
+  const volumeInfo = result.items[0].volumeInfo;
+  bookInfo.imageUrl = volumeInfo?.imageLinks?.thumbnail!;
+  bookInfo.pageCount = volumeInfo?.pageCount!;
 
-    if (!bookInfo.imageUrl) { throw new Error('This books image was not found in GoogleBooksAPI'); }
+  if (!bookInfo.imageUrl) { throw new Error('This books image was not found in GoogleBooksAPI'); }
 
-    return bookInfo.imageUrl;
+  return bookInfo.imageUrl;
 }
--- src/routes/(app)/books/+layout.svelte
+++ src/routes/(app)/books/+layout.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
     import type { LayoutData } from './$types';    
-    import { preparingGapi } from '$lib/GoogleBooksAPI/clientManage';
-    export let data: LayoutData;
+    //export let data: LayoutData;
 
-    //data.preparingGapi();
-
-    onMount(async () => {
-        await preparingGapi();
-    });
 </script>
-
-<svelte:head>
-	<script src="https://apis.google.com/js/api.js"></script>
-</svelte:head>
 
 <main class="flex-1 my-2 max-md:pb-16 flexWidth">
     <!-- 各ページでレンダリング -->
