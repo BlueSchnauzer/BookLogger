c5714af1602a67b44a230e2368575a50309a52cc
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-14 19:27:25 +0900

    Change opacity property and add slot

--- src/lib/components/parts/FullCoverZindex10.svelte
+++ src/lib/components/parts/FullCoverZindex10.svelte
@@ -13,7 +13,9 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-	class="z-10 absolute top-0 left-0 w-full h-full opacity-30
+	class="z-10 absolute top-0 left-0 w-full h-full bg-opacity-30
     {isDisplay ? '' : 'hidden'} {isUseBackGroundColor ? 'bg-black' : ''}"
 	on:click={changeDisplay}
-/>
+>
+	<slot/>
+</div>
