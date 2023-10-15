6643f43eb5bd081894caf964dfad9a4e2102f5a1
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-15 23:46:33 +0900

    Add SearchModal render proccess

--- src/lib/components/app/ContentHeader.svelte
+++ src/lib/components/app/ContentHeader.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import type { ComponentType } from 'svelte';
+	import SearchModal from './SearchModal.svelte';
 
 	export let headerIcon: ComponentType;
 	export let headerText: string;
 	export let isDisplayAddButton: boolean = false;
 	const colorStone700 = '#44403C';
+
+	let isDisplayModal = false;
 </script>
 
 <div class="flex items-center justify-between">
@@ -13,10 +16,11 @@
 		<svelte:component this={headerIcon} width={30} height={30} />
 		<h1 class="text-xl pl-2">{headerText}</h1>
 	</div>
-	<button
+	<button on:click={() => isDisplayModal = !isDisplayModal}
 		class="w-10 h-10 flex items-center justify-center rounded-full hover:bg-stone-300
         {isDisplayAddButton ? '' : 'invisible'}"
 	>
 		<Icon icon="ph:plus" width="36" height="36" color={colorStone700} />
 	</button>
+	<SearchModal bind:isDisplay={isDisplayModal}/>
 </div>
