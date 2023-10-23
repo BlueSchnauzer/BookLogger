3cb30d6f2fd65f219a6ad7f236a8d146299c6e2b
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-23 19:11:07 +0900

    Adjust search result style and implement re-search process

--- src/routes/(app)/books/searchresult/+page.svelte
+++ src/routes/(app)/books/searchresult/+page.svelte
@@ -2,48 +2,58 @@
     import BookAdd from '$lib/icons/BookAdd.svelte';
 	import ContentHeader from '$lib/components/app/ContentHeader.svelte';
     import type { PageData } from './$types';
-	import { getBookInfosByQueries } from '$lib/GoogleBooksAPI/RequestManage';
-	import type { books_v1 } from 'googleapis';
+	import PrimalyButton from '$lib/components/parts/PrimalyButton.svelte';
+	import SearchModal from '$lib/components/app/SearchModal.svelte';
    
     export let data: PageData;
-    const promise = getBookInfosByQueries(data.bookTitle!, data.author!, data.isbn!) as books_v1.Schema$Volumes;
+    let isDisplaySearchModal = false;
 
 </script>
 
 <div class="pl-2 pr-3 pt-1.5 h-24 flex flex-col justify-between">
 	<ContentHeader headerIcon={BookAdd} headerText="書籍検索" isDisplayAddButton={false} />
-    <div class="py-2">
-        <span class="font-medium">検索結果</span>
-    </div>    
+    <div class="flex justify-end">
+        <PrimalyButton type='button' text='再検索' isUseMargin={false} on:click={() => isDisplaySearchModal = !isDisplaySearchModal}/>
+    </div>
+    <SearchModal bind:isDisplay={isDisplaySearchModal} action=''/>
 </div>
 <div class="mx-2 my-1 bg-stone-400 h-[1px] xl:block" />
 <div class="flex flex-col p-1 contentHeight overflow-auto customScroll">
-    {#await promise}
+    {#await data.getBookInfo()}
         <div class="flex flex-1 justify-center items-center">
             <span class="animate-spin w-14 h-14 border-4 border-lime-600 rounded-full border-t-transparent"></span>
         </div>
     {:then result} 
         {#if result.items}
+            <div class="py-2">
+                <span class="font-medium">検索結果</span>
+                <button>前へ</button>
+                <span>{result.totalItems}件</span>
+                <button>次へ</button>
+            </div>        
             <ul>
             {#each result.items as item}
                 <li class="p-2 my-2 flex bg-gray-100 rounded-lg shadow-md">
                     {#if item.volumeInfo?.imageLinks?.thumbnail}
                         <img
-                            class="w-[128px] h-[182px]"
+                            class="self-center w-[128px] h-[182px]"
                             src={item.volumeInfo?.imageLinks?.thumbnail}
                             alt="test"
                         />						
                     {:else}
-                        <div class="w-[128px] h-[182px] bg-slate-300">
+                        <div class="self-center flex justify-center items-center w-[128px] h-[182px] bg-slate-300">
                             <span>No Image</span>
                         </div>
                     {/if}
                     <div class="p-2 flex flex-col">
-                        <span>タイトル：{item.volumeInfo?.title}</span>
-                        <span>ページ数：{item.volumeInfo?.pageCount}</span>
-                        <span>発売日：{item.volumeInfo?.publishedDate}</span>
-                        <div class="p-2">
-                            <p class="collapseDescription">{item.volumeInfo?.description ?? ''}</p>
+                        <span class="font-bold text-lime-700">{item.volumeInfo?.title ?? 'データ無し'}</span>
+                        <div class="p-2 flex flex-col">
+                            <span>著者：{item.volumeInfo?.authors?.join(', ') ?? 'データ無し'}</span>
+                            <span>ページ数：{item.volumeInfo?.pageCount ?? 'データ無し'}</span>
+                            <span>発売日：{item.volumeInfo?.publishedDate ?? 'データ無し'}</span>
+                            <div class="p-2">
+                                <p class="collapseDescription">{item.volumeInfo?.description ?? ''}</p>
+                            </div>
                         </div>
                     </div>
                 </li>
@@ -54,6 +64,7 @@
         <span class="text-red-500 font-medium">{error.message}</span>
     {/await}
 </div>
+
 
 <style>
 	.contentHeight {
--- src/routes/(app)/books/searchresult/+page.ts
+++ src/routes/(app)/books/searchresult/+page.ts
@@ -1,9 +1,13 @@
 import type { PageLoad } from './$types';
+import { getBookInfosByQueries } from '$lib/GoogleBooksAPI/RequestManage';
+import type { books_v1 } from 'googleapis';
 
 export const load = (async (params) => {
     const bookTitle = params.url.searchParams.get('booktitle');
     const author = params.url.searchParams.get('author');
     const isbn = params.url.searchParams.get('isbn');
 
-    return { bookTitle, author, isbn };
+    //formで受け取った値で検索を行う関数を作成し、クライアント側に渡して実行
+    const getBookInfo = () => getBookInfosByQueries(bookTitle, author, isbn) as books_v1.Schema$Volumes;
+    return { getBookInfo };
 }) satisfies PageLoad;
