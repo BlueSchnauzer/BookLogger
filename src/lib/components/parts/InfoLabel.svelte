158989a65b13d4d619c3fc2d77d8917674ec87a1
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-24 23:12:23 +0900

    Separate tags into components

---
+++ src/lib/components/parts/InfoLabel.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let categoryText: string;
+    export let condition: any;
+    export let labelFunction: () => string | undefined;
+</script>
+
+<span class="font-medium">{categoryText} : </span>
+{#if condition}
+    <span>{labelFunction()}</span>
+{:else}
+    <span class="text-gray-400">データ無し</span>
+{/if}
---
+++ src/lib/components/parts/PagingLabel.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+	import { createEventDispatcher } from "svelte";
+
+    export let isLoading = false;
+    export let startIndex: number;
+    export let resultCount: number;
+    export let isBottom = false;
+    const colorLime800 = '#3F6212';
+
+    const dispatch = createEventDispatcher();
+	const clickBackward = () => {
+		dispatch('backward');
+	};
+    const clickForward = () => {
+		dispatch('forward');
+    }
+
+</script>
+
+{#if !isBottom || !isLoading}
+    <div class="flex max-w-xl">      
+        <button class="hover:bg-stone-300 rounded" type="button" on:click={clickBackward} disabled={isLoading}>
+            <Icon icon="ph:caret-left" width="32" height="32" color={colorLime800} />
+        </button>   
+        <span class="m-auto px-2">{`${resultCount? startIndex + 1 : 0 }～${(startIndex + 10) >= resultCount ? resultCount : (startIndex + 10)}/${resultCount}`}件</span>
+        <button class="hover:bg-stone-300 rounded" type="button" on:click={clickForward} disabled={isLoading}>
+            <Icon icon="ph:caret-right" width="32" height="32" color={colorLime800} />
+        </button>
+    </div>       
+{/if} 
