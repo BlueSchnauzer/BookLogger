5b2ea562205b554fb42b5d38d4dbd488b7795555
BlueSchnauzer <kbrucke08@gmail.com>
2023-10-24 17:04:40 +0900

    Add describe

--- src/lib/components/app/test/SearchResult.test.ts
+++ src/lib/components/app/test/SearchResult.test.ts
@@ -1,2 +1,9 @@
 import { render, fireEvent, screen } from '@testing-library/svelte';
 import { describe, expect, it, vitest } from 'vitest';
+import SearchResult from '../SearchResult.svelte';
+import type { books_v1 } from 'googleapis';
+import { getBookInfosByQueries } from '$lib/GoogleBooksAPI/RequestManage';
+
+//awaitがテストできないので保留
+describe('SearchResult', () => {
+});
