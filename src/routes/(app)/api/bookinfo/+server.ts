f87e19b14c063f6e9e853469d5912ec7c13685fd
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-03 03:58:21 +0900

    Add endpoint

--- src/routes/(app)/api/bookinfo/+server.ts
+++ src/routes/(app)/api/bookinfo/+server.ts
@@ -1,8 +1,9 @@
 import type { RequestHandler } from './$types';
-import { getBookInfoByUserId, insertBookInfo } from '$lib/server/services/database.service';
+import { getBookInfoByUserId, insertBookInfo, updateBookInfo } from '$lib/server/services/database.service';
 import { json } from '@sveltejs/kit';
 import type { books_v1 } from 'googleapis';
 import { BookInfo } from '$lib/server/models/BookInfo';
+import { validateReadingCount, validateReadingDate } from '$lib/utils';
 
 /**DBからユーザIDに一致するデータを取得する */
 export const GET: RequestHandler = async () => {
@@ -25,3 +26,36 @@ export const POST: RequestHandler = async ({ request }) => {
     //一旦そのまま返す
     return await insertBookInfo(bookInfoToInsert);
 };
+
+/**DBの書誌データを更新する */
+export const PUT: RequestHandler = async ({ request }) => {
+    const item = await request.json() as BookInfo;
+    if (!validatePutItem(item)) { return new Response('データが不正です', { status: 400}); }
+
+    return await updateBookInfo(item);
+};
+
+/**更新用データが不正でないか確認する */
+const validatePutItem = (bookInfo: BookInfo): boolean => {
+    if (!bookInfo._id) { 
+        return false;
+    }
+
+    let result = true;
+    if (!bookInfo.history) { return result; }
+    
+    bookInfo.history.forEach(item => {
+        if (!result) { return; }
+
+        if (!item.date) {
+            result = false;
+            return;
+        }
+        if (!validateReadingCount(item.currentPage, bookInfo.pageCount)) {
+            result = false;
+            return;
+        }
+    });
+
+    return result;
+}
