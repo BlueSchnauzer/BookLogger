78aef971030c3376c5e96d20a657e05713bb72d5
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-05 02:05:25 +0900

    Comment out hooks because unable to use basic auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,21 +1,22 @@
-import type { Handle } from "@sveltejs/kit";
-import { BASIC_AUTH } from "$env/static/private";
+// import type { Handle } from "@sveltejs/kit";
+// import { BASIC_AUTH } from "$env/static/private";
 
-//デプロイ後の簡易的なアクセス制限のため、Basic認証をかける
-//認証機能が実装できたり、ローカルで動かす際は削除する
+// //デプロイ後の簡易的なアクセス制限のため、Basic認証をかける
+// //認証機能が実装できたり、ローカルで動かす際は削除する
+// → Vercelにデプロイしたけど正しい情報を入れても認証が通らないのでコメントアウト
 
-export const handle: Handle = async ({ event, resolve }) => {
-  const auth = event.request.headers.get("Authorization");
+// export const handle: Handle = async ({ event, resolve }) => {
+//   const auth = event.request.headers.get("Authorization");
 
-  if (auth !== `Basic ${btoa(BASIC_AUTH)}`) {
-    return new Response("Not authorized", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate":
-          'Basic realm="User Visible Realm", charset="UTF-8"',
-      },
-    });
-  }
+//   if (auth !== `Basic ${btoa(BASIC_AUTH)}`) {
+//     return new Response("Not authorized", {
+//       status: 401,
+//       headers: {
+//         "WWW-Authenticate":
+//           'Basic realm="User Visible Realm", charset="UTF-8"',
+//       },
+//     });
+//   }
 
-	return await resolve(event);
-};
+// 	return await resolve(event);
+// };
