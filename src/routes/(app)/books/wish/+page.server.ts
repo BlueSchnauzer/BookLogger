19f2911be726fc82f47e5069be2a70080b7b88e2
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-06 00:12:41 +0900

    Add wish endpoint and call proccess

--- src/routes/(app)/books/wish/+page.server.ts
+++ src/routes/(app)/books/wish/+page.server.ts
@@ -2,7 +2,8 @@ import type { PageServerLoad } from './$types';
 import type { BookInfo } from '$lib/server/models/BookInfo';
 
 export const load = (async ({fetch}) => {
-    const response = await fetch('/api/bookinfo');
+    //読みたい本のみを取得
+    const response = await fetch('/api/bookinfo/wish');
     let bookInfos: BookInfo[] = await response.json();
 
     return { bookInfos };
