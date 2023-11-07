c33db89b6b01e3188e2aa10ae8b939e26023ef19
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-07 14:45:53 +0900

    Add endpoint

---
+++ src/routes/(app)/api/bookinfo/reading/+server.ts
@@ -0,0 +1,14 @@
+import type { RequestHandler } from './$types';
+import collections from '$lib/server/database/collections';
+import { json } from '@sveltejs/kit';
+import { getReadingBookInfo } from '$lib/server/database/bookInfo.service';
+
+/**DBから読んでいる本(isCompleteがFalseで、historyに記録がある)で、ユーザIDに一致するデータを取得する */
+export const GET: RequestHandler = async () => {
+    const userId = 1; //todo クッキーから取る？無ければエラー
+
+    if (!collections) { return json(userId, {status: 500});}
+    let bookInfos = await getReadingBookInfo(collections, userId);
+
+    return json(bookInfos, {status: 200});
+};
