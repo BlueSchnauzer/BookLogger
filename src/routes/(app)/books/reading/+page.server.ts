be61a95311f7f092892de8dcaaf0f266b5d0395f
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-16 00:52:37 +0900

    Change indent

--- src/routes/(app)/books/reading/+page.server.ts
+++ src/routes/(app)/books/reading/+page.server.ts
@@ -1,13 +1,13 @@
 import type { PageServerLoad } from './$types';
 import type { BookInfo } from '$lib/server/models/BookInfo';
 
-export const load = (async ({fetch}) => {
-    //読んでいる本のみを取得
-    const response = await fetch('/api/bookinfo/reading');
-    let bookInfos: BookInfo[] = await response.json();
+export const load = (async ({ fetch }) => {
+  //読んでいる本のみを取得
+  const response = await fetch('/api/bookinfo/reading');
+  let bookInfos: BookInfo[] = await response.json();
 
-    return { 
-        bookInfos, 
-        emptyMessage: '読んでいる本が登録されていません。<br>本の詳細画面を開いて読んだ記録を追加してください！' 
-    };
+  return {
+    bookInfos,
+    emptyMessage: '読んでいる本が登録されていません。<br>本の詳細画面を開いて読んだ記録を追加してください！'
+  };
 }) satisfies PageServerLoad;
