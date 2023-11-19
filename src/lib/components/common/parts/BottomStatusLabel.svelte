65223f89b6fa28da8bc23a99a02cfad697e4f785
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-20 00:32:43 +0900

    Add icon by status

--- src/lib/components/common/parts/BottomStatusLabel.svelte
+++ src/lib/components/common/parts/BottomStatusLabel.svelte
@@ -1,21 +1,30 @@
 <script lang='ts'>
 	import type { typeForBottomLabel } from '$lib/customTypes';
+	import CompleteBook from '$lib/icons/CompleteBook.svelte';
 	import type { BookInfo } from '$lib/server/models/BookInfo';
 	import { convertDate } from '$lib/utils';
+	import Icon from '@iconify/svelte';
 
   export let typeForLabel : typeForBottomLabel;
   export let bookInfo: BookInfo;
+	const colorStone700 = '#44403C';
 </script>
 
 
 {#if typeForLabel === 'createDate'}
   <div class="self-center flex justify-between max-sm:hidden">
-    <span class="pl-2 text-xs">登録日</span>
+    <div class="pl-2 flex items-center">
+      <Icon icon="mdi:pencil-plus-outline" width="18" height="18" color={colorStone700}/>
+      <span class="pl-1 text-xs">登録日</span>
+    </div>
     <span class="pr-2 text-sm">{convertDate(bookInfo.createDate)}</span>
   </div>
 {:else if typeForLabel === 'completeDate'}
   <div class="self-center flex justify-between max-sm:hidden">
-    <span class="pl-2 text-xs">読み終わった日</span>
+    <div class="pl-2 flex items-center">
+      <CompleteBook width={18} height={18} color={colorStone700}/>
+      <span class="pl-1 text-xs">読み終わった日</span>
+    </div>
     <span class="pr-2 text-sm">{convertDate(bookInfo.completeDate)}</span>
   </div>
 {:else}
