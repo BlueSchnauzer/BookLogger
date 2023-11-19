8e86523f111642b13adedeec3b37ce5d98846202
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-19 21:13:27 +0900

    Create bottom status label component for book info grid

---
+++ src/lib/components/common/parts/BottomStatusLabel.svelte
@@ -0,0 +1,26 @@
+<script lang='ts'>
+	import type { typeForBottomLabel } from '$lib/customTypes';
+	import type { BookInfo } from '$lib/server/models/BookInfo';
+	import { convertDate } from '$lib/utils';
+
+  export let typeForLabel : typeForBottomLabel;
+  export let bookInfo: BookInfo;
+</script>
+
+
+{#if typeForLabel === 'createDate'}
+  <div class="self-center flex justify-between max-sm:hidden">
+    <span class="pl-2 text-xs">登録日</span>
+    <span class="pr-2 text-sm">{convertDate(bookInfo.createDate)}</span>
+  </div>
+{:else if typeForLabel === 'completeDate'}
+  <div class="self-center flex justify-between max-sm:hidden">
+    <span class="pl-2 text-xs">読み終えた日</span>
+    <span class="pr-2 text-sm">{convertDate(bookInfo.completeDate)}</span>
+  </div>
+{:else}
+  <div class="self-center flex justify-between max-sm:hidden">
+    <span class="pl-2 text-xs">progressを出す予定</span>
+    <span class="pr-2 text-sm">{convertDate(bookInfo.createDate)}</span>
+  </div>
+{/if}
