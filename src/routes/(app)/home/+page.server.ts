b06ef4788f5604f65d59821f082a6f37d81ba75f
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-29 23:18:02 +0900

    Fix proccess to count page count in this week

--- src/routes/(app)/home/+page.server.ts
+++ src/routes/(app)/home/+page.server.ts
@@ -17,40 +17,51 @@ export const load = (async ({ fetch }) => {
   
   const bookInfos: BookInfo[] = await recentBook.json();
   const pagesWithDate = getPageCountInCurrentWeek(await history.json());
+  console.log(pagesWithDate);
   
   return { bookInfos, pagesWithDate };
 }) satisfies PageServerLoad;
 
 const getPageCountInCurrentWeek = (bookInfos: BookInfo[]) => {
-  if (bookInfos == null) { return []; }
-
-  //操作しやすくするためにstringで保持
-  let dates: string[] = [];
+  //文字列の日付と、ページ数を持つmapを作成
+  const today = new Date();
+  const pageMap = new Map<string, number>();
   for (let i = 6; i >= 0; i--) {
-    const date = new Date();
+    const date = new Date(today);
     date.setDate(date.getDate() - i);
-    dates.push(date.toLocaleDateString('ja-JP'));
+    pageMap.set(date.toLocaleDateString('ja-JP'), 0);
   }
 
-  //日付の文字列をプロパティ名とし、値にはページ数を持つオブジェクトを作成
-  let pagesWithDate = dates.map(date => ({date: date, pageCount: 0}));
+  if (!bookInfos) { return pageMap; }
 
-  const today = new Date();
-  today.setDate(today.getDate() - 6);
   //先週の0時0分を取得
+  today.setDate(today.getDate() - 6);
   const lastDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
   
+  //各書誌データのHistoryを確認して1日に読んだページ数を取得
   bookInfos.forEach(bookInfo => {
-    //今週のhistoryを取得
+    //今週分のhistoryを取得し、日付の重複を排除する(残すのは最大のページ数)
     const historyInCurrentWeek = bookInfo.history?.filter(item => new Date(item.date) >= lastDate);
+    const historyMap = new Map<string, number>();
 
-    //今週分の読んだ記録があれば、ページ数を加算
     historyInCurrentWeek?.forEach(item => {
-      const itemDate = new Date(item.date).toLocaleDateString('ja-JP');
-      const matchedData = pagesWithDate.find(pageData => pageData.date === itemDate);
-      if (matchedData) { matchedData.pageCount += item.currentPage;}
-    })
+      //DBから取ったデータは文字列になっているため変換
+      const key = new Date(item.date).toLocaleDateString('ja-JP');
+      if (!historyMap.has(key) || historyMap.get(key)! < item.currentPage) {
+        historyMap.set(key, item.currentPage);
+      }
+    });
+
+    if (historyMap.size === 0) { return; }
+
+    //ページ数を加算する
+    historyMap.forEach((value, key) => {
+      if (pageMap.has(key)) { 
+        const orgValue = pageMap.get(key);
+        pageMap.set(key, orgValue! + value);
+      }
+    });
   });
 
-  return pagesWithDate;
+  return pageMap;
 }
