1694524c27f6b9e1aebdcccc773ec4f5fd080a8b
BlueSchnauzer <kbrucke08@gmail.com>
2023-12-01 00:56:54 +0900

    Replace utils files

--- src/lib/utils/bookInfo.ts
+++ src/lib/utils/bookInfo.ts
@@ -1,53 +1,7 @@
-import { toast } from "@zerodevx/svelte-toast";
 import type { BookInfo } from "$lib/server/models/BookInfo";
 import type { ObjectId } from "mongodb";
 import type { toggleFilterItem, typeForBottomLabel } from "$lib/customTypes";
-
-export const convertDate = (date: Date | string | undefined, useYear = true): string => {
-	if (!date) { return 'データ無し'; }
-	//DBから取った書誌データは文字列で日付を持ってるため
-	if (typeof date === 'string') { date = new Date(date); }
-	return `${useYear? `${date.getFullYear()}/` : ''}${date.getMonth() + 1}/${date.getDate()}`;
-}
-
-/**日付(読んだ記録)チェック */
-export const validateReadingDate = (readingDate: string): boolean => {
-	if (!readingDate) { return false; }
-
-	return true;
-}
-
-/**ページ数(読んだ記録)チェック */
-export const validateReadingCount = (readingCount: number, pageCount: number): boolean => {
-	if (!readingCount || readingCount < 1 || pageCount < readingCount) {
-		return false;
-	}
-
-	return true;
-}
-
-//トーストは利用先でコンテナのインポート、スタイル適用とアンマウント時のpopが必要
-
-/**画面右上にトーストを表示する*/
-export const pushToast = (message: string, target: string): void => {
-	toast.push(message, { theme: { '--toastBarBackground': '#65a30d' }, target });
-}
-
-/**画面下部に正常完了用トーストを表示する*/
-export const pushSuccessToast = (message: string, target: string): void => {
-	toast.push(message, { reversed: true, intro: { y: 100 },
-		theme: { '--toastBarBackground': '#65a30d' }, 
-		target 
-	});
-}
-
-/**画面下部に異常完了用トーストを表示する*/
-export const pushErrorToast = (message: string, target: string): void => {
-	toast.push(message, { reversed: true, intro: { y: 100 }, 
-		theme:{ '--toastBarHeight': 0, '--toastWidth': 'auto', '--toastBackground': '#dc2626' }, 
-		target 
-	});
-}
+import { pushSuccessToast } from "$lib/utils/toast";
 
 /**編集内容を反映した書誌データを返す(再レンダリングに使用) */
 export const applyChangesToBookInfos = (bookInfos: BookInfo[], detail: {message: string, updatedItem: BookInfo, deletedId: ObjectId}): BookInfo[] => {
@@ -99,4 +53,11 @@ export const getTypeForBottomLabel = (pathName: string): typeForBottomLabel => {
 			: 'createDate';
 
 	return typeForLabel;
+}
+
+export const convertDate = (date: Date | string | undefined, useYear = true): string => {
+	if (!date) { return 'データ無し'; }
+	//DBから取った書誌データは文字列で日付を持ってるため
+	if (typeof date === 'string') { date = new Date(date); }
+	return `${useYear? `${date.getFullYear()}/` : ''}${date.getMonth() + 1}/${date.getDate()}`;
 }
