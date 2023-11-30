6628dc3d3f2ef64ecf9465442e22ed8f13487d0b
BlueSchnauzer <kbrucke08@gmail.com>
2023-11-30 10:21:03 +0900

    Add chart js to display bar graph in home

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    //export let data: PageData;
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+  import { Chart } from 'chart.js/auto';
+
+	export let data: PageData;
+  let countGraph: HTMLCanvasElement;
+
+  const labels = Array.from(data.pagesWithDate.keys());
+  const graphData = Array.from(data.pagesWithDate.values());
+
+  onMount(() => {
+    const chart = new Chart(countGraph, {
+      type: 'bar',
+      data: {
+        labels,
+        datasets: [{
+          label: '1週間に読んだページ数',
+          data: graphData
+        }]
+      }
+    });
+  })
 </script>
 
-<h1>ホーム</h1>
+<main class="flex-1 my-2 max-md:pb-16 flexWidth">
+  <canvas bind:this={countGraph} id="countGraph"></canvas>
+</main>
+
+<style>
+  .flexWidth {
+		width: calc(100% - (224px));
+	}
+</style>
