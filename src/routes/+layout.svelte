b93633746e74c798c513bff7fe198e9af3ffd017
BlueSchnauzer <kbrucke08@gmail.com>
2023-12-05 01:19:36 +0900

    Add progress bar to  route layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,29 @@
 <script>
 	import '../app.css';
+	import NProgress from 'nprogress';
+	import 'nprogress/nprogress.css';
+	import { navigating } from '$app/stores';
+
+  NProgress.configure({
+    showSpinner: false
+  });
+ 
+	$: {
+		//ページ移動中にプログレスを表示する
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
+
 </script>
 
 <slot />
+
+<style>
+	:global(#nprogress .bar) {
+		background-color: #65a30d;
+	}
+</style>
