be087267662e37656edf9cc31a27aaf99b401caa
BlueSchnauzer <kbrucke08@gmail.com>
2023-12-07 01:02:03 +0900

    Create firebase init file

---
+++ src/lib/firebase.ts
@@ -0,0 +1,31 @@
+import {
+  PUBLIC_FIREBASE_API_KEY,
+  PUBLIC_FIREBASE_AUTH_DOMAIN,
+  PUBLIC_FIREBASE_PROJECT_ID,
+  PUBLIC_FIREBASE_STORAGE_BUCKET,
+  PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  PUBLIC_FIREBASE_APP_ID
+} from '$env/static/public';
+import { getApps, initializeApp, type FirebaseOptions, getApp, deleteApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+ 
+const firebaseConfig: FirebaseOptions = {
+  apiKey: PUBLIC_FIREBASE_API_KEY,
+  authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: PUBLIC_FIREBASE_APP_ID
+};
+
+let firebaseApp;
+
+if (!getApps().length) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = getApp();
+  deleteApp(firebaseApp);
+  firebaseApp = initializeApp(firebaseConfig);
+}
+ 
+export const auth = getAuth(firebaseApp);
