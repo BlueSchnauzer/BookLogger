4ed8d5bd686e03e2a3c6005c290c5bc9de4b527d
BlueSchnauzer <kbrucke08@gmail.com>
2023-12-10 00:03:08 +0900

    Change to display logout button in sidemenu

--- src/lib/components/menu/SideMenu.svelte
+++ src/lib/components/menu/SideMenu.svelte
@@ -24,37 +24,47 @@
 
 </script>
 
-<nav class="max-md:hidden m-2 w-56 rounded-xl shadow-2xl bg-stone-700">
+<nav class="flex flex-col max-md:hidden m-2 w-56 rounded-xl shadow-2xl bg-stone-700">
 	<div class="flex p-3">
 		<Icon icon="ph:books-light" width="36" height="36" color={colorStone200}/>
 		<p class="ml-2.5 text-xl text-stone-200">BookLogger</p>
 	</div>
 	<div class="mx-3 my-2 bg-stone-200 h-[1px]" />
-	<ul>
-		{#each MenuItemDatas as data (data.icon)}
-			{#if data.icon === BookShelf}
-				<li>
-					<div class="mx-3 my-2 bg-stone-200 h-[1px]" />
-				</li>
-			{/if}
-			<li
-				class="flex h-14 duration-300 border-l-4 border-transparent hover:border-x-lime-600 hover:bg-stone-600 
-				{data.ref === pathName ? 'border-x-lime-600 bg-stone-600 ' : ''}"
-			>
-				<a href={data.ref} class="flex flex-1 group items-center rounded-md">
-					<div class="w-9 h-9 m-0.5 p-1.5 rounded-lg bg-stone-600">
-						<svelte:component this={data.icon} color={colorStone200} />
-					</div>
-					{#if data.icon === BookShelf}
-						<div class="ml-2.5 mr-3 flex flex-1 justify-between">
-							<span class=" text-stone-200">{data.jpName}</span>
-							<Icon icon="ph:arrow-down" width="24" height="24" color={colorStone200}/>
+	<div class="flex flex-col flex-grow justify-between pb-2">
+		<ul>
+			{#each MenuItemDatas as data (data.icon)}
+				{#if data.icon === BookShelf}
+					<li>
+						<div class="mx-3 my-2 bg-stone-200 h-[1px]" />
+					</li>
+				{/if}
+				<li
+					class="flex h-14 duration-300 border-l-4 border-transparent hover:border-x-lime-600 hover:bg-stone-600 
+					{data.ref === pathName ? 'border-x-lime-600 bg-stone-600 ' : ''}"
+				>
+					<a href={data.ref} class="flex flex-1 group items-center rounded-md">
+						<div class="w-9 h-9 m-0.5 p-1.5 rounded-lg bg-stone-600">
+							<svelte:component this={data.icon} color={colorStone200} />
 						</div>
-					{:else}
-						<span class="ml-2.5 text-stone-200">{data.jpName}</span>
-					{/if}
-				</a>
-			</li>
-		{/each}
-	</ul>
+						{#if data.icon === BookShelf}
+							<div class="ml-2.5 mr-3 flex flex-1 justify-between">
+								<span class=" text-stone-200">{data.jpName}</span>
+								<Icon icon="ph:arrow-down" width="24" height="24" color={colorStone200}/>
+							</div>
+						{:else}
+							<span class="ml-2.5 text-stone-200">{data.jpName}</span>
+						{/if}
+					</a>
+				</li>
+			{/each}
+		</ul>
+		<div class="flex h-14 duration-300 border-l-4 border-transparent hover:border-x-lime-600 hover:bg-stone-600">
+			<button class="flex flex-1 group items-center rounded-md">
+				<div class="w-9 h-9 m-0.5 p-1.5 rounded-lg bg-stone-600">
+					<Icon icon="ph:sign-out-bold" width="24" height="24" color={colorStone200}/>
+				</div>
+				<span class="ml-2.5 text-stone-200">ログアウト</span>
+			</button>
+		</div>
+	</div>
 </nav>
