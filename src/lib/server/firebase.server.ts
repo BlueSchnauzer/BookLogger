ad1cf5b4a8e8db9b9ab63d59592341d4044af143
BlueSchnauzer <kbrucke08@gmail.com>
2023-12-10 14:52:59 +0900

    Fix build error for using named import

--- src/lib/server/firebase.server.ts
+++ src/lib/server/firebase.server.ts
@@ -3,10 +3,12 @@ import {
   FIREBASE_ADMIN_CLIENT_EMAIL
 } from '$env/static/private'
 import { PUBLIC_FIREBASE_PROJECT_ID } from '$env/static/public';
-import { credential } from 'firebase-admin';
+import admin from 'firebase-admin';
 import { getApps, initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
+//ビルド時に名前付きエクスポートするとエラーになるので、一度デフォルトエクスポートを挟んでから取得
+const { credential } = admin;
 let firebaseAdmin;
 
 if (!getApps().length) {
