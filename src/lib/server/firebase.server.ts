c966a210529e8d9a71425a978eb01ff27066f280
BlueSchnauzer <kbrucke08@gmail.com>
2023-12-10 21:31:38 +0900

    Fix env type and Add env value to action file

--- src/lib/server/firebase.server.ts
+++ src/lib/server/firebase.server.ts
@@ -1,4 +1,8 @@
-import { env } from '$env/dynamic/private'
+import {
+  FIREBASE_ADMIN_PRIVATE_KEY,
+  FIREBASE_ADMIN_CLIENT_EMAIL
+} from '$env/static/private'
+import { PUBLIC_FIREBASE_PROJECT_ID } from '$env/static/public';
 import admin from 'firebase-admin';
 import { getApps, initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
@@ -10,9 +14,9 @@ let firebaseAdmin;
 if (!getApps().length) {
   firebaseAdmin = initializeApp({
     credential: credential.cert({
-      privateKey: env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      clientEmail: env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      projectId: env.PUBLIC_FIREBASE_PROJECT_ID
+      privateKey: FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: FIREBASE_ADMIN_CLIENT_EMAIL,
+      projectId: PUBLIC_FIREBASE_PROJECT_ID
     })
   })
 }
