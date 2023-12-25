c9ebc0cbdd2be2ab8a5f07780538184ec713586a
BlueSchnauzer <kbrucke08@gmail.com>
2023-12-25 22:11:06 +0900

    Use query parameter to search

--- src/routes/(app)/books/search/+page.ts
+++ src/routes/(app)/books/search/+page.ts
@@ -6,8 +6,8 @@ export const load = (async (params) => {
   const bookTitle = params.url.searchParams.get('booktitle');
   const author = params.url.searchParams.get('author');
   const isbn = params.url.searchParams.get('isbn');
-  const page = params.url.searchParams.get('page');
-  const count = params.url.searchParams.get('count');
+  const page = Number(params.url.searchParams.get('page'));
+  const startIndex = page > 0 ? page : 1;
 
   //パラメータを条件に検索を行う関数を作成し、クライアント側に渡して実行
   const requestBookInfo = async (startIndex = 0) => requestBookInfosByQueries(bookTitle!, author!, isbn!, 10, startIndex) as books_v1.Schema$Volumes;
