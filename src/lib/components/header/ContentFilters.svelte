eb1d55485c5171628c9dca62f731ab615773508f
BlueSchnauzer <kbrucke08@gmail.com>
2024-02-08 21:08:28 +0900

    Add style to books route buttons to fix current route

--- src/lib/components/header/ContentFilters.svelte
+++ src/lib/components/header/ContentFilters.svelte
@@ -43,12 +43,14 @@
 </script>
 
 <div class="flex justify-between items-center">
-	<ul class="flex items-center -mb-[9px]">
+	<ul class="flex items-center -mb-[5px]">
 		{#each MenuItemDatas as item }
-			<li class="h-10 px-2 whitespace-nowrap 
-				rounded-t-xl
-				{item.ref === '/books'? 'border-x bg-vellum' : 'border-r'} 
-				border-t border-stone-500 bg-stone-300">
+			<li class="h-11 px-2 whitespace-nowrap 
+				{item.ref === '/books' ? 'border-x' : 'border-r'} 
+				{item.ref === pathName ? 'bg-vellum text-lime-700 font-medium border-t-[3px] border-t-lime-700' : 'border-b'}
+				border-t border-stone-500 bg-stone-300
+				hover:bg-vellum duration-150"
+			>
 				<a href={item.ref} class="leading-10">
 					<p>{item.name}</p>
 				</a>
