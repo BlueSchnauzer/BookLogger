7dd5a2539e2dbad399c5b8239ce64da6ea0b065c
BlueSchnauzer <kbrucke08@gmail.com>
2024-02-14 21:37:41 +0900

    Fix search route

--- tests/(app)/books/search/search.spec.ts
+++ tests/(app)/books/search/search.spec.ts
@@ -1,21 +1,33 @@
 import { test, expect } from '../../../fixtures/loginAndLogout';
 
 test.describe('書籍検索', () => {
-  test('検索結果を表示できること', async({ loginedPage }) => {
-    await loginedPage.page.getByTestId('btnDisplaySearch').click();
+  test('簡易検索で検索結果を表示できること', async({ loginedPage }) => {
+    const pageName = '書籍検索'
+    await loginedPage.page.locator('[href="/books/search"]', {hasNotText: pageName}).click();
+    await expect(loginedPage.page.waitForURL('/books/search')).toBeTruthy();
+    await expect(loginedPage.page.getByTestId('headerText')).toHaveText(pageName);
     
-    await loginedPage.page.getByTestId('btnDisplayDetailQueries').click();
     const bookTitle = 'クイーンズ・ギャンビット';
-    await loginedPage.page.getByRole('textbox', {name: 'title'}).click;
-    await loginedPage.page.getByRole('textbox', {name: 'title'}).fill(bookTitle);
-    await loginedPage.page.getByTitle('検索').click();
-    await expect(loginedPage.page.waitForURL(/books\/search\?booktitle=.*&author=&isbn=/)).toBeTruthy();
+    const input = loginedPage.page.getByRole('textbox', {name: 'query'});
+    await input.click();
+    await input.fill(bookTitle);
+    await loginedPage.page.getByTitle('検索', {exact: true}).click();
+    await expect(loginedPage.page.waitForURL(`/books/search?query=${bookTitle}&page=0`)).toBeTruthy();
 
-    //作成して削除だと不安定なため保留
-    // await loginedPage.page.getByTestId('searchLoader').waitFor({state: 'detached'});
-    // await loginedPage.page.getByText(bookTitle, {exact: true}).click();
-    // await expect(loginedPage.page.getByText('書籍登録')).toBeVisible();
-    // await loginedPage.page.getByRole('button', {name: '登録'}).click();
-    // await expect(loginedPage.page.getByText('登録しました')).toBeVisible();
+  });
+
+  test('詳細検索で検索結果を表示できること', async({ loginedPage }) => {
+    const pageName = '書籍検索'
+    await loginedPage.page.locator('[href="/books/search"]', {hasNotText: pageName}).click();
+    await expect(loginedPage.page.waitForURL('/books/search')).toBeTruthy();
+    await expect(loginedPage.page.getByTestId('headerText')).toHaveText(pageName);
+
+    const bookTitle = 'クイーンズ・ギャンビット';
+    await loginedPage.page.getByTestId('btnDisplayDetailQueries').click();
+    const input = loginedPage.page.getByRole('textbox', {name: 'title'});
+    await input.click();
+    await input.fill(bookTitle);
+    await loginedPage.page.getByTitle('検索', {exact: true}).click();
+    await expect(loginedPage.page.waitForURL(/books\/search\?booktitle=.*&author=&isbn=&page=0/)).toBeTruthy();
   });
 })
