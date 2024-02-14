d6bff86768e662c41e3eff63f66d7b28dc2776a9
BlueSchnauzer <kbrucke08@gmail.com>
2024-02-14 21:12:12 +0900

    Fix library pages tests

--- tests/(app)/books/complete/complete.spec.ts
+++ tests/(app)/books/complete/complete.spec.ts
@@ -2,9 +2,11 @@ import { test, expect } from '../../../fixtures/loginAndLogout';
 
 test.describe('読み終わった本', () => {
   test('読み終わった本の画面が表示できること', async ({ loginedPage }) => {
-    const pageName = '読み終わった本';
+    await loginedPage.gotoLibrary();
+  
+    const pageName = '読み終わった';
     await loginedPage.page.getByText(pageName).click();
     await expect(loginedPage.page.waitForURL('/books/complete')).toBeTruthy();
-    await expect(loginedPage.page.getByTestId('headerText')).toHaveText(pageName);
+    await expect(loginedPage.page.getByTestId('headerText')).toHaveText(`${pageName}本`);
   });
 })
