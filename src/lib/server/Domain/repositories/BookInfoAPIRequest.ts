6a8e8bcb8033b13049de7bb91fe30735c16f4688
BlueSchnauzer <kbrucke08@gmail.com>
2024-03-05 21:25:51 +0900

    Fix bookinfo api request repo's argument and return values

--- src/lib/server/Domain/repositories/BookInfoAPIRequest.ts
+++ src/lib/server/Domain/repositories/BookInfoAPIRequest.ts
@@ -1,3 +1,4 @@
+import type { books_v1 } from "googleapis";
 import type { BookInfo } from "../Entities/BookInfo";
 
 export interface IBookInfoAPIRequestRepository {
@@ -10,15 +11,15 @@ export interface IBookInfoAPIRequestRepository {
   /**読み終わった本ステータスの書誌データを取得する */
   getComplete(): Promise<BookInfo[]>;
   /**直近で読んだ書誌データを取得する */
-  getRecent(): Promise<BookInfo>;
+  getRecent(): Promise<BookInfo | undefined>;
   /**1週間に読んだページ数を取得する */
-  getHistory(): Promise<Map<string, number>>;
+  getHistory(): Promise<Map<string, number> | undefined>;
   /**書誌データを保存する */
-  post(bookInfo: BookInfo): Promise<boolean>;
+  post(postDate: books_v1.Schema$Volumes): Promise<{isSuccess: boolean, message: string}>;
   /**書誌データを更新する */
-  put(bookInfo: BookInfo): Promise<boolean>;
+  put(bookInfo: BookInfo, isComplete: boolean): Promise<{isSuccess: boolean, message: string}>;
   /**書誌データを削除する 
    * 一旦BookInfoのidを取る
   */
-  delete(bookInfo: BookInfo): Promise<boolean>
+  delete(bookInfo: BookInfo): Promise<{isSuccess: boolean, message: string}>
 }
