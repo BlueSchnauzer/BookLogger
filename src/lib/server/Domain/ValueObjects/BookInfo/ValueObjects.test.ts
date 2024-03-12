7bf771fedc2521bbefee251e3ab98cec6e7c4473
BlueSchnauzer <kbrucke08@gmail.com>
2024-03-13 00:08:42 +0900

    Add userId tests

--- src/lib/server/Domain/ValueObjects/BookInfo/ValueObjects.test.ts
+++ src/lib/server/Domain/ValueObjects/BookInfo/ValueObjects.test.ts
@@ -10,4 +10,18 @@ describe('UserId', () => {
     expect(userId).not.toBeUndefined();
     expect(userId.value).toEqual(data);
   });
+
+  it('Falthyな値もしくはstring以外の値を渡した際にエラーが発生すること', () => {
+    expect(new UserId('')).toThrowError(Error);
+    expect(new UserId(10 as unknown as string)).toThrowError(Error);
+  });
+
+  it('equals()でUserIdのvalueと引数の値が同一かを判定できること', () => {
+    const userIdA = new UserId(data);
+    const userIdB = new UserId(data);
+    const userIdC = new UserId('test');
+
+    expect(userIdA.equals(userIdB)).toBeTruthy();
+    expect(userIdA.equals(userIdC)).toBeFalsy();
+  });
 });
