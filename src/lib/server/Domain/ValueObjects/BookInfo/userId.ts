50ad7d0e5a8e118ffe292cc72276164c7e212341
BlueSchnauzer <kbrucke08@gmail.com>
2024-03-14 21:34:19 +0900

    use validation error class

--- src/lib/server/Domain/ValueObjects/BookInfo/userId.ts
+++ src/lib/server/Domain/ValueObjects/BookInfo/userId.ts
@@ -1,4 +1,5 @@
 import { ValueObjectsBase } from "$lib/server/Domain/ValueObjects/ValueObjectBase";
+import { ValidationError } from "$lib/server/Domain/Exceptions/ValidationError";
 
 export class UserId extends ValueObjectsBase<string> {
   constructor(public value: string) {
@@ -6,8 +7,8 @@ export class UserId extends ValueObjectsBase<string> {
   }
   
   validate(value: string): void {
-    if (!value) { throw Error('UserIdが設定されていません'); }
-    if (typeof value !== 'string') { throw Error('UserIdの形式が文字列ではありません'); }
+    if (!value) { throw new ValidationError('UserIdが設定されていません'); }
+    if (typeof value !== 'string') { throw new ValidationError('UserIdの形式が文字列ではありません'); }
   }
 
   equalsCore(vo: ValueObjectsBase<string>): boolean {
