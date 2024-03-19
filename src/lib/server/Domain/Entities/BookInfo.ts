283d0d6e120925c714cceff1048a76b7cfd86169
BlueSchnauzer <kbrucke08@gmail.com>
2024-03-19 21:56:17 +0900

    Implement bookinfo constructor

--- src/lib/server/Domain/Entities/BookInfo.ts
+++ src/lib/server/Domain/Entities/BookInfo.ts
@@ -1,10 +1,12 @@
-import type { status } from '$lib/customTypes';
-import type { books_v1 } from 'googleapis';
 import type { ObjectId } from 'mongodb';
+import { UserId } from '$lib/server/Domain/ValueObjects/BookInfo/UserId';
+import { Status, type status } from "$lib/server/Domain/ValueObjects/BookInfo/Status";
+import { PageHistory, type pageHistory } from '$lib/server/Domain/ValueObjects/BookInfo/PageHistory';
+import { Identifiers, type identifiers } from '$lib/server/Domain/ValueObjects/BookInfo/Identifier';
 
 /**書誌情報 */
 export class BookInfo {
-	public userId: string;
+	public userId: UserId;
 	public title: string;
 	public author: string[];
 	public thumbnail: string;
@@ -12,57 +14,52 @@ export class BookInfo {
 	public updateDate: Date;
 	public pageCount: number;
 	public isFavorite: boolean;
-	public status: status;
+	public status: Status;
 	public memorandum: string;
 	public isVisible: boolean;
-	public _id?: ObjectId;
+	public id?: ObjectId;
 	public completeDate?: Date;
-	public pageHistory?: 
-		{
-			id: string;
-			date: Date;
-			currentPage: number;
-		}[]
-	;
-	public identifier?: {
-		isbn_13?: string;
-		isbn_10?: string;
-	};
-	public shelfCategory?: ObjectId[]
+	public pageHistories?: PageHistory[];
+	public identifiers?: Identifiers[];
+	public shelfCategories?: ObjectId[]
 	public gapiId?: string;
 
-	/**GAPIのvolumeで初期化する */
-	constructor(volume: books_v1.Schema$Volume, userId: string){
-		const currentDate = new Date;
-
-		this.userId = userId;
-		this.title = volume.volumeInfo?.title ?? '';
-		this.author = volume.volumeInfo?.authors ?? [''];
-		this.thumbnail = volume.volumeInfo?.imageLinks?.thumbnail ?? ''; //gapi固有の情報だが、画像そのものではなく場所を表すURLを保存する。
-		this.createDate = currentDate;
-		this.updateDate = currentDate;
-		this.pageCount = volume.volumeInfo?.pageCount ?? 0;
-		this.status = 'wish';
-		this.isFavorite = false;
-		this.memorandum = '';
-		this.isVisible = true;
-		this.identifier = getIdentifier(volume.volumeInfo?.industryIdentifiers);
-		this.gapiId = volume.id ?? this.title; //gapi固有の情報なので入れたら微妙な感じではある
+	//MongoDBのモデルをそのまま渡して、内部でプロパティを参照するように修正する。
+	constructor(userId: string, title: string, author: string[], thumbnail: string, createDate: Date, updateDate: Date,
+		pageCount: number, isFavorite: boolean, status: status, memorandum: string, isVisible: boolean,
+		_id?: ObjectId, completeDate?: Date, pageHistories?: pageHistory[], identifiers?: identifiers[], shelfCategories?: ObjectId[], gapiId?: string) {
+		this.userId = new UserId(userId);
+		this.title = title;
+		this.author = author;
+		this.thumbnail = thumbnail;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+		this.pageCount = pageCount;
+		this.isFavorite = isFavorite;
+		this.status = new Status(status);
+		this.memorandum = memorandum;
+		this.isVisible = isVisible;
+		this.id = _id;
+		this.completeDate = completeDate;
+		this.pageHistories = pageHistories?.map(item => new PageHistory(item));
+		this.identifiers = identifiers?.map(item => new Identifiers(item));
+		this.shelfCategories = shelfCategories;
+		this.gapiId = gapiId;
 	}
 }
 
 type industryIdentifiers = {
 	identifier?: string | undefined;
 	type?: string | undefined;
-} [] | undefined
+}[] | undefined
 
 /**ISBNを取得する(存在しない場合はundefined) */
 const getIdentifier = (identifiers: industryIdentifiers) => {
 	const isbn_13 = identifiers?.find(id => id.type === 'ISBN_13')?.identifier;
-	if (isbn_13) { return {isbn_13}; }
-	
+	if (isbn_13) { return { isbn_13 }; }
+
 	const isbn_10 = identifiers?.find(id => id.type === 'ISBN_10')?.identifier;
-	if (isbn_10) { return {isbn_10}; }
+	if (isbn_10) { return { isbn_10 }; }
 
 	return undefined;
 }
