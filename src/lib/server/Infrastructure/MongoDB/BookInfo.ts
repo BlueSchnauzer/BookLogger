235fb353f2086de7881038fb68782c2e33ab1ee4
BlueSchnauzer <kbrucke08@gmail.com>
2024-03-26 22:11:08 +0900

    Converting entity to mongoDBModel before interacting mongoDB in mongoDB infrastructure

--- src/lib/server/Infrastructure/MongoDB/BookInfo.ts
+++ src/lib/server/Infrastructure/MongoDB/BookInfo.ts
@@ -1,8 +1,9 @@
 import type { IBookInfoRepositories } from "$lib/server/Domain/repositories/BookInfo";
 import type { bookInfosCollection } from "$lib/server/Infrastructure/MongoDB/MongoDBHelper";
-import type { UserId } from "$lib/server/Domain/ValueObjects/BookInfo/UserId";
 import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
-import type { status } from "$lib/customTypes";
+import type MongoDBModel from "$lib/server/Domain/Entities/MongoDBModel/BookInfo";
+import type { UserId } from "$lib/server/Domain/ValueObjects/BookInfo/UserId";
+import type { Status } from "$lib/server/Domain/ValueObjects/BookInfo/Status";
 import { ObjectId, type Filter, type UpdateFilter } from 'mongodb';
 
 /**MongoDBでの書誌データ操作を管理する */
@@ -16,55 +17,55 @@ export class BookInfoMongoDB implements IBookInfoRepositories {
   }
   
   async get(): Promise<BookInfo[]> {
-    let bookInfos: BookInfo[] = [];
+    let mongoDBModel: MongoDBModel[] = [];
   
     try {
-      bookInfos = await this._collection.find({userId: this._userId}).toArray() as BookInfo[];  
+      mongoDBModel = await this._collection.find({userId: this._userId}).toArray() as MongoDBModel[];  
     }
     catch (error) {
       console.log(error);
       console.log('書誌データの取得に失敗しました。');
     }
   
-    return bookInfos;
+    return mongoDBModel.map(item => item.convertToEntity());
   }
 
-  async getByStatus(status: status): Promise<BookInfo[]> {
-    let bookInfos: BookInfo[] = [];
+  async getByStatus(status: Status): Promise<BookInfo[]> {
+    let mongoDBModel: MongoDBModel[] = [];
   
     try {
-      const filter: Filter<BookInfo> = {
+      const filter: Filter<MongoDBModel> = {
         $and: [
           {userId: this._userId},
-          {status}
+          {status: status.value}
         ]
       };
-      bookInfos = await this._collection.find(filter).toArray() as BookInfo[];  
+      mongoDBModel = await this._collection.find(filter).toArray() as MongoDBModel[];  
     }
     catch (error) {
       console.log(error);
       console.log('書誌データの取得に失敗しました。');
     }
   
-    return bookInfos;
+    return mongoDBModel.map(item => item.convertToEntity());
   }
 
   async getRecent(): Promise<BookInfo[]> {
-    let bookInfos: BookInfo[] = [];
+    let mongoDBModel: MongoDBModel[] = [];
 
     try {
       //pageHistoryが0より大きいデータを、更新日を降順にしてから、1個だけ取る
-      bookInfos = await this._collection.find({
+      mongoDBModel = await this._collection.find({
           userId: this._userId,
           "pageHistory.0": { $exists: true}
-        }).sort({updateDate: -1}).limit(1).toArray() as BookInfo[];  
+        }).sort({updateDate: -1}).limit(1).toArray() as MongoDBModel[];  
     }
     catch (error) {
       console.log(error);
       console.log('書誌データの取得に失敗しました。');
     }
 
-    return bookInfos;
+    return mongoDBModel.map(item => item.convertToEntity());
   }
 
   async getPageHistory(): Promise<BookInfo[]> {
@@ -91,7 +92,7 @@ export class BookInfoMongoDB implements IBookInfoRepositories {
     }
   
     try {
-      const result = await this._collection?.insertOne(bookInfo);
+      const result = await this._collection?.insertOne(bookInfo.convertToMongoDBModel());
       if (result?.acknowledged){
         response = new Response('書誌データの作成に成功しました。', {status: 201} );
       }
@@ -106,6 +107,7 @@ export class BookInfoMongoDB implements IBookInfoRepositories {
 
   async update(bookInfo: BookInfo, isCompleteReading: boolean): Promise<Response> {
     let response = new Response('書誌データの更新に失敗しました。', {status: 400});
+    const mongoDBModel = bookInfo.convertToMongoDBModel();
 
     try{
       //読み終わっている場合のみcompleteDateを更新対象にする
@@ -117,25 +119,25 @@ export class BookInfoMongoDB implements IBookInfoRepositories {
             updateDate: true,
             completeDate: true
           }
-        } as UpdateFilter<BookInfo>
+        } as UpdateFilter<MongoDBModel>
       } else {
         updateFilter = {
           $currentDate: {
             updateDate: true
           }
-        } as UpdateFilter<BookInfo>
+        } as UpdateFilter<MongoDBModel>
       }
   
       //(日付以外は)以下の項目のみ更新
       updateFilter.$set = {
-        isFavorite: bookInfo.isFavorite,
-        pageCount: bookInfo.pageCount,
-        status: bookInfo.status,
-        pageHistory: bookInfo.pageHistory,
-        memorandum: bookInfo.memorandum
+        isFavorite: mongoDBModel.isFavorite,
+        pageCount: mongoDBModel.pageCount,
+        status: mongoDBModel.status,
+        pageHistory: mongoDBModel.pageHistory,
+        memorandum: mongoDBModel.memorandum
       }
   
-      const result = await this._collection.updateOne({_id: new ObjectId(bookInfo._id)}, updateFilter);
+      const result = await this._collection.updateOne({_id: new ObjectId(mongoDBModel._id)}, updateFilter);
   
       if (result?.matchedCount === 0){
         return response;
@@ -156,7 +158,7 @@ export class BookInfoMongoDB implements IBookInfoRepositories {
     let response = new Response('書誌データの削除に失敗しました。', {status: 400});
 
     try {
-      const result = await this._collection.deleteOne({_id: new ObjectId(bookInfo._id)});
+      const result = await this._collection.deleteOne({_id: new ObjectId(bookInfo.convertToMongoDBModel()._id)});
       if (result && result.deletedCount){
         response = new Response('書誌データの削除に成功しました。', {status: 202} );
       } 
@@ -176,8 +178,8 @@ export class BookInfoMongoDB implements IBookInfoRepositories {
     let isDuplicate = false;
 
     try {
-      const bookInfos = await this._collection.find({userId: this._userId, gapiId: keyId}).toArray() as BookInfo[];
-      isDuplicate = bookInfos.length === 0 ? false : true;
+      const mongoDBModel = await this._collection.find({userId: this._userId, gapiId: keyId}).toArray() as MongoDBModel[];
+      isDuplicate = mongoDBModel.length === 0 ? false : true;
     }
     catch (error) {
       console.log(error);
