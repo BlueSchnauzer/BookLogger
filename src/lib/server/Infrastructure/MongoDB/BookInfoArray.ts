d662edae2edb361575d38fc54ff0023c2f559db7
BlueSchnauzer <kbrucke08@gmail.com>
2024-03-29 20:35:09 +0900

    Add interface methods

--- src/lib/server/Infrastructure/MongoDB/BookInfoArray.ts
+++ src/lib/server/Infrastructure/MongoDB/BookInfoArray.ts
@@ -1,5 +1,16 @@
-import { IBookInfoArray } from "$lib/server/Domain/repositories/BookInfoArray";
+import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
+import type { Id } from "$lib/server/Domain/ValueObjects/BookInfo/Id";
+import type { IBookInfoArray } from "$lib/server/Domain/repositories/BookInfoArray";
 
 export class BookInfoMongoDBArray implements IBookInfoArray {
+  applyChange(detail: { message: string; updatedItem: BookInfo; deletedId: Id; }, isBooksRoute: boolean): BookInfo[] {
+    throw new Error("Method not implemented.");
+  }
+  toggleFavorite(): BookInfo[] {
+    throw new Error("Method not implemented.");
+  }
+  handleSuccess(detail: { message: string; updatedItem: BookInfo; deletedId: Id; }, target: string, isBooksRoute: boolean): BookInfo[] {
+    throw new Error("Method not implemented.");
+  }
 
 }
