6d6168092fa6bcbf163106fdc835b2178f153435
BlueSchnauzer <kbrucke08@gmail.com>
2024-03-30 21:12:08 +0900

    Add bookinfo as argument

--- src/lib/server/Domain/repositories/BookInfoArray.ts
+++ src/lib/server/Domain/repositories/BookInfoArray.ts
@@ -4,9 +4,9 @@ import type { Id } from "$lib/server/Domain/ValueObjects/BookInfo/Id";
 /**BookInfoの配列を操作するリポジトリ */
 export interface IBookInfoArray {
   /**編集内容を反映した書誌データを返す(再レンダリングに使用) */
-  applyChange(detail: {message: string, updatedItem: BookInfo, deletedId: Id}, isBooksRoute: boolean): BookInfo[]; 
+  applyChange(bookInfos: BookInfo[], detail: {message: string, updatedItem: BookInfo, deletedId: Id}, isBooksRoute: boolean): BookInfo[]; 
   /**お気に入り状態の書誌データのみを表示状態にして返す(再レンダリングに使用) */
-  toggleFavorite(): BookInfo[];
+  toggleFavorite(bookInfos: BookInfo[]): BookInfo[];
   /**成功用トーストを表示し、編集内容を反映した書誌データを返す(再レンダリングに使用) */
-  handleSuccess(detail: {message: string, updatedItem: BookInfo, deletedId: Id}, target: string, isBooksRoute: boolean): BookInfo[];
+  handleSuccess(bookInfos: BookInfo[], detail: {message: string, updatedItem: BookInfo, deletedId: Id}, target: string, isBooksRoute: boolean): BookInfo[];
 }
