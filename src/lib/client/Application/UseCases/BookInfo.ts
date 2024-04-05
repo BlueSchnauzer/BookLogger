0302e4f3d94f9e7e52f9fa25c55519314da96477
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-05 23:44:56 +0900

    Remove api request repos and re-implement as usecase

--- src/lib/client/Application/UseCases/BookInfo.ts
+++ src/lib/client/Application/UseCases/BookInfo.ts
@@ -1,10 +1,11 @@
 import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
-import type { IBookInfoAPIRequestRepository } from "$lib/server/Domain/repositories/BookInfoAPIRequest";
+import type { Id } from "$lib/server/Domain/ValueObjects/BookInfo/Id";
 import { error } from "@sveltejs/kit";
 import type { books_v1 } from "googleapis";
 
-export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
-  async get(): Promise<BookInfo[]> {
+export class BookInfoUseCase {
+  /**登録済みの全書誌データ取得する */
+  public async getAll(): Promise<BookInfo[]> {
     const response = await fetch('/api/bookinfo');
     if (!response.ok) {
       //throw error(response.status);
@@ -14,7 +15,8 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
     return await response.json() as BookInfo[];
   }
 
-  async getWish(): Promise<BookInfo[]> {
+  /**読みたい本ステータスの書誌データを取得する */
+  public async getWish(): Promise<BookInfo[]> {
     const response = await fetch('/api/bookinfo/wish');
     if (!response.ok) {
       return [];
@@ -23,7 +25,8 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
     return await response.json() as BookInfo[];
   }
 
-  async getReading(): Promise<BookInfo[]> {
+  /**読んでいる本ステータスの書誌データを取得する */
+  public async getReading(): Promise<BookInfo[]> {
     const response = await fetch('/api/bookinfo/reading');
     if (!response.ok) {
       return [];
@@ -32,7 +35,8 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
     return await response.json() as BookInfo[];
   }
 
-  async getComplete(): Promise<BookInfo[]> {
+  /**読み終わった本ステータスの書誌データを取得する */
+  public async getComplete(): Promise<BookInfo[]> {
     const response = await fetch('/api/bookinfo/complete');
     if (!response.ok) {
       return [];
@@ -41,7 +45,8 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
     return await response.json() as BookInfo[];
   }
 
-  async getRecent(): Promise<BookInfo | undefined> {
+  /**直近で読んだ書誌データを取得する */
+  public async getRecent(): Promise<BookInfo | undefined> {
     const bookInfo = await fetch('/api/bookinfo?recentbook=true');
     if (!bookInfo.ok) {
       return undefined;
@@ -50,7 +55,8 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
     await bookInfo.json() as BookInfo;
   }
 
-  async getHistory(): Promise<Map<string, number> | undefined> {
+  /**1週間に読んだページ数を取得する */
+  public async getHistory(): Promise<Map<string, number> | undefined> {
     const history = await fetch('/api/bookinfo?history=true');
     if (!history.ok) {
       throw error(history.status);
@@ -60,7 +66,7 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
   }
 
   /**書誌データの配列から、文字列の日付と、ページ数を持つmapを作成する */
-  getPageCountInCurrentWeek(bookInfos: BookInfo[]): Map<string, number> {
+  private getPageCountInCurrentWeek(bookInfos: BookInfo[]): Map<string, number> {
     const today = new Date();
     const pageMap = new Map<string, number>();
     for (let i = 6; i >= 0; i--) {
@@ -103,7 +109,8 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
     return pageMap;
   }
 
-  async post(postDate: books_v1.Schema$Volumes): Promise<{ isSuccess: boolean, message: string }> {
+  /**書誌データを保存する */
+  public async post(postDate: books_v1.Schema$Volumes): Promise<{ isSuccess: boolean, message: string }> {
     const { ok: isSuccess, status } = await fetch('/api/bookinfo', {
       method: 'POST',
       body: JSON.stringify(postDate),
@@ -116,7 +123,8 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
     return { isSuccess, message };
   }
 
-  async put(bookInfo: BookInfo, isComplete: boolean): Promise<{ isSuccess: boolean, message: string }> {
+  /**書誌データを更新する */
+  public async put(bookInfo: BookInfo, isComplete: boolean): Promise<{ isSuccess: boolean, message: string }> {
     // 引数が不正でないか、とか調べる？
     const { ok: isSuccess } = await fetch('/api/bookinfo', {
       method: 'PUT',
@@ -128,10 +136,11 @@ export class BookInfoSvelteAPIRequest implements IBookInfoAPIRequestRepository {
     return { isSuccess, message };
   }
 
-  async delete(bookInfo: BookInfo): Promise<{ isSuccess: boolean, message: string }> {
+  /**書誌データを削除する */
+  public async delete(id: Id): Promise<{ isSuccess: boolean, message: string }> {
     const { ok: isSuccess } = await fetch('/api/bookinfo', {
       method: 'DELETE',
-      body: JSON.stringify(bookInfo.id),
+      body: JSON.stringify(id),
       headers: { 'Content-type': 'application/json' }
     });
     const message = isSuccess ? '削除しました' : '削除に失敗しました。\<br\>時間をおいて再度登録してください。';
