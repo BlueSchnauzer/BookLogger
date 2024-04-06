e43e1bd41c35ef05c36b75f09c79607a52f690fd
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-06 20:35:59 +0900

    Implement Bookinfo view class

---
+++ src/lib/client/Application/Views/BookInfo.ts
@@ -0,0 +1,48 @@
+import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
+
+/**BookInfoを配列で受け取り、画面表示用に操作するView */
+export class BookInfoView {
+  constructor(public readonly bookInfos: BookInfo[]){}
+
+
+  //↓プロパティとして配列を持たせるとしたら、どうやって実装しよう。
+
+
+  // /**編集内容を反映した書誌データを返す(再レンダリングに使用) */
+  // applyChange(bookInfos: BookInfo[], detail: { message: string; updatedItem: BookInfo; deletedId: Id; }, isBooksRoute: boolean): BookInfo[] {
+  //   let appliedItems = bookInfos;
+
+  //   if (detail.updatedItem) {
+  //     const oldItem = bookInfos.find(item => item.id === detail.updatedItem.id);
+  
+  //     if (!isBooksRoute && oldItem?.status !== detail.updatedItem.status){
+  //       //全データ表示時以外で、ステータスが変わった場合は一覧から削除して、現在の表示から削除する
+  //       appliedItems = bookInfos.filter(item => item.id !== detail.updatedItem.id);
+  //     }
+  //     else {
+  //       //編集したアイテムを一覧に反映する
+  //       const index = bookInfos.findIndex(item => item.id === detail.updatedItem.id);
+  //       appliedItems = [...bookInfos.slice(0, index), detail.updatedItem, ...bookInfos.slice(index + 1)];	
+  //     }
+  //   }
+  //   if (detail.deletedId) {
+  //     //削除したアイテムを一覧からも削除する
+  //     appliedItems = bookInfos.filter(item => item.id !== detail.deletedId);
+  //   }
+     
+  //   return appliedItems;
+  // }
+
+  // /**お気に入り状態の書誌データのみを表示状態にして返す(再レンダリングに使用) */
+  // toggleFavorite(bookInfos: BookInfo[]): BookInfo[] {
+  //   throw new Error("Method not implemented.");
+  // }
+
+  // /**成功用トーストを表示し、編集内容を反映した書誌データを返す(再レンダリングに使用) */
+  // handleSuccess(bookInfos: BookInfo[], detail: { message: string; updatedItem: BookInfo; deletedId: Id; }, target: string, isBooksRoute = false): BookInfo[] {
+  //   const appliedItems = this.applyChange(bookInfos, detail, isBooksRoute);
+  //   //pushSuccessToast(detail.message, target);
+  
+  //   return appliedItems;
+  // }
+}
