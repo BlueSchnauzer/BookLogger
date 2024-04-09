0078193db1363b3b13c2633a105eb19ce3a64993
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-10 01:45:18 +0900

    Change id value type

--- src/lib/server/Domain/ValueObjects/BookInfo/Id.ts
+++ src/lib/server/Domain/ValueObjects/BookInfo/Id.ts
@@ -4,17 +4,21 @@ import type { ObjectId } from "mongodb";
 
 //一旦、StringとObjectID。あまり複数の型を指定できるのは良くない？
 
-export class Id extends ValueObjectsBase<string | ObjectId> {
-  constructor(public value: string | ObjectId) {
+export class Id extends ValueObjectsBase<id> {
+  constructor(public value: id) {
     super(value);
   }
   
-  validate(value: string | ObjectId): void {
+  validate(value: id): void {
     if (!value) { throw new ValidationError('Idが設定されていません'); }
-    if (typeof value !== 'string') { throw new ValidationError('Idの形式が文字列またはObjectIdではありません'); }
+    
+    //ObjectIdの判定方法が分からないので保留
+    // if (typeof value !== 'string' || value instanceof ObjectId) { throw new ValidationError('Idの形式が文字列またはObjectIdではありません'); }
   }
 
-  equalsCore(vo: ValueObjectsBase<string | ObjectId>): boolean {
+  equalsCore(vo: ValueObjectsBase<id>): boolean {
     return this.value === vo.value;
   }
 }
+
+export type id = string | ObjectId;
