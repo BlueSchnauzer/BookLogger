d92d81d6ddce26a4260fbdab2d8b009d650dff0f
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-09 23:51:46 +0900

    Add test datas for ddd

--- src/lib/vitest-setup.ts
+++ src/lib/vitest-setup.ts
@@ -1,11 +1,18 @@
 //マッチ関数を簡略化するために追加
 import 'vitest-dom/extend-expect'
 import type { BookInfo } from '$lib/server/models/BookInfo';
+import { BookInfo as BookInfoEntity } from '$lib/server/Domain/Entities/BookInfo';
+import type MongoDBModel from '$lib/server/Domain/Entities/MongoDBModel/BookInfo';
 import { ObjectId } from 'mongodb'
 
 export const firstId = new ObjectId('651451ed67241f439ce8a1af');
 export const secondId = new ObjectId('651451ed67241f439ce8a1b0');
 export const thirdId = new ObjectId('651451ed67241f439ce8a1b1');
+
+const firstId_test = '651451ed67241f439ce8a1af';
+const secondId_test = '651451ed67241f439ce8a1b0';
+const thirdId_test = '651451ed67241f439ce8a1b1';
+
 
 /**テスト用書誌データ(1件) */
 export const getTestData = () => {
@@ -33,6 +40,33 @@ export const getTestData = () => {
   }
 
   return testData;
+}
+
+export const getEntityTestData = (): BookInfoEntity => {
+  const modelData: MongoDBModel = {
+    _id: firstId_test,
+    userId: 'firstData',
+    isVisible: true,
+    identifier: {
+      isbn_13: '978-4-15-120051-9'
+    },
+    title: 'わたしを離さないで',
+    author: ['イシグロカズオ'],
+    thumbnail: '',
+    createDate: new Date,
+    updateDate: new Date,
+    pageCount: 300,
+    pageHistory: [{
+      id: crypto.randomUUID(),
+      date: new Date,
+      pageCount: 0
+    }],
+    isFavorite: false,
+    status: 'wish',
+    memorandum: 'メモです1'
+  }
+
+  return new BookInfoEntity(modelData);
 }
 
 /**テスト用書誌データ(3件) */
@@ -105,4 +139,75 @@ export const getTestDatas = () => {
   }]
 
   return testDatas;
+}
+
+export const getEntityTestDatas = (): BookInfoEntity[] => {
+  const modelDatas: BookInfo[] = [{
+    _id: firstId_test,
+    userId: 'firstData',
+    isVisible: true,
+    identifier: {
+      isbn_13: '978-4-15-120051-9'
+    },
+    title: 'わたしを離さないで',
+    author: ['イシグロカズオ'],
+    thumbnail: '',
+    createDate: new Date,
+    updateDate: new Date,
+    pageCount: 300,
+    pageHistory: [{
+      id: crypto.randomUUID(),
+      date: new Date,
+      currentPage: 0
+    }],
+    isFavorite: false,
+    status: 'wish',
+    memorandum: 'メモです1'
+  },
+  {
+    _id: secondId_test,
+    userId: 'secondData',
+    isVisible: true,
+    identifier: {
+      isbn_13: '978-4-15-031316-6'
+    },
+    title: 'エピローグ',
+    author: ['円城塔'],
+    thumbnail: '',
+    createDate: new Date,
+    updateDate: new Date,
+    pageCount: -1,
+    pageHistory: [{
+      id: crypto.randomUUID(),
+      date: new Date,
+      currentPage: 0
+    }],
+    isFavorite: false,
+    status: 'wish',
+    memorandum: 'メモです1'
+  },
+  {
+    _id: thirdId_test,
+    userId: 'thirdData',
+    isVisible: true,
+    identifier: {
+      isbn_13: '978-4-16-791019-8'
+    },
+    title: 'プロローグ',
+    author: ['円城塔'],
+    thumbnail: '',
+    createDate: new Date,
+    updateDate: new Date,
+    pageCount: -1,
+    pageHistory: [{
+      id: crypto.randomUUID(),
+      date: new Date,
+      currentPage: 0
+    }],
+    isFavorite: false,
+    status: 'wish',
+    memorandum: 'メモです2'
+  }]
+
+  return modelDatas.map(item => new BookInfoEntity(item as MongoDBModel));
 }
