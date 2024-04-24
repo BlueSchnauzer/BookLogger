2b2dc3909892882ad5897fd9b76d96cf49fd9d8a
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-24 19:49:54 +0900

    Create helper function for api route

---
+++ src/lib/server/Helpers/SvelteAPI.ts
@@ -0,0 +1,21 @@
+import { verifyAndGetUid } from "$lib/server/verification";
+import { UserId } from "$lib/server/Domain/ValueObjects/BookInfo/UserId";
+
+//verifyAndGetUid()はどっかのタイミングで共通関数で置きなおす。
+
+/**クッキー内のidTokenを検証してUserId型にして返す */
+export const verifyAndGetUserId = async (idToken: string) => {
+  let userId;
+
+  try {
+    const token = await verifyAndGetUid(idToken);
+    userId = new UserId(token!);
+  }
+  catch (error) {
+    console.log(error);
+    console.log('UserIdの取得に失敗しました。');
+    return undefined;
+  }
+  
+  return userId;
+}
