296616a12f23164ea01d9d1a212698bb9aee4856
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-25 21:51:59 +0900

    Change argument type and remove isDuplicate method

--- src/lib/server/Domain/repositories/BookInfoEntity.ts
+++ src/lib/server/Domain/repositories/BookInfoEntity.ts
@@ -1,7 +1,8 @@
 import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
-import type { Status } from "$lib/server/Domain/ValueObjects/BookInfo/Status";
-import type { Id } from "$lib/server/Domain/ValueObjects/BookInfo/Id";
+import type { status } from "$lib/server/Domain/ValueObjects/BookInfo/Status";
+import type { id } from "$lib/server/Domain/ValueObjects/BookInfo/Id";
 import type { PageHistory } from "$lib/server/Domain/ValueObjects/BookInfo/PageHistory";
+import type { books_v1 } from "googleapis";
 
 /**書誌データのEntityの取得や更新を扱うリポジトリ
  * 返却する値はEntityもしくはValueObject
@@ -11,17 +12,15 @@ export interface IBookInfoEntityRepository {
   /**書誌データを取得する */
   get(): Promise<BookInfo[]>;
   /**statusが引数と一致した書誌データを取得する */
-  getByStatus(status: Status): Promise<BookInfo[]>;
+  getByStatus(status: status): Promise<BookInfo[]>;
   /**直近で読んだ、書誌データを1件取得する */
   getRecent(): Promise<BookInfo[]>;
   /**書誌データから、pageHistoryのみを取得する */
   getPageHistory(): Promise<PageHistory[]>;
   /**書誌データを保存する */
-  insert(bookInfo: BookInfo): Promise<Response>;
+  insert(item: books_v1.Schema$Volumes): Promise<Response>;
   /**書誌データを更新する */
   update(bookInfo: BookInfo, isCompleteReading: boolean): Promise<Response>;
   /**書誌データを削除する */
-  delete(id: Id): Promise<Response>;
-  /**同様の書誌データが既に保存されているか */
-  isDuplicate(keyId: string): Promise<boolean>;
+  delete(id: id): Promise<Response>;
 }
