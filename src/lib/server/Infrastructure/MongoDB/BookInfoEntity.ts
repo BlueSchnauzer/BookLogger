d7218e4eae53f35539cc12df7cd0a6604382daec
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-26 23:40:05 +0900

    Implement bookinfo infrastructure to fetch svelte api and instantiate bookinfo entity

---
+++ src/lib/server/Infrastructure/MongoDB/BookInfoEntity.ts
@@ -0,0 +1,67 @@
+import type { IBookInfoEntityRepository } from "$lib/server/Domain/repositories/BookInfoEntity";
+import { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
+import type { id } from "$lib/server/Domain/ValueObjects/BookInfo/Id";
+import { PageHistory, type pageHistory } from "$lib/server/Domain/ValueObjects/BookInfo/PageHistory";
+import type { status } from "$lib/server/Domain/ValueObjects/BookInfo/Status";
+import type { UserId } from "$lib/server/Domain/ValueObjects/BookInfo/UserId";
+import type { books_v1 } from "googleapis";
+import type DBModel from "$lib/server/Domain/Entities/MongoDBModel/BookInfo";
+
+export class BookInfoEntity implements IBookInfoEntityRepository {
+  private readonly requestUrl = '/api/bookinfo';
+
+  constructor(private readonly userId: UserId) {} 
+
+  async get(): Promise<BookInfo[]> {
+    const response = await fetch(this.requestUrl);
+    const dbModels = await response.json() as DBModel[];
+
+    return dbModels.map(item => BookInfo.fromDBModel(item));
+  }
+
+  async getByStatus(status: status): Promise<BookInfo[]> {
+    //eg. '/api/bookinfo?wish=true'
+    const response = await fetch(`${this.requestUrl}?${status}=true`);
+    const dbModels = await response.json() as DBModel[];
+
+    return dbModels.map(item => BookInfo.fromDBModel(item));
+  }
+
+  async getRecent(): Promise<BookInfo[]> {
+    const response = await fetch(`${this.requestUrl}?recent=true`);
+    const dbModels = await response.json() as DBModel[];
+
+    return dbModels.map(item => BookInfo.fromDBModel(item));
+  }
+
+  async getPageHistory(): Promise<PageHistory[]> {
+    const response = await fetch(`${this.requestUrl}/history`);
+    const pageHistory = await response.json() as pageHistory[];
+
+    return pageHistory.map(item => new PageHistory(item));
+  }
+  
+  async insert(item: books_v1.Schema$Volumes): Promise<Response> {
+		return await fetch(this.requestUrl, {
+			method: 'POST',
+			body: JSON.stringify(item),
+			headers: {'Content-type': 'application/json'}
+		});
+  }
+
+  async update(bookInfo: BookInfo, isCompleteReading: boolean): Promise<Response> {
+		return await fetch(this.requestUrl, {
+			method: 'PUT',
+			body: JSON.stringify({bookInfo, isCompleteReading}),
+			headers: {'Content-type': 'application/json'}
+		});
+  }
+
+  async delete(id: id): Promise<Response> {
+		return await fetch(this.requestUrl, {
+			method: 'DELETE',
+			body: JSON.stringify(id),
+			headers: {'Content-type': 'application/json'}
+		});
+  }
+}
