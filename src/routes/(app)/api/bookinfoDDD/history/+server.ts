29343f596914501af99aa25711b4523fe84ea616
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-28 21:02:48 +0900

    Add history api route for fetching page history

---
+++ src/routes/(app)/api/bookinfoDDD/history/+server.ts
@@ -0,0 +1,16 @@
+import type { RequestHandler } from "../../bookinfo/$types";
+import { verifyAndGetUid } from "$lib/server/verification";
+import collections from '$lib/server/database/collections';
+import { json } from '@sveltejs/kit';
+import { BookInfoEntity } from "$lib/server/Infrastructure/MongoDB/BookInfoEntity";
+
+export const GET: RequestHandler = async ({ cookies }) => {
+  const userId = await verifyAndGetUid(cookies.get('idToken'));
+    
+  if (!userId) { return json('ログイン情報が不正です', {status: 400}); }
+  if (!collections) { return new Response('サーバーエラー', { status: 500 }); }
+
+  const response = new BookInfoEntity().getPageHistory();
+
+  return json(response, { status: 200 });
+};
