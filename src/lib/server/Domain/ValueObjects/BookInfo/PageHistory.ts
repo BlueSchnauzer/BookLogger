7561264dfb749aafa848c15d4ea2e81f935da206
BlueSchnauzer <kbrucke08@gmail.com>
2024-04-30 22:46:58 +0900

    Fix PageHistory value object to handle string converted from date type and create tests

--- src/lib/server/Domain/ValueObjects/BookInfo/PageHistory.ts
+++ src/lib/server/Domain/ValueObjects/BookInfo/PageHistory.ts
@@ -9,16 +9,34 @@ export class PageHistory extends ValueObjectsBase<pageHistory> {
 
   protected validate(value: pageHistory): void {
     if (!value || (!value.id || !value.date || value.pageCount < 0 )) { throw new ValidationError('PageHistoryの値が設定されていません'); }
-    if (typeof value.id !== 'string' || !(value.date instanceof Date) || typeof value.pageCount !== 'number' ) { throw new ValidationError('PageHistoryの型が不正です'); }
+    if (typeof value.id !== 'string' || !this.validateDate(value.date) || typeof value.pageCount !== 'number' ) { throw new ValidationError('PageHistoryの型が不正です'); }
   }
 
   protected equalsCore(vo: ValueObjectsBase<pageHistory>): boolean {
     return this.value.id === vo.value.id && this.value.date === vo.value.date && this.value.pageCount === vo.value.pageCount;
   }
+
+  private validateDate(date: Date | string): boolean {
+    if (date instanceof Date) {
+      return true;
+    }
+    else if (typeof date === 'string') {
+      const convertedDate = new Date(date);
+      if (!isNaN(convertedDate.getTime())) { return true; }
+    }
+
+    return false;
+  };
+
+  protected convertValue(value: pageHistory): pageHistory {
+    if (typeof value.date === 'string') { value.date = new Date(value.date); }
+    
+    return value;
+  }
 }
 
 export type pageHistory = {
   id: string,
-  date: Date,
+  date: Date | string,
   pageCount: number
 }
--- src/lib/server/Domain/ValueObjects/BookInfo/ValueObjects.test.ts
+++ src/lib/server/Domain/ValueObjects/BookInfo/ValueObjects.test.ts
@@ -1,12 +1,13 @@
 import { describe, expect, it } from "vitest";
 import { UserId } from "$lib/server/Domain/ValueObjects/BookInfo/UserId";
 import { Identifiers } from "$lib/server/Domain/ValueObjects/BookInfo/Identifier";
-import { ValidationError } from "../../Exceptions/ValidationError";
+import { ValidationError } from "$lib/server/Domain/Exceptions/ValidationError";
+import { PageHistory, type pageHistory } from "$lib/server/Domain/ValueObjects/BookInfo/PageHistory";
 
 describe('UserId', () => {
   const data = 'ユーザID';
 
-  it('stringの値を渡すことでその値を持ったUserIdを生成できること', () => {
+  it('対応した型の値を渡すことでその値を持ったValueObjectを生成できること', () => {
     const userId = new UserId(data);
 
     expect(userId).not.toBeUndefined();
@@ -28,11 +29,73 @@ describe('UserId', () => {
   });
 });
 
-describe('Identifiers', () => {
+describe('PageHistory', () => {
+  const data = [
+    {
+      id: crypto.randomUUID(),
+      date: new Date,
+      pageCount: 0
+    }
+  ];
+
+  it('対応した型の値を渡すことでその値を持ったValueObjectを生成できること', () => {
+    const pageHistory = data.map(item => new PageHistory(item));
+
+    expect(pageHistory[0].value.id).toEqual(data[0].id);
+    expect(pageHistory[0].value.date).toEqual(data[0].date);
+    expect(pageHistory[0].value.pageCount).toEqual(data[0].pageCount);
+  });
+
+  it('Falthyな値を渡した際にエラーが発生すること', () => {
+    expect(() => new PageHistory({} as pageHistory)).toThrowError(ValidationError);
+  });
+
+  it('idにstring以外の型を指定した場合にエラーが発生すること', () => {
+    expect(() => new PageHistory({id: 1000, date: new Date(), pageCount: 0} as unknown as pageHistory)).toThrowError(ValidationError);
+  });
+
+  it('dateにDate型もしくはDateを変換したString以外の型を指定した場合にエラーが発生すること', () => {
+    //エラーになること
+    expect(() => new PageHistory({id: 1000, date: 1000, pageCount: 0} as unknown as pageHistory)).toThrowError(ValidationError);
+
+    //日付を文字列に変換したものは、再度Date型に変換してValueObjectを生成できること
+    const date = new Date();
+    const dateString = date.toJSON();
+    let pageHistory: PageHistory;
+
+    expect(() => {
+      pageHistory = new PageHistory({id: crypto.randomUUID(), date: dateString, pageCount: 0})
+    }).not.toThrowError();
+    expect(pageHistory!.value.date).toEqual(date);
+  });
+
+  it('pageCountに0以上の数値もしくはNumber以外の型を指定した場合にエラーが発生すること', () => {
+    expect(() => new PageHistory({id: crypto.randomUUID(), date: new Date(), pageCount: 0} as unknown as pageHistory)).toThrowError(ValidationError);
+  });
+
+  it('equals()でValueObject同士の値を比較できること', () => {
+    const pageHistoryA = data.map(item => new PageHistory(item));
+    const pageHistoryB = data.map(item => new PageHistory(item));
+
+    const data2 = [
+      {
+        id: crypto.randomUUID(),
+        date: new Date,
+        pageCount: 100
+      }
+    ];
+    const pageHistoryC = data2.map(item => new PageHistory(item));
+
+    expect(pageHistoryA[0].equals(pageHistoryB[0])).toBeTruthy();
+    expect(pageHistoryA[0].equals(pageHistoryC[0])).toBeFalsy();
+  });
+});
+
+describe('Identifier', () => {
   const isbn_13 = '978-4-15-120051-9';
   const isbn_10 = '4-15-031316-6';
 
-  it('ISBNの値を渡すことでその値を持ったIdentifiersを生成できること', () => {
+  it('対応した型の値を渡すことでその値を持ったValueObjectを生成できること', () => {
     const identifiers = new Identifiers({isbn_13, isbn_10});
 
     expect(identifiers).not.toBeUndefined();
@@ -58,12 +121,12 @@ describe('Identifiers', () => {
     expect(idWithIsbn10.value.isbn_13).toBeUndefined();
   });
 
-  // it('equals()でUserIdのvalueと引数の値が同一かを判定できること', () => {
-  //   const userIdA = new Identifiers(data);
-  //   const userIdB = new Identifiers(data);
-  //   const userIdC = new Identifiers('test');
+  it('equals()でValueObject同士の値を比較できること', () => {
+    const idWithIsbn13A = new Identifiers({isbn_13});
+    const idWithIsbn13B = new Identifiers({isbn_13});
+    const idWithIsbn10 = new Identifiers({isbn_10});
 
-  //   expect(userIdA.equals(userIdB)).toBeTruthy();
-  //   expect(userIdA.equals(userIdC)).toBeFalsy();
-  // });
+    expect(idWithIsbn13A.equals(idWithIsbn13B)).toBeTruthy();
+    expect(idWithIsbn13A.equals(idWithIsbn10)).toBeFalsy();
+  });
 });
