692910f91d13c0661b6b5c197b2c3d43a00129a8
BlueSchnauzer <kbrucke08@gmail.com>
2024-05-01 20:17:35 +0900

    Implement book info entity get method test

--- src/lib/server/Infrastructure/MongoDB/BookInfoEntity.test.ts
+++ src/lib/server/Infrastructure/MongoDB/BookInfoEntity.test.ts
@@ -1,9 +1,21 @@
-import { describe, it } from "vitest";
-
+import { describe, expect, it, vi } from "vitest";
+import { BookInfoEntity } from "./BookInfoEntity";
+import { getEntityTestData } from "$lib/vitest-setup";
+import { json } from "@sveltejs/kit";
+import BookInfoMongoDBModel from "$lib/server/Domain/Entities/MongoDBModel/BookInfo";
 
 describe('get', () => {
-  it('SvelteAPIへのリクエストが成功した際に、レスポンスをEntityに変換して戻り値で返すこと', () => {
+  it('SvelteAPIへのリクエストが成功した際に、レスポンスをEntityに変換して戻り値で返すこと', async () => {
+    const mockFetch = vi.spyOn(global, 'fetch');
+    const dbModels: BookInfoMongoDBModel[] = [];
+    dbModels.push(new BookInfoMongoDBModel(getEntityTestData()));
+    mockFetch.mockImplementation(async () => json(dbModels, {status: 200}));
 
+    const repos = new BookInfoEntity();
+    const response = await repos.get();
+
+    expect(response.length).toEqual(1);
+    expect(response[0].title).toEqual(dbModels[0].title);
   });
 
   it('エラーになった時に対応した戻り値を返すこと？', () => {
