873f09c73229c3869c0c2a93df815f2d1381c5bb
BlueSchnauzer <kbrucke08@gmail.com>
2024-05-10 22:35:24 +0900

    Rename argument names in book search repos

--- src/lib/server/Domain/repositories/BookSearch.ts
+++ src/lib/server/Domain/repositories/BookSearch.ts
@@ -6,26 +6,26 @@ import type { books_v1 } from "googleapis";
 export interface IBookSearchRepositories{
   /**指定した検索条件で書誌APIにリクエストする
    * @param queries 検索条件
-   * @param resultCount 取得件数
+   * @param maxResults 取得件数
    * @param startIndex データ取得開始位置
    */
-  search(queries: string[], resultCount: number, startIndex: number): Promise<books_v1.Schema$Volumes>;
+  search(queries: string[], maxResults: number, startIndex: number): Promise<books_v1.Schema$Volumes>;
 
   /**指定した検索条件でリクエストし、リソースを限定して取得する */
   searchWithPartialResource(queries: string[], resource?: string): Promise<books_v1.Schema$Volumes>;
 
   /**(あいまい検索)検索条件を指定して書誌データを取得する
    * @param query 検索条件
-   * @param resultCount 取得件数
+   * @param maxResults 取得件数
    * @param startIndex データ取得開始位置
    */
-  searchByFuzzyQuery(query: string, resultCount: number, startIndex: number): Promise<books_v1.Schema$Volumes>;
+  searchByFuzzyQuery(query: string, maxResults: number, startIndex: number): Promise<books_v1.Schema$Volumes>;
 
   /**書名、著者名とISBNのいずれか、または全てを指定して書誌データを取得する
-   * @param resultCount 取得件数
+   * @param maxResults 取得件数
    * @param startIndex データ取得開始位置
    */
-  searchByQueries(booktitle: string, author: string, isbn_13: string, resultCount: number, startIndex: number): Promise<books_v1.Schema$Volumes>;
+  searchByQueries(booktitle: string, author: string, isbn_13: string, maxResults: number, startIndex: number): Promise<books_v1.Schema$Volumes>;
   
   /**ISBNでリクエストして書影データを取得する */
   getThumbnailByIsbn(isbn_13: string): Promise<string>;
