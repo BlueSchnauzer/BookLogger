21aa867cda6a3143c4d99cea0d54872bb0d714d7
BlueSchnauzer <kbrucke08@gmail.com>
2024-05-20 22:14:33 +0900

    Implement book search use case

--- src/lib/client/Application/Views/BookSearch.ts
+++ src/lib/client/Application/Views/BookSearch.ts
@@ -1,2 +1,21 @@
+import type { IBookSearchRepositories } from "$lib/server/Domain/repositories/BookSearch";
+import { BookSearchGoogleBooksAPIUseCase } from "$lib/client/Application/UseCases/BookSearch";
+import type { books_v1 } from "googleapis";
+
 export class BookSearchView {
+  private readonly _usecase: BookSearchGoogleBooksAPIUseCase;
+
+  constructor(private readonly _repos: IBookSearchRepositories) {
+    this._usecase = new BookSearchGoogleBooksAPIUseCase(_repos);
+  }
+
+  /**Usecaseを呼び出してあいまい検索を行う */
+  public async searcyByFuzzyQuery(query: string, maxResults: number, startIndex: number): Promise<books_v1.Schema$Volumes> {
+    return await this._usecase.searcyByFuzzyQuery(query, maxResults, startIndex);
+  }
+
+  /**Usecaseを呼び出して詳細検索を行う */
+  public async searchByQueries(booktitle: string, author: string, isbn_13: string, maxResults: number, startIndex: number): Promise<books_v1.Schema$Volumes> {
+    return await this._usecase.searchByQueries(booktitle, author, isbn_13, maxResults, startIndex);
+  }
 }
