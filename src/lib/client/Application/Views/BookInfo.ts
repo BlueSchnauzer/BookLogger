73baf3bd9843bef91ba32824a7ff23934afcc902
BlueSchnauzer <kbrucke08@gmail.com>
2024-05-21 23:52:42 +0900

    Implement apply change method in book info use case

--- src/lib/client/Application/Views/BookInfo.ts
+++ src/lib/client/Application/Views/BookInfo.ts
@@ -1,37 +1,34 @@
 import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
+import type { Id } from "$lib/server/Domain/ValueObjects/BookInfo/Id";
 
 /**BookInfoを配列で受け取り、画面表示用に操作するView */
 export class BookInfoView {
-  constructor(public readonly bookInfos: BookInfo[]){}
+  constructor(public bookInfos: BookInfo[]){}
 
+  /**編集内容を反映した書誌データを返す(再レンダリングに使用) */
+  public applyChange(bookInfos: BookInfo[], detail: { message: string; updatedItem: BookInfo; deletedId: Id }, isBooksRoute: boolean): void {
+    let appliedItems: BookInfo[] = [];
 
-  //↓プロパティとして配列を持たせるとしたら、どうやって実装しよう。
-
-
-  // /**編集内容を反映した書誌データを返す(再レンダリングに使用) */
-  // applyChange(bookInfos: BookInfo[], detail: { message: string; updatedItem: BookInfo; deletedId: Id; }, isBooksRoute: boolean): BookInfo[] {
-  //   let appliedItems = bookInfos;
-
-  //   if (detail.updatedItem) {
-  //     const oldItem = bookInfos.find(item => item.id === detail.updatedItem.id);
+    if (detail.updatedItem) {
+      const oldItem = bookInfos.find(item => item.id === detail.updatedItem.id);
   
-  //     if (!isBooksRoute && oldItem?.status !== detail.updatedItem.status){
-  //       //全データ表示時以外で、ステータスが変わった場合は一覧から削除して、現在の表示から削除する
-  //       appliedItems = bookInfos.filter(item => item.id !== detail.updatedItem.id);
-  //     }
-  //     else {
-  //       //編集したアイテムを一覧に反映する
-  //       const index = bookInfos.findIndex(item => item.id === detail.updatedItem.id);
-  //       appliedItems = [...bookInfos.slice(0, index), detail.updatedItem, ...bookInfos.slice(index + 1)];	
-  //     }
-  //   }
-  //   if (detail.deletedId) {
-  //     //削除したアイテムを一覧からも削除する
-  //     appliedItems = bookInfos.filter(item => item.id !== detail.deletedId);
-  //   }
+      if (!isBooksRoute && oldItem?.status !== detail.updatedItem.status){
+        //全データ表示時以外で、ステータスが変わった場合は一覧から削除して、現在の表示から削除する
+        appliedItems = bookInfos.filter(item => item.id !== detail.updatedItem.id);
+      }
+      else {
+        //編集したアイテムを一覧に反映する
+        const index = bookInfos.findIndex(item => item.id === detail.updatedItem.id);
+        appliedItems = [...bookInfos.slice(0, index), detail.updatedItem, ...bookInfos.slice(index + 1)];	
+      }
+    }
+    if (detail.deletedId) {
+      //削除したアイテムを一覧からも削除する
+      appliedItems = bookInfos.filter(item => item.id !== detail.deletedId);
+    }
      
-  //   return appliedItems;
-  // }
+    this.bookInfos = appliedItems;
+  }
 
   // /**お気に入り状態の書誌データのみを表示状態にして返す(再レンダリングに使用) */
   // toggleFavorite(bookInfos: BookInfo[]): BookInfo[] {
