d7c0b331c8a5e77aa1fefcbf3bb51f03a8c1ed65
BlueSchnauzer <kbrucke08@gmail.com>
2024-05-23 20:16:47 +0900

    Create utils folder in application and add toast file

---
+++ src/lib/client/Application/Utils/toast.ts
@@ -0,0 +1,24 @@
+import { toast } from "@zerodevx/svelte-toast";
+
+//トーストは利用先でコンテナのインポート、スタイル適用とアンマウント時のpopが必要
+
+/**画面右上にトーストを表示する*/
+export const pushToast = (message: string, target: string): void => {
+	toast.push(message, { theme: { '--toastBarBackground': '#65a30d' }, target });
+}
+
+/**画面下部に正常完了用トーストを表示する*/
+export const pushSuccessToast = (message: string, target: string): void => {
+	toast.push(message, { reversed: true, intro: { y: 100 },
+		theme: { '--toastBarBackground': '#65a30d' }, 
+		target 
+	});
+}
+
+/**画面下部に異常完了用トーストを表示する*/
+export const pushErrorToast = (message: string, target: string): void => {
+	toast.push(message, { reversed: true, intro: { y: 100 }, 
+		theme:{ '--toastBarHeight': 0, '--toastWidth': 'auto', '--toastBackground': '#dc2626' }, 
+		target 
+	});
+}
