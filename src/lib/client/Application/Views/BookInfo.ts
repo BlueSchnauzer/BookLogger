1875eda564365e593697d67a648adcba009c835e
BlueSchnauzer <kbrucke08@gmail.com>
2024-05-25 21:34:27 +0900

    Add bookinfo view for handling a bookinfo object

---
+++ src/lib/client/Application/Views/BookInfo.ts
@@ -0,0 +1,51 @@
+import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
+
+/**単一のBookInfoを受け取り、画面表示用に操作するView */
+export class BookInfoView {
+  constructor(public bookInfo: BookInfo) { }
+
+  /**書誌データを保存する。 */
+  public insertBookInfo() {
+
+  }
+  
+  /**書誌データを更新する。 */
+  public updateBookInfo() {
+
+  }
+
+  /**書誌データを削除する。 */
+  public deleteBookInfo() {
+
+  }
+
+  /**pageHisotryを追加する。 */
+  public addPageHistory() {
+
+  }
+
+  /**StatusがCompleteに変更された際に、最終ページまでの記録が無ければ追加する。 */
+  public changeToComplete() {
+
+  }
+
+  /**最終ページのpageHistoryがあるかを確認する。 */
+  private hasCompleteHistory() {
+
+  }
+
+  /**タイトルを取得する(存在しなければ「データ無し」を返す) */
+  public getTitleLabel() {
+    return this.bookInfo.title ?? 'データ無し';
+  }
+
+  /**書誌データの日付を画面表示用の形式に変換する。 */
+  public getDateLabel() {
+
+  }
+
+  /**グリッドアイテムのラベル表示用のタイプを判定して返す。 */
+  public getTypeForBottomLabel() {
+
+  }
+}
