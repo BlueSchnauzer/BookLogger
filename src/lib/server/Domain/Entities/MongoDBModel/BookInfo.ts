5eadcf7447c90ac6e11ad50ecd2a5eaedf676281
BlueSchnauzer <kbrucke08@gmail.com>
2024-06-05 21:06:32 +0900

    Revert constructor for bookinfo db model

--- src/lib/server/Domain/Entities/MongoDBModel/BookInfo.ts
+++ src/lib/server/Domain/Entities/MongoDBModel/BookInfo.ts
@@ -28,27 +28,47 @@ export default class BookInfoMongoDBModel {
 	public shelfCategories?: ObjectId[]
 	public gapiId?: string;
 
+	constructor(volume: books_v1.Schema$Volume, userId: string);
 	constructor(entity: BookInfoEntity);
 
 	/**GAPIのデータもしくは書誌情報のEntityを用いて初期化する
 	 * GAPIのデータを使うので汎用的にはなっていないけど
 	 */
-	constructor(resource: BookInfoEntity) {
-		this.userId = resource.userId.value;
-		this.title = resource.title;
-		this.author = resource.author;
-		this.thumbnail = resource.thumbnail;
-		this.createDate = resource.createDate;
-		this.updateDate = resource.updateDate;
-		this.pageCount = resource.pageCount;
-		this.status = resource.status.value;
-		this.isFavorite = resource.isFavorite;
-		this.memorandum = resource.memorandum;
-		this.isVisible = resource.isVisible;
-		this._id = resource.id ? new ObjectId(resource.id.value) : undefined;
-		this.pageHistories = resource.pageHistories ? resource.pageHistories?.map(item => item.value) : [];
-		this.identifiers = resource.identifiers?.value;
-		this.gapiId = resource.gapiId;
+	constructor(resource?: books_v1.Schema$Volume | BookInfoEntity, userId?: string) {
+		if (resource instanceof BookInfoEntity) {
+			this.userId = resource.userId.value;
+			this.title = resource.title;
+			this.author = resource.author;
+			this.thumbnail = resource.thumbnail;
+			this.createDate = resource.createDate;
+			this.updateDate = resource.updateDate;
+			this.pageCount = resource.pageCount;
+			this.status = resource.status.value;
+			this.isFavorite = resource.isFavorite;
+			this.memorandum = resource.memorandum;
+			this.isVisible = resource.isVisible;
+			this._id = resource.id ? new ObjectId(resource.id.value) : undefined;
+			this.pageHistories = resource.pageHistories ? resource.pageHistories?.map(item => item.value) : [];
+			this.identifiers = resource.identifiers?.value;
+			this.gapiId = resource.gapiId;
+		}
+		else {
+			const currentDate = new Date;
+
+			this.userId = userId!;
+			this.title = resource!.volumeInfo?.title ?? '';
+			this.author = resource!.volumeInfo?.authors ?? [''];
+			this.thumbnail = resource!.volumeInfo?.imageLinks?.thumbnail ?? ''; //gapi固有の情報だが、画像そのものではなく場所を表すURLを保存する。
+			this.createDate = currentDate;
+			this.updateDate = currentDate;
+			this.pageCount = resource!.volumeInfo?.pageCount ?? 0;
+			this.status = 'wish';
+			this.isFavorite = false;
+			this.memorandum = '';
+			this.isVisible = true;
+			this.identifiers = getIdentifier(resource!.volumeInfo?.industryIdentifiers);
+			this.gapiId = resource!.id ?? this.title; //gapi固有の情報なので入れたら微妙な感じではある
+		}
 	}
 }
 
