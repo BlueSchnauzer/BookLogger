c3c0a37064041e7f8a7989287c943db9558e2b3f
BlueSchnauzer <kbrucke08@gmail.com>
2024-06-09 20:04:45 +0900

    Add functions for display in book search view

--- src/lib/client/Application/Views/BookSearch.ts
+++ src/lib/client/Application/Views/BookSearch.ts
@@ -5,7 +5,28 @@ import type { books_v1 } from "googleapis";
 export class BookSearchView {
   private readonly _usecase: BookSearchGoogleBooksAPIUseCase;
 
-  constructor(private readonly repos: IBookSearchRepositories) {
+  constructor(public booksVolume: books_v1.Schema$Volume, private readonly repos: IBookSearchRepositories) {
     this._usecase = new BookSearchGoogleBooksAPIUseCase(repos);
-  }  
+  }
+
+  /**タイトルを取得する(存在しなければ「データ無し」を返す) */
+  public getTitleLabel() {
+    return this.booksVolume.volumeInfo?.title ?? 'データ無し';
+  }
+
+  /**著者が複数名いる場合に句点で区切る*/
+	public joinAuthorNames = (): string => {
+    let authors = this.booksVolume.volumeInfo?.authors;
+		if (!authors) { return '';	}
+
+		let result: string;
+		//多すぎる場合は短縮
+		if (authors.length >= 6) {
+			result = `${authors.slice(0, 5).join(', ')}...`;
+		} else {
+			result = authors.join(', ');
+		}
+
+		return result;
+	};
 }
