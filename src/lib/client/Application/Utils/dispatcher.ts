96de3cdb8005db302d460b1a9860240adfb5ace0
BlueSchnauzer <kbrucke08@gmail.com>
2024-06-16 20:14:29 +0900

    Add book info dispatch handler

--- src/lib/client/Application/Utils/dispatcher.ts
+++ src/lib/client/Application/Utils/dispatcher.ts
@@ -1,4 +1,14 @@
+import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
+import type { ObjectId } from "mongodb";
 import type { EventDispatcher } from "svelte";
+
+const handleBookInfoRequest = (dispatch: EventDispatcher<any>, isSuccess: boolean, message: string, updatedItem?: BookInfo, deletedId?: ObjectId) => {
+  if (isSuccess){
+    dispatch('success', {message, updatedItem, deletedId});
+  } else {
+    dispatch('failed', message);
+  }
+};
 
 const handleBookSearchRequest = (dispatch: EventDispatcher<any>, isSuccess: boolean, message: string) => {
   if (isSuccess){
