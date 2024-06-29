7c78d6b288cd2ba3df4ffab459cecdaddf2e1ba4
BlueSchnauzer <kbrucke08@gmail.com>
2024-06-29 20:41:45 +0900

    Fix setCurrentDate

--- src/lib/server/Domain/Entities/BookInfo.ts
+++ src/lib/server/Domain/Entities/BookInfo.ts
@@ -191,9 +191,10 @@ export type bookInfoProperties = {
 	gapiId?: string
 }
 
-const date = new Date();
-const setCurrentDate = () =>
-	`${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
+const setCurrentDate = () => {
+	const date = new Date();
+	return `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`
+}
 
 const isBookInfoProperties = (obj: any): obj is bookInfoProperties => {
 	return 'userId' in obj;
