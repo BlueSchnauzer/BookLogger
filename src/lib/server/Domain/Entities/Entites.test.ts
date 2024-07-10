cc0bacbd0b6b6766ab2c286535857ef8ec3fcfed
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-10 20:22:13 +0900

    Implement getMaxPageCount test method

--- src/lib/server/Domain/Entities/Entites.test.ts
+++ src/lib/server/Domain/Entities/Entites.test.ts
@@ -87,12 +87,20 @@ describe('BookInfoEntity', () => {
   });
 
   describe('getMaxPageCount', () => {
-    it('PageHistories内の最大のページ数を取得できること', () => {
+    it('PageHistories内の最大のページ数を取得できること', () => { 
+      const entity = new BookInfo(bookInfoPropertiesMock);
 
+      expect(entity.getMaxPageCount()).toEqual(bookInfoPropertiesMock.pageHistories![1].pageCount);
     });
 
-    it('PageHistoriesがFalthyな場合にUndefinedが返ること', () => {
+    it('PageHistoriesがFalthyな場合にUndefinedが返ること', () => { 
+      const copiedMock: bookInfoProperties = {
+        ...bookInfoPropertiesMock,
+        pageHistories: undefined
+      };
+      const entity = new BookInfo(copiedMock);
 
+      expect(entity.getMaxPageCount()).not.toBeDefined();
     });
   });
 });
