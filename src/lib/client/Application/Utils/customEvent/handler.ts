113cc943b0eb341ef7afd0715f70cc0a9b0e7fd5
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-14 19:36:47 +0900

    Add custom event handler for bookInfo entity

---
+++ src/lib/client/Application/Utils/customEvent/handler.ts
@@ -0,0 +1,41 @@
+import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
+import type { bookInfoDispatchParameter } from "$lib/client/Application/Utils/customEvent/dispatcher";
+import { pushSuccessToast, pushErrorToast } from "$lib/client/Application/Utils/toast";
+import { BookInfoView } from "$lib/client/Application/Views/BookInfo";
+
+export const handleSuccess = (bookInfoViews: BookInfoView[], detail: bookInfoDispatchParameter, target: string, isBooksRoute = false) => {
+  const appliedItems = applyChange(bookInfoViews, detail, isBooksRoute);
+  pushSuccessToast(detail.message, target);
+
+  return appliedItems;
+}
+
+export const handleFailure = (message: string, target: string) => {
+  pushErrorToast(message, target);
+}
+
+const applyChange = (bookInfoViews: BookInfoView[], detail: bookInfoDispatchParameter, isBooksRoute: boolean) => {
+  //一旦、viewの中身を取り出して新しく操作する、上手くいくか確認
+  let bookInfos = bookInfoViews.map(item => item.bookInfo);
+  let appliedItems: BookInfo[] = [];
+
+  if (detail.updatedItem) {
+    const oldItem = bookInfos.find(item => item.id?.equals(detail.updatedItem?.id!));
+
+    if (!isBooksRoute && !oldItem?.status.equals(detail.updatedItem.status)) {
+      //全データ表示時以外で、ステータスが変わった場合は一覧から削除して、現在の表示から削除する
+      appliedItems = bookInfos.filter(item => item.id?.equals(detail.updatedItem?.id!));
+    }
+    else {
+      //編集したアイテムを一覧に反映する
+      const index = bookInfos.findIndex(item => item.id?.equals(detail.updatedItem?.id!));
+      appliedItems = [...bookInfos.slice(0, index), detail.updatedItem, ...bookInfos.slice(index + 1)];
+    }
+  }
+  if (detail.deletedId) {
+    //削除したアイテムを一覧からも削除する
+    appliedItems = bookInfos.filter(item => item.id?.equals(detail.deletedId!));
+  }
+
+  return appliedItems.map(item => new BookInfoView(item));
+}
