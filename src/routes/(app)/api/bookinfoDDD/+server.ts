cabbc4477fcf0f9a4466a2617bf2330249efa77b
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-16 19:51:47 +0900

    use validation proccess before put method

--- src/routes/(app)/api/bookinfoDDD/+server.ts
+++ src/routes/(app)/api/bookinfoDDD/+server.ts
@@ -6,6 +6,7 @@ import DBModel from '$lib/server/Domain/Entities/MongoDBModel/BookInfo';
 import { BookInfoMongoDBResource } from '$lib/server/Infrastructure/MongoDB/BookInfoDBResource';
 import type { books_v1 } from 'googleapis';
 import type { BookInfo } from '$lib/server/Domain/Entities/BookInfo';
+import { validatePutBookInfo, validateReadingCount } from '$lib/client/Application/Utils/validation';
 
 /**書誌データを取得する
  * クエリパラメータに応じて返却するデータを変更する。
@@ -59,8 +60,7 @@ export const PUT: RequestHandler = async ({ request, cookies }) => {
   const item = await request.json() as { bookInfo: BookInfo, isCompleteReading: boolean };
   const repos = new BookInfoMongoDBResource(collections.bookInfos!, userId);
 
-  //作り直してから呼ぶ
-  //if (!validatePutItem(item)) { return new Response('データが不正です', { status: 400}); }
+  if (!validatePutBookInfo(item)) { return new Response('データが不正です', { status: 400}); }
   return await repos.update(new DBModel(item.bookInfo), item.isCompleteReading);
 }
 
@@ -77,33 +77,3 @@ export const DELETE: RequestHandler = async ({ request, cookies }) => {
 
   return await repos.delete(id);
 };
-
-//どっかに作り直す
-/**更新用データが不正でないか確認する */
-const validatePutItem = ({ bookInfo, isComplete }: { bookInfo: BookInfo, isComplete: boolean }): boolean => {
-  let result = true;
-
-  //作成直後はhistoryが空なのでそのままtrue、編集してある場合は中身が不正でないか調べる。
-  if (!bookInfo.getPageHistories()) { return result; }
-  bookInfo.getPageHistories()?.forEach(item => {
-    if (!result) { return; }
-
-    if (!item.value.date) {
-      result = false;
-      return;
-    }
-    //汎用処理なのでどっかに作り直してから呼ぶ
-    // if (!validateReadingCount(item.value.pageCount, bookInfo.pageCount)) {
-    //     result = false;
-    //     return;
-    // }
-  });
-
-  //読み終わっている場合、最終ページの記録があるか確認
-  if (isComplete && result) {
-    const isExist = bookInfo.getPageHistories()?.findIndex(item => item.value.pageCount === bookInfo.getPageCount());
-    result = isExist !== -1 ? true : false;
-  }
-
-  return result;
-}
