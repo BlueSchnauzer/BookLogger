0a7ac602edec87eb704dd5187a25ec775798dd8b
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-16 19:51:15 +0900

    add validation function for put server route

--- src/lib/client/Application/Utils/validation.ts
+++ src/lib/client/Application/Utils/validation.ts
@@ -1,3 +1,5 @@
+import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
+
 /**日付(読んだ記録)チェック */
 export const validateReadingDate = (readingDate: string): boolean => {
 	if (!readingDate) { return false; }
@@ -12,4 +14,31 @@ export const validateReadingCount = (readingCount: number, pageCount: number): b
 	}
 
 	return true;
+}
+
+export const validatePutBookInfo = ({ bookInfo, isCompleteReading }: { bookInfo: BookInfo, isCompleteReading: boolean }): boolean => {
+  let result = true;
+
+  //作成直後はhistoryが空なのでそのままtrue、編集してある場合は中身が不正でないか調べる。
+  if (!bookInfo.getPageHistories()) { return result; }
+  bookInfo.getPageHistories()?.forEach(item => {
+    if (!result) { return; }
+
+    if (!item.value.date) {
+      result = false;
+      return;
+    }
+    if (!validateReadingCount(item.value.pageCount, bookInfo.getPageCount())) {
+        result = false;
+        return;
+    }
+  });
+
+  //読み終わっている場合、最終ページの記録があるか確認
+  if (isCompleteReading && result) {
+    const isExist = bookInfo.getPageHistories()?.findIndex(item => item.value.pageCount === bookInfo.getPageCount());
+    result = isExist !== -1 ? true : false;
+  }
+
+  return result;
 }
