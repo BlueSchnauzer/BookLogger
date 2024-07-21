c0ce3a10999be6c0aba6da4e93809122b87b6d5b
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-22 01:15:33 +0900

    Separate event dispatchers and handlers to use generics event type

--- src/lib/client/Utils/customEvent/dispatcher.ts
+++ src/lib/client/Utils/customEvent/dispatcher.ts
@@ -2,33 +2,46 @@ import type { BookInfoView } from "$lib/client/Application/Views/BookInfo";
 import type { Id } from "$lib/server/Domain/ValueObjects/BookInfo/Id";
 import { createEventDispatcher } from "svelte";
 
-export interface bookInfoDispatchParameter {
+export interface bookInfoEvent<T> {
+  success: T,
+  failed: string
+}
+
+export interface updateBookInfoParameter {
   message: string,
-  updatedItem?: BookInfoView,
-  deletedId?: Id
+  updatedItem: BookInfoView
 }
 
-export interface bookInfoEvents {
-  success: bookInfoDispatchParameter;
-  failed: string;
+export interface deletionBookInfoParameter {
+  message: string,
+  deletedId: Id
 }
 
-export interface bookSearchEvents {
-  success: string;
-  failed: string;
-}
-
-export const dispatchBookInfoRequest = (isSuccess: boolean, message: string, updatedItem?: BookInfoView, deletedId?: Id) => {
-  const dispatch = createEventDispatcher<bookInfoEvents>();
+export const dispatchUpdateBookInfoRequest = (isSuccess: boolean, message: string, updatedItem: BookInfoView) => {
+  const dispatch = createEventDispatcher<bookInfoEvent<updateBookInfoParameter>>();
   if (isSuccess) {
-    dispatch('success', { message, updatedItem, deletedId });
+    dispatch('success', { message, updatedItem });
   } else {
     dispatch('failed', message);
   }
 };
 
+export const dispatchDeletionBookInfoRequest = (isSuccess: boolean, message: string, deletedId: Id) => {
+  const dispatch = createEventDispatcher<bookInfoEvent<deletionBookInfoParameter>>();
+  if (isSuccess) {
+    dispatch('success', { message, deletedId });
+  } else {
+    dispatch('failed', message);
+  }
+};
+
+export interface bookSearchEvent {
+  success: string,
+  failed: string
+}
+
 export const dispatchBookSearchRequest = (isSuccess: boolean, message: string) => {
-  const dispatch = createEventDispatcher<bookSearchEvents>();
+  const dispatch = createEventDispatcher<bookSearchEvent>();
   if (isSuccess) {
     dispatch('success', message);
   } else {
--- src/lib/client/Utils/customEvent/handler.ts
+++ src/lib/client/Utils/customEvent/handler.ts
@@ -1,42 +1,66 @@
-import type { BookInfo } from "$lib/server/Domain/Entities/BookInfo";
-import type { bookInfoDispatchParameter } from "$lib/client/Utils/customEvent/dispatcher";
+import type { bookInfoEvent, updateBookInfoParameter, deletionBookInfoParameter } from "$lib/client/Utils/customEvent/dispatcher";
 import { pushSuccessToast, pushErrorToast } from "$lib/client/Utils/toast";
 import { BookInfoView } from "$lib/client/Application/Views/BookInfo";
 
-export const handleSuccess = (bookInfoViews: BookInfoView[], detail: bookInfoDispatchParameter, target: string, isBooksRoute = false) => {
-  const appliedItems = applyChange(bookInfoViews, detail, isBooksRoute);
-  pushSuccessToast(detail.message, target);
-
-  return appliedItems;
-}
-
-export const handleFailure = (message: string, target: string) => {
-  pushErrorToast(message, target);
-}
-
-const applyChange = (bookInfoViews: BookInfoView[], detail: bookInfoDispatchParameter, isBooksRoute: boolean) => {
+export const handleUpdateSuccess = (bookInfoViews: BookInfoView[], paramter: updateBookInfoParameter, targetToast: string, isBooksRoute = false) => {
   //一旦、viewの中身を取り出して新しく操作する、上手くいくか確認
   let copiedViews = [...bookInfoViews];
   let appliedViews: BookInfoView[] = [];
+  const oldItem = copiedViews.find(item => item.id?.equals(paramter.updatedItem.id!));
 
-  //updatedItemがBookInfoViewなので、上手く動くか確認
-  if (detail.updatedItem) {
-    const oldItem = copiedViews.find(item => item.id?.equals(detail.updatedItem?.id!));
+  if (!isBooksRoute && !oldItem?.status.equals(paramter.updatedItem.status)) {
+    //全データ表示時以外で、ステータスが変わった場合は一覧から削除して、現在の表示から削除する
+    appliedViews = copiedViews.filter(item => item.id?.equals(paramter.updatedItem.id!));
+  }
+  else {
+    //編集したアイテムを一覧に反映する
+    const index = copiedViews.findIndex(item => item.id?.equals(paramter.updatedItem.id!));
+    appliedViews = [...copiedViews.slice(0, index), paramter.updatedItem, ...copiedViews.slice(index + 1)];
+  }
 
-    if (!isBooksRoute && !oldItem?.status.equals(detail.updatedItem.status)) {
-      //全データ表示時以外で、ステータスが変わった場合は一覧から削除して、現在の表示から削除する
-      appliedViews = copiedViews.filter(item => item.id?.equals(detail.updatedItem?.id!));
-    }
-    else {
-      //編集したアイテムを一覧に反映する
-      const index = copiedViews.findIndex(item => item.id?.equals(detail.updatedItem?.id!));
-      appliedViews = [...copiedViews.slice(0, index), detail.updatedItem, ...copiedViews.slice(index + 1)];
-    }
-  }
-  if (detail.deletedId) {
-    //削除したアイテムを一覧からも削除する
-    appliedViews = copiedViews.filter(item => item.id?.equals(detail.deletedId!));
-  }
+  pushSuccessToast(paramter.message, targetToast);
 
   return appliedViews;
 }
+
+export const handleDeletionSuccess = (bookInfoViews: BookInfoView[], parameter: deletionBookInfoParameter, targetToast: string) => {
+  //一旦、viewの中身を取り出して新しく操作する、上手くいくか確認
+  let copiedViews = [...bookInfoViews];
+  let appliedViews: BookInfoView[] = [];
+  appliedViews = copiedViews.filter(item => item.id?.equals(parameter.deletedId));
+
+  pushSuccessToast(parameter.message, targetToast);
+
+  return appliedViews;
+}
+
+export const handleFailure = (message: string, targetToast: string) => {
+  pushErrorToast(message, targetToast);
+}
+
+// const applyChange = (bookInfoViews: BookInfoView[], paramter: updateBookInfoParameter | deletionBookInfoParameter, isBooksRoute: boolean) => {
+//   //一旦、viewの中身を取り出して新しく操作する、上手くいくか確認
+//   let copiedViews = [...bookInfoViews];
+//   let appliedViews: BookInfoView[] = [];
+
+//   //updatedItemがBookInfoViewなので、上手く動くか確認
+//   if () {
+//     const oldItem = copiedViews.find(item => item.id?.equals(paramter.updatedItem?.id!));
+
+//     if (!isBooksRoute && !oldItem?.status.equals(paramter.updatedItem.status)) {
+//       //全データ表示時以外で、ステータスが変わった場合は一覧から削除して、現在の表示から削除する
+//       appliedViews = copiedViews.filter(item => item.id?.equals(paramter.updatedItem?.id!));
+//     }
+//     else {
+//       //編集したアイテムを一覧に反映する
+//       const index = copiedViews.findIndex(item => item.id?.equals(paramter.updatedItem?.id!));
+//       appliedViews = [...copiedViews.slice(0, index), paramter.updatedItem, ...copiedViews.slice(index + 1)];
+//     }
+//   }
+//   if (paramter.deletedId) {
+//     //削除したアイテムを一覧からも削除する
+//     appliedViews = copiedViews.filter(item => item.id?.equals(paramter.deletedId!));
+//   }
+
+//   return appliedViews;
+// }
