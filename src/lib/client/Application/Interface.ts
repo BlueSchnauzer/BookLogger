0e3c9eb92a6c2f767e1fa65df4c161784fc31e83
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-29 23:52:42 +0900

    Add properties and constructor

--- src/lib/client/Application/Interface.ts
+++ src/lib/client/Application/Interface.ts
@@ -2,6 +2,10 @@ import type { books_v1 } from 'googleapis';
 
 export type BookSearchResultType = books_v1.Schema$Volume;
 
+export const isGAPIResult = (obj: any): obj is books_v1.Schema$Volume => {
+	return 'volumeInfo' in obj;
+};
+
 export interface bookInfoChangeResponse {
 	isSuccess: boolean;
 	message: string;
--- src/lib/client/Application/Views/BookSearch.ts
+++ src/lib/client/Application/Views/BookSearch.ts
@@ -1,7 +1,25 @@
-import type { BookSearchResultType } from "$lib/client/Application/Interface";
+import { isGAPIResult, type BookSearchResultType } from '$lib/client/Application/Interface';
 
 export class BookSearchView<T extends BookSearchResultType> {
-	constructor(public readonly searchResult: T) { }
+	public readonly title?: string;
+	public readonly authors?: string[];
+	public readonly publisher?: string;
+	public readonly publishDate?: string;
+	public readonly pageCount?: number;
+	public readonly thumbnail?: string;
+	public readonly description?: string;
+
+	constructor(public readonly searchResult: T) {
+		if (isGAPIResult(searchResult)) {
+			this.title = searchResult.volumeInfo?.title;
+			this.authors = searchResult.volumeInfo?.authors;
+			this.publisher = searchResult.volumeInfo?.publisher;
+			this.publishDate = searchResult.volumeInfo?.publishedDate;
+			this.pageCount = searchResult.volumeInfo?.pageCount;
+			this.thumbnail = searchResult.volumeInfo?.imageLinks?.thumbnail;
+			this.description = searchResult.volumeInfo?.description;
+		}
+	}
 
 	/**タイトルを取得する(存在しなければ「データ無し」を返す) */
 	public getTitleLabel() {
@@ -9,9 +27,11 @@ export class BookSearchView<T extends BookSearchResultType> {
 	}
 
 	/**著者が複数名いる場合に句点で区切る*/
-	public joinAuthorNames = (): string => {
+	public getJoinedAuthorNames = (): string => {
 		let authors = this.searchResult.volumeInfo?.authors;
-		if (!authors) { return ''; }
+		if (!authors) {
+			return '';
+		}
 
 		let result: string;
 		//多すぎる場合は短縮
@@ -23,4 +43,8 @@ export class BookSearchView<T extends BookSearchResultType> {
 
 		return result;
 	};
+
+	public getPageCountLabel() {
+		return `${this.pageCount}ページ`;
+	}
 }
