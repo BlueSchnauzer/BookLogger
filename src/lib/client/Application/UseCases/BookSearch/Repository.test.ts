68b06ba78eb1de3f27d04b7bf9e4224e0ecdbb9a
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-29 13:01:48 +0900

    Create book search usecase tests

---
+++ src/lib/client/Application/UseCases/BookSearch/Repository.test.ts
@@ -0,0 +1,32 @@
+import type { IBookSearchRepository } from '$lib/client/Domain/Repositories/IBookSearch';
+import { BookSearchGoogleBooksAPI } from '$lib/client/Infrastructure/GoogleBooksAPI/BookSearch';
+import { getEntityTestData } from '$lib/mock/Data';
+import type { books_v1 } from 'googleapis';
+import { describe, expect, it } from 'vitest';
+import { BookSearchUseCase } from './Repository';
+
+describe('BookSearchUseCase', () => {
+	const testData = getEntityTestData();
+	//ひとまずGAPIでの実装を利用
+	const repos: IBookSearchRepository<books_v1.Schema$Volumes> = new BookSearchGoogleBooksAPI();
+
+	it('searcyByFuzzyQuery', async () => {
+		const usecase = new BookSearchUseCase(repos);
+		const view = await usecase.searcyByFuzzyQuery('イシグロカズオ');
+
+		expect(view).toBeDefined();
+		expect(view.searchResult).toBeDefined();
+	});
+
+	it('searchByQueries', async () => {
+		const usecase = new BookSearchUseCase(repos);
+		const view = await usecase.searchByQueries(
+			testData.title,
+			testData.author[0],
+			testData.identifiers?.value.isbn_13!
+		);
+
+		expect(view).toBeDefined();
+		expect(view.searchResult).toBeDefined();
+	});
+});
