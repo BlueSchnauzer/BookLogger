9e6aaed08cee2b7be22e3bc3a4afaa0590374b0c
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-30 23:26:27 +0900

    Add toast target name variable

--- src/lib/client/Utils/Toast.ts
+++ src/lib/client/Utils/Toast.ts
@@ -1,26 +1,29 @@
-import { toast } from "@zerodevx/svelte-toast";
+import { toast } from '@zerodevx/svelte-toast';
 
 //トーストは利用先でコンテナのインポート、スタイル適用とアンマウント時のpopが必要
+export const toastTargetName = 'toastTarget';
 
 /**画面右上にトーストを表示する*/
-export const pushToast = (message: string, targetToast: string): void => {
-	toast.push(message, { theme: { '--toastBarBackground': '#65a30d' }, target: targetToast });
-}
+export const pushToast = (message: string): void => {
+	toast.push(message, { theme: { '--toastBarBackground': '#65a30d' }, target: toastTargetName });
+};
 
 /**画面下部に正常完了用トーストを表示する*/
-export const pushSuccessToast = (message: string, targetToast: string): void => {
+export const pushSuccessToast = (message: string): void => {
 	toast.push(message, {
-		reversed: true, intro: { y: 100 },
+		reversed: true,
+		intro: { y: 100 },
 		theme: { '--toastBarBackground': '#65a30d' },
-		target: targetToast
+		target: toastTargetName
 	});
-}
+};
 
 /**画面下部に異常完了用トーストを表示する*/
-export const pushErrorToast = (message: string, targetToast: string): void => {
+export const pushErrorToast = (message: string): void => {
 	toast.push(message, {
-		reversed: true, intro: { y: 100 },
+		reversed: true,
+		intro: { y: 100 },
 		theme: { '--toastBarHeight': 0, '--toastWidth': 'auto', '--toastBackground': '#dc2626' },
-		target: targetToast
+		target: toastTargetName
 	});
-}
+};
