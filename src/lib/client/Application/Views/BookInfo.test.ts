749a82ab9e536c52f70d6a231918c4f3e7a66ad7
BlueSchnauzer <kbrucke08@gmail.com>
2024-07-31 22:21:25 +0900

    implement rest tests

--- src/lib/client/Application/Views/BookInfo.test.ts
+++ src/lib/client/Application/Views/BookInfo.test.ts
@@ -1,13 +1,13 @@
-import { BookInfo, type bookInfoProperties } from '$lib/client/Domain/Entities/BookInfo';
+import { BookInfo } from '$lib/client/Domain/Entities/BookInfo';
+import { Status, type status } from '$lib/client/Domain/ValueObjects/BookInfo/Status';
 import { bookInfoPropertiesMock, getEntityTestData } from '$lib/mock/Data';
 import { describe, expect, it } from 'vitest';
 import { BookInfoView } from './BookInfo';
 
 describe('BookInfoView', () => {
-	const entity = getEntityTestData();
-
 	describe('initialize', () => {
 		it('properties', () => {
+			const entity = getEntityTestData();
 			const view = new BookInfoView(entity);
 			const handleExpect = (viewValue: any, entityValue: any) => {
 				expect(viewValue).toBe(entityValue);
@@ -33,7 +33,8 @@ describe('BookInfoView', () => {
 	});
 
 	describe('titleLabel', () => {
-		it('get', () => {
+		it('get label', () => {
+			const entity = getEntityTestData();
 			const view = new BookInfoView(entity);
 			const handleExpect = (view: BookInfoView, expectValue: string) => {
 				expect(view.titleLabel).toBe(expectValue);
@@ -48,7 +49,8 @@ describe('BookInfoView', () => {
 	});
 
 	describe('pageCountLabel', () => {
-		it('get', () => {
+		it('get label', () => {
+			const entity = getEntityTestData();
 			const view = new BookInfoView(entity);
 			const handleExpect = (view: BookInfoView, expectValue: string) => {
 				expect(view.pageCountLabel).toBe(expectValue);
@@ -63,6 +65,7 @@ describe('BookInfoView', () => {
 	});
 
 	describe('isDisplayableProgress', () => {
+		const entity = getEntityTestData();
 		const handleExpect = (view: BookInfoView, expectValue: boolean) => {
 			expect(view.isDisplayableProgress).toBe(expectValue);
 		};
@@ -73,14 +76,14 @@ describe('BookInfoView', () => {
 			handleExpect(view, true);
 		});
 
-		it('pageCount', () => {
+		it('falthy pageCount', () => {
 			const view = new BookInfoView(entity);
 
 			view.pageCount = 0;
 			handleExpect(view, false);
 		});
 
-		it('pageHistory length', () => {
+		it('falthy pageHistory length', () => {
 			const view = new BookInfoView(entity);
 
 			view.pageHistories = undefined;
@@ -91,7 +94,8 @@ describe('BookInfoView', () => {
 	});
 
 	describe('progressByPercent', () => {
-		it('get', () => {
+		it('get percent', () => {
+			const entity = getEntityTestData();
 			const view = new BookInfoView(entity);
 
 			expect(view.progressByPercent).toBe('3%');
@@ -99,12 +103,13 @@ describe('BookInfoView', () => {
 	});
 
 	describe('getDateLabel', () => {
-		it('get', () => {
+		it('get label by type', () => {
 			const handleExpect = (
 				dateType: 'create' | 'update' | 'complete',
 				useYear: boolean,
 				expectValue: RegExp
 			) => {
+				const entity = getEntityTestData();
 				const view = new BookInfoView(entity);
 				expect(view.getDateLabel(dateType, useYear)).toMatch(expectValue);
 			};
@@ -113,6 +118,85 @@ describe('BookInfoView', () => {
 			handleExpect('update', true, /^\d{4}\/\d{1,2}\/\d{1,2}$/);
 			handleExpect('complete', true, /データ無し/);
 			handleExpect('create', false, /^\d{1,2}\/\d{1,2}$/);
+		});
+	});
+
+	describe('getTypeForBottomLabel', () => {
+		it('get label by type', () => {
+			const handleExpect = (pathName: string, expectValue: string) => {
+				const entity = getEntityTestData();
+				const view = new BookInfoView(entity);
+				expect(view.getTypeForBottomLabel(pathName)).toBe(expectValue);
+			};
+
+			handleExpect('/home', 'progress');
+			handleExpect('/books', 'createDate');
+			handleExpect('/books/wish', 'createDate');
+			handleExpect('/books/reading', 'progress');
+			handleExpect('', 'completeDate');
+		});
+	});
+
+	describe('addPageHistory', () => {
+		const entity = getEntityTestData();
+		it('add', () => {
+			const view = new BookInfoView(entity);
+			const result = view.addPageHistory('2023-10-30', 100);
+
+			expect(result.isSuccess).toBeTruthy();
+			expect(view.pageHistories?.length).toBe(3);
+		});
+
+		it('Falthyな日付文字列とページ数の場合にエラーステータスとメッセージが返ること', () => {
+			const view = new BookInfoView(entity);
+			let result = view.addPageHistory('', 100);
+			expect(result.isSuccess).toBeFalsy();
+			expect(result.errorMessage).toBe('日付が未入力です');
+
+			result = view.addPageHistory('2023-10-30', 0);
+			expect(result.isSuccess).toBeFalsy();
+			expect(result.errorMessage).toBe(`ページ数は1～${view.pageCount}ページで入力してください`);
+		});
+
+		it('読んだ記録が0件の状態で追加された際に、ステータスがReadingに変わること', () => {
+			const noPageHistoryEntity = new BookInfo({ ...bookInfoPropertiesMock, pageHistories: [] });
+			const view = new BookInfoView(noPageHistoryEntity);
+			const result = view.addPageHistory('2023-10-30', 100);
+
+			expect(result.isSuccess).toBeTruthy();
+			expect(view.status.value).toBe<status>('reading');
+		});
+
+		it('読んだ記録が0件の状態で追加された際に、ステータスがReadingに変わること', () => {
+			const view = new BookInfoView(entity);
+			const result = view.addPageHistory('2023-10-30', view.pageCount);
+
+			expect(result.isSuccess).toBeTruthy();
+			expect(view.status.value).toBe<status>('complete');
+		});
+	});
+
+	describe('deletePageHistory', () => {
+		it('delete', () => {
+			const entity = getEntityTestData();
+			const view = new BookInfoView(entity);
+			const idToDelete = view.pageHistories![0].value.id!;
+			view.deletePageHistory(idToDelete);
+
+			expect(view.pageHistories?.length).toBe(1);
+		});
+	});
+
+	describe('addPageHistoryWhenComplete', () => {
+		it('add', () => {
+			const entity = getEntityTestData();
+			const view = new BookInfoView(entity);
+			view.status = new Status('complete');
+			view.addPageHistoryWhenComplete();
+
+			expect(view.pageHistories?.length).toBe(3);
+			expect(view.pageHistories![2].value.pageCount).toBe(view.pageCount);
+			expect(view.status.value).toBe<status>('complete');
 		});
 	});
 });
