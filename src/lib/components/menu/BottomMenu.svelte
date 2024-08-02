da8c5198724cde333e9e3756445070bd23625361
BlueSchnauzer <kbrucke08@gmail.com>
2024-08-02 22:59:48 +0900

    use static menu item array in main menu components

--- src/lib/components/menu/BottomMenu.svelte
+++ src/lib/components/menu/BottomMenu.svelte
@@ -1,19 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import type { menuItemData } from '$lib/customTypes';
-	import Home from '$lib/icons/Home.svelte';
-	import BookCase from '$lib/icons/BookCase.svelte';
-	import BookShelf from '$lib/icons/BookShelf.svelte';
-	import MagnifingGlass from '$lib/icons/MagnifingGlass.svelte';
-
-	//モバイルメニュー(画面サイズが小さい時にサイドメニューと入れ替えで表示)
-	const MenuItemDatas: menuItemData[] = [
-		{ icon: Home, ref: '/home', name: 'ホーム' },
-		{ icon: BookCase, ref: '/books', name: 'ライブラリ' },
-		{ icon: MagnifingGlass, ref: '/books/search', name: '書籍検索' },
-		{ icon: BookShelf, ref: '/shelf', name: '本棚' }
-	];
-	const colorStone200 = '#E7E5E4';
+	import { mainMenuItems } from '$lib/client/UI/Shared/DisplayData';
+	import { colorStone200 } from '$lib/client/UI/Shared/StaticValues';
 
 	//ページ移動の度に対応したページにスタイルを当てる
 	let pathName: string;
@@ -22,7 +10,7 @@
 
 <nav class="hidden max-md:flex w-full fixed bottom-0 h-16 bg-stone-700">
 	<ul class="flex flex-1">
-		{#each MenuItemDatas as data (data.icon)}
+		{#each mainMenuItems as data (data.icon)}
 			<li
 				class="flex flex-1 border-b-4 border-transparent
 				{data.ref === pathName ? 'border-y-lime-600 bg-stone-600' : ''}"
