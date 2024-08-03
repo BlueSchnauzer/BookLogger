be752bc1b1c53f870e27d0d93a6ee4d383893267
BlueSchnauzer <kbrucke08@gmail.com>
2024-08-03 15:33:34 +0900

    Add search conditions interface

--- src/lib/client/Utils/types.ts
+++ src/lib/client/Utils/types.ts
@@ -1,1 +1,8 @@
 export type searchType = 'none' | 'fuzzy' | 'detail';
+
+export interface searchConditions {
+	query: string | null;
+	bookTitle: string | null;
+	author: string | null;
+	isbn: string | null;
+}
