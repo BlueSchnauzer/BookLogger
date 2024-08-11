706ca77ffba44b294acaa50f8da9f3d8b1eb4502
BlueSchnauzer <kbrucke08@gmail.com>
2024-08-11 20:09:01 +0900

    change book info click event param

--- src/lib/client/Helpers/CustomEvent/Dispatcher.ts
+++ src/lib/client/Helpers/CustomEvent/Dispatcher.ts
@@ -1,18 +1,20 @@
-import type { BookSearchResponseItem } from '$lib/client/Application/Interface';
+import type {
+	BookInfoResponseItem,
+	BookSearchResponseItem
+} from '$lib/client/Application/Interface';
 import type { BookInfo } from '$lib/client/Domain/Entities/BookInfo';
-import type { BookSearch } from '$lib/client/Domain/Entities/BookSearch';
 import type { Id } from '$lib/client/Domain/ValueObjects/BookInfo/Id';
 import type { EventDispatcher } from 'svelte';
 
 export interface bookInfoClickEvent {
-	click: BookInfo;
+	click: BookInfoResponseItem;
 }
 
 export const dispatchBookInfoClick = (
 	dispatch: EventDispatcher<bookInfoClickEvent>,
-	entity: BookInfo
+	item: BookInfoResponseItem
 ) => {
-	dispatch('click', entity);
+	dispatch('click', item);
 };
 
 export interface bookInfoUpdateEvent {
