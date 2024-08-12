0606e196a3e0a304d08db68423828d6d6b1c3d6c
BlueSchnauzer <kbrucke08@gmail.com>
2024-08-12 14:53:04 +0900

    get book info data in books route page load

--- src/routes/(app)/books/+page.ts
+++ src/routes/(app)/books/+page.ts
@@ -1,12 +1,9 @@
-import { BookInfoUseCase } from '$lib/client/Application/UseCases/BookInfo';
-import { BookInfoEntityResource } from '$lib/client/Infrastructure/MongoDB/BookInfoEntityResource';
+import { getBookInfoUseCase } from '$lib/client/Application/UseCases/BookInfo';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-	const repos = new BookInfoEntityResource(fetch);
-	const usecase = new BookInfoUseCase(repos);
-	const views = await usecase.get();
-	console.log('bookinfo views', views);
+	const usecase = getBookInfoUseCase(fetch);
+	const result = await usecase.get();
 
-	return { views };
+	return { items: result.items };
 }) satisfies PageLoad;
