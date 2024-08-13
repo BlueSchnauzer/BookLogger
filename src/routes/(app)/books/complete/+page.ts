db65c2177fef46e3cd0736ed4c64b7bf5532a6d2
BlueSchnauzer <kbrucke08@gmail.com>
2024-08-13 17:55:36 +0900

    Remove log

--- src/routes/(app)/books/complete/+page.ts
+++ src/routes/(app)/books/complete/+page.ts
@@ -4,7 +4,6 @@ import type { PageLoad } from './$types';
 export const load = (async ({ fetch }) => {
 	const usecase = getCompleteBookInfoUseCase(fetch);
 	const result = await usecase.get();
-	console.log('result', result);
 
 	return { items: result.items };
 }) satisfies PageLoad;
