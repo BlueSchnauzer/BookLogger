6d76d9a7d30f4f2894d91d64de1748884e0ee4ed
BlueSchnauzer <kbrucke08@gmail.com>
2024-08-24 10:14:24 +0900

    Fix Category Label functions

--- src/lib/client/UI/Shared/Components/CategoryLabel.svelte
+++ src/lib/client/UI/Shared/Components/CategoryLabel.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	export let categoryText: string;
-	export let condition: any;
-	export let labelFunction: () => string | undefined;
+	export let displayText: string | undefined;
 </script>
 
 <div class="mb-2 flex flex-col justify-start items-stretch">
 	<span class="mb-1 text-lime-800">{categoryText}</span>
-	{#if condition}
-		<span class="mb-2 border-b-stone-400 border-b-[1px]">{labelFunction()}</span>
+	{#if displayText}
+		<span class="mb-2 border-b-stone-400 border-b-[1px]">{displayText}</span>
 	{:else}
 		<span class="mb-2 text-gray-500 border-b-stone-400 border-b-[1px]">データ無し</span>
 	{/if}
