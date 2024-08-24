9a9977c0755041f3568d98a27c82d98db23f5e04
BlueSchnauzer <kbrucke08@gmail.com>
2024-08-24 17:00:19 +0900

    Separate input component for search conditions

---
+++ src/lib/client/UI/Search/ConditionModal/ConditionInput.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let value: string;
+	export let name: string;
+	export let disabled: boolean;
+	export let formError: boolean;
+
+	/**インプットタグでのEnterを無効化*/
+	const preventSubmit = (e: KeyboardEvent) => {
+		formError = false;
+		if (e.key === 'Enter') {
+			e.preventDefault();
+		}
+	};
+</script>
+
+<input
+	class="px-2 py-1 rounded-lg border border-stone-300"
+	bind:value
+	{name}
+	aria-label={name}
+	type="text"
+	size="30"
+	on:keypress={(e) => preventSubmit(e)}
+	{disabled}
+/>
