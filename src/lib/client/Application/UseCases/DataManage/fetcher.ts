383609f05f1424b297980fa97a69f2e912bda325
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-07 09:06:49 +0900

    Add data manage functions for book info

---
+++ src/lib/client/Application/UseCases/DataManage/fetcher.ts
@@ -0,0 +1,50 @@
+import { convertDBModelToBookInfo, type BookInfo } from '$lib/client/Domain/Entities/BookInfo';
+import type { status } from '$lib/client/Domain/ValueObjects/BookInfo/Status';
+import type { BookInfoDBModel } from '$lib/server/Domain/Entities/MongoDB/BookInfoModel';
+import {
+	PageHistory,
+	type pageHistory
+} from '$lib/client/Domain/ValueObjects/BookInfo/PageHistory';
+import { getPageHistoryMapInCurrentWeek } from '$lib/client/Utils/PageHistory';
+import { bookInfoAPIRoute } from '$lib/client/Shared/Constants/requestUrls';
+import type { FetchInterface } from '$lib/client/Application/UseCases/DataManage/interface';
+
+export const getBookInfos = async (fetch: FetchInterface): Promise<BookInfo[]> => {
+	const response = await fetch(bookInfoAPIRoute);
+	const models = (await response.json()) as BookInfoDBModel[];
+
+	return models.map((item) => convertDBModelToBookInfo(item));
+};
+
+export const getBookInfosByStatus = async (
+	fetch: FetchInterface,
+	status: status
+): Promise<BookInfo[]> => {
+	const response = await fetch(`${bookInfoAPIRoute}?type=${status}`);
+	const models = (await response.json()) as BookInfoDBModel[];
+
+	return models.map((item) => convertDBModelToBookInfo(item));
+};
+
+export const getRecentBookInfo = async (fetch: FetchInterface): Promise<BookInfo | undefined> => {
+	const response = await fetch(`${bookInfoAPIRoute}?type=recent`);
+	const model = (await response.json()) as BookInfoDBModel;
+
+	if (!model) {
+		return undefined;
+	}
+
+	return convertDBModelToBookInfo(model);
+};
+
+export const getHistory = async (
+	fetch: FetchInterface
+): Promise<Map<string, number> | undefined> => {
+	const response = await fetch(`${bookInfoAPIRoute}/history`);
+	const pageHistory = (await response.json()) as Array<pageHistory[]>;
+	const ValueObjects = pageHistory.map((item) =>
+		item.map((pageHistory) => new PageHistory(pageHistory))
+	);
+
+	return getPageHistoryMapInCurrentWeek(ValueObjects);
+};
