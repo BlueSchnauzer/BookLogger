ab33461ce0baafcc454df5df37dd5bdb338498a6
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-11 00:04:59 +0900

    Add data view functions

---
+++ src/lib/client/Feature/Search/DataView/dataView.test.ts
@@ -0,0 +1,49 @@
+import { getTitleLabel } from '$lib/client/Application/Views/BookInfo';
+import { describe, it, expect } from 'vitest';
+import {
+	joinUpToFiveAuthorNames,
+	joinAuthorNames,
+	getPageCountLabel
+} from '$lib/client/Feature/Search/DataView/dataView';
+
+describe('titleLabel', () => {
+	it('get label', () => {
+		expect(getTitleLabel('title')).toBe('title');
+		expect(getTitleLabel('')).toBe('データ無し');
+		expect(getTitleLabel()).toBe('データ無し');
+	});
+});
+
+describe('joinAuthorNames', () => {
+	it('get names', () => {
+		const author = ['author'];
+		const twoAuthors = ['authorA', 'authorB'];
+		const sixAuthors = [...twoAuthors, 'authorC', 'authorD', 'authorE', 'authorF'];
+		expect(joinUpToFiveAuthorNames(author)).toBe('author');
+		expect(joinUpToFiveAuthorNames(twoAuthors)).toBe('authorA, authorB');
+		expect(joinUpToFiveAuthorNames(sixAuthors)).toBe(
+			'authorA, authorB, authorC, authorD, authorE...'
+		);
+	});
+});
+
+describe('joinUpToFiveAuthorNames', () => {
+	it('get names', () => {
+		const author = ['author'];
+		const twoAuthors = ['authorA', 'authorB'];
+		const sixAuthors = [...twoAuthors, 'authorC', 'authorD', 'authorE', 'authorF'];
+		expect(joinAuthorNames(author)).toBe('author');
+		expect(joinAuthorNames(twoAuthors)).toBe('authorA, authorB');
+		expect(joinAuthorNames(sixAuthors)).toBe(
+			'authorA, authorB, authorC, authorD, authorE, authorF'
+		);
+	});
+});
+
+describe('pageCountLabel', () => {
+	it('get label', () => {
+		expect(getPageCountLabel(300)).toBe(`300ページ`);
+		expect(getPageCountLabel(0)).toBe('0ページ');
+		expect(getPageCountLabel()).toBe('0ページ');
+	});
+});
