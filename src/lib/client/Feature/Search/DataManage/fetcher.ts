5ed22cbef556cb60dde2c68d6dc2798b682f3b8c
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-10 22:16:47 +0900

    separate book search functions from book search usecases

---
+++ src/lib/client/Feature/Search/DataManage/fetcher.ts
@@ -0,0 +1,47 @@
+import {
+	convertResponseToBookSearch,
+	type BookSearchResultListType,
+	type BookSearchResultType
+} from '$lib/client/Domain/Entities/BookSearch';
+import {
+	requestByFuzzyQuery,
+	requestByQueries
+} from '$lib/client/Feature/Search/API/GoogleBooksAPI';
+
+export const searchByFuzzyQuery = async <
+	ResultListType extends BookSearchResultListType,
+	ResultType extends BookSearchResultType<ResultListType>
+>(
+	query: string,
+	maxResults = 10,
+	startIndex = 0
+) => {
+	const response = await requestByFuzzyQuery(query, maxResults, startIndex);
+
+	const totalCount = response.totalItems ?? 0;
+	const items = response.items?.map((item) =>
+		convertResponseToBookSearch<ResultType>(item as ResultType)
+	);
+
+	return { totalCount, items };
+};
+
+export const searchByQueries = async <
+	ResultListType extends BookSearchResultListType,
+	ResultType extends BookSearchResultType<ResultListType>
+>(
+	booktitle: string,
+	author: string,
+	isbn_13: string,
+	maxResults = 10,
+	startIndex = 0
+) => {
+	const response = await requestByQueries(booktitle, author, isbn_13, maxResults, startIndex);
+
+	const totalCount = response.totalItems ?? 0;
+	const items = response.items?.map((item) =>
+		convertResponseToBookSearch<ResultType>(item as ResultType)
+	);
+
+	return { totalCount, items };
+};
