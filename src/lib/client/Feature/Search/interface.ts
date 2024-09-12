04df4fac5facc2f9b3e739855e6478bd2ece2a6a
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-12 22:55:07 +0900

    fix search promise return value def

--- src/lib/client/Feature/Search/interface.ts
+++ src/lib/client/Feature/Search/interface.ts
@@ -1,3 +1,3 @@
 import type { BookSearch } from '$lib/client/Domain/Entities/BookSearch';
 
-export type SearchPromise = () => Promise<{ totalCount: number; items: BookSearch }>;
+export type SearchPromise = () => Promise<{ totalCount: number; items: BookSearch | undefined }>;
