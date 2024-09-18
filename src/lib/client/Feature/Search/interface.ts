aed651edb54d424c79791a33faf4ca21908d87c1
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-18 21:02:12 +0900

    Replace ui interface to feature file

--- src/lib/client/Feature/Search/interface.ts
+++ src/lib/client/Feature/Search/interface.ts
@@ -1,3 +1,19 @@
 import type { BookSearch } from '$lib/client/Feature/Search/BookSearch';
 
-export type SearchPromise = () => Promise<{ totalCount: number; items: BookSearch | undefined }>;
+export type SearchPromise = () => Promise<{ totalCount: number; items: BookSearch[] | undefined }>;
+
+export type SearchType = 'none' | 'fuzzy' | 'detail';
+
+export interface SearchConditions {
+	query: string | null;
+	bookTitle: string | null;
+	author: string | null;
+	isbn: string | null;
+}
+
+export interface SearchProps {
+	searchType: SearchType;
+	searchConditions: SearchConditions;
+	pageCount: number;
+	startIndex: number;
+}
