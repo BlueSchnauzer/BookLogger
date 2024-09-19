772eaa7407b4d06d07da93a55e84f122168d6856
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-19 20:50:52 +0900

    Fix tests

--- src/lib/client/UI/Search/ItemModal/ItemDetail.test.ts
+++ src/lib/client/UI/Search/ItemModal/ItemDetail.test.ts
@@ -1,13 +1,13 @@
 import ItemDetail from '$lib/client/UI/Search/ItemModal/ItemDetail.svelte';
-import { gapiItemAndBookSearchResponseItemMock, gapiTestDatas } from '$lib/mock/Data';
+import { bookSearchInterfaceMock } from '$lib/mock/Data';
 import { render, screen, waitFor } from '@testing-library/svelte';
 import { describe, expect, it } from 'vitest';
 
 describe('ResultDetail', async () => {
-	const { item, bookSearchResponseItem } = gapiItemAndBookSearchResponseItemMock();
+	const bookSearch = bookSearchInterfaceMock;
 
 	it('レンダリング', async () => {
-		render(ItemDetail, { bookSearch: bookSearchResponseItem });
+		render(ItemDetail, { bookSearch });
 
 		await waitFor(
 			() => {
@@ -15,11 +15,11 @@ describe('ResultDetail', async () => {
 			},
 			{ timeout: 5000 }
 		);
-		expect(screen.getByText(item.volumeInfo?.title!)).toBeInTheDocument();
-		expect(screen.getByText(item.volumeInfo?.authors?.join(',')!)).toBeInTheDocument();
+		expect(screen.getByText(bookSearch.title!)).toBeInTheDocument();
+		expect(screen.getByText(bookSearch.authors?.join(',')!)).toBeInTheDocument();
 		//expect(screen.getByText('データ無し')).toBeInTheDocument(); //データが無い場合はテキストを変更する
-		expect(screen.getByText(item.volumeInfo?.publishedDate!)).toBeInTheDocument();
-		expect(screen.getByText(`${item.volumeInfo?.pageCount!}ページ`)).toBeInTheDocument();
-		expect(screen.getByText(item.volumeInfo?.description!)).toBeInTheDocument();
+		expect(screen.getByText(bookSearch.publishedDate!)).toBeInTheDocument();
+		expect(screen.getByText(`${bookSearch.pageCount!}ページ`)).toBeInTheDocument();
+		expect(screen.getByText(bookSearch.description!)).toBeInTheDocument();
 	});
 });
--- src/lib/client/UI/Search/ItemModal/ItemModal.test.ts
+++ src/lib/client/UI/Search/ItemModal/ItemModal.test.ts
@@ -1,23 +1,28 @@
-import type { BookSearchResponseItem } from '$lib/client/Application/Interface';
-import { bookSearchView } from '$lib/client/Application/Views/BookSearch';
-import { convertResponseToBookSearch } from '$lib/client/Feature/Search/BookSearch';
 import ItemModal from '$lib/client/UI/Search/ItemModal/ItemModal.svelte';
-import { gapiTestDatas } from '$lib/mock/Data';
-import { fireEvent, render, screen, waitFor } from '@testing-library/svelte';
-import { afterEach, describe, expect, it, vi, vitest } from 'vitest';
+import { bookSearchInterfaceMock } from '$lib/mock/Data';
+import { fireEvent, render, screen } from '@testing-library/svelte';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 
 //dialogタグの関数がJSDomだとサポートされていない？ためスキップ
 describe.skip('ContentModal', async () => {
-	const entity = convertResponseToBookSearch(gapiTestDatas.items![0]);
-	const view = bookSearchView(entity);
-	const bookSearch: BookSearchResponseItem = { entity, view };
+	// const entity = convertResponseToBookSearch(gapiTestDatas.items![0]);
+	// const view = bookSearchView(entity);
+	// const bookSearch: BookSearchResponseItem = { entity, view };
+	const bookSearch = bookSearchInterfaceMock;
+	const onSuccess = (message: string) => void 0;
+	const onFailed = (message: string) => void 0;
 
 	afterEach(() => {
 		vi.restoreAllMocks();
 	});
 
 	it('レンダリング', () => {
-		render(ItemModal, { isDisplay: true, bookSearch });
+		render(ItemModal, {
+			isDisplay: true,
+			bookSearch,
+			onSuccess,
+			onFailed
+		});
 
 		expect(screen.getByText('書籍登録')).toBeInTheDocument();
 		expect(screen.getByAltText('書影')).toBeInTheDocument();
@@ -28,7 +33,12 @@ describe.skip('ContentModal', async () => {
 
 	it('isDisplayがFaulthyな場合に非表示に変わること', async () => {
 		const testId = 'layerZ30';
-		const { component } = render(ItemModal, { isDisplay: true, bookSearch });
+		const { component } = render(ItemModal, {
+			isDisplay: true,
+			bookSearch,
+			onSuccess,
+			onFailed
+		});
 
 		expect(screen.getByTestId(testId)).toBeInTheDocument();
 
@@ -38,7 +48,12 @@ describe.skip('ContentModal', async () => {
 
 	it('閉じる・キャンセルボタンクリックで非表示に変わること', async () => {
 		const testId = 'layerZ30';
-		const { component } = render(ItemModal, { isDisplay: true, bookSearch });
+		const { component } = render(ItemModal, {
+			isDisplay: true,
+			bookSearch,
+			onSuccess,
+			onFailed
+		});
 
 		const btnClose = screen.getByTestId('btnClose');
 		const btnCancel = screen.getByText('キャンセル');
@@ -51,42 +66,52 @@ describe.skip('ContentModal', async () => {
 		expect(screen.getByTestId(testId)).toHaveClass('hidden');
 	});
 
-	it('登録成功時に、イベントを発信できること', async () => {
-		//fetchのモックを作成
-		let mockFetch = vi.spyOn(global, 'fetch');
-		mockFetch.mockImplementation(async () => new Response('成功しました。', { status: 201 }));
+	// it('登録成功時に、イベントを発信できること', async () => {
+	// 	//fetchのモックを作成
+	// 	let mockFetch = vi.spyOn(global, 'fetch');
+	// 	mockFetch.mockImplementation(async () => new Response('成功しました。', { status: 201 }));
 
-		const { component } = render(ItemModal, { isDisplay: true, bookSearch });
-		const mockSuccess = vitest.fn();
-		component.$on('success', mockSuccess);
+	// 	const { component } = render(ItemModal, {
+	// 		isDisplay: true,
+	// 		bookSearch,
+	// 		onSuccess,
+	// 		onFailed
+	// 	});
+	// 	const mockSuccess = vitest.fn();
+	// 	component.$on('success', mockSuccess);
 
-		const btnRegister = screen.getByText('登録');
-		await fireEvent.click(btnRegister);
+	// 	const btnRegister = screen.getByText('登録');
+	// 	await fireEvent.click(btnRegister);
 
-		await waitFor(
-			() => {
-				expect(mockSuccess).toHaveBeenCalled();
-			},
-			{ timeout: 3000 }
-		);
-	});
+	// 	await waitFor(
+	// 		() => {
+	// 			expect(mockSuccess).toHaveBeenCalled();
+	// 		},
+	// 		{ timeout: 3000 }
+	// 	);
+	// });
 
-	it('登録失敗時に、イベントを発信できること', async () => {
-		let mockFetch = vi.spyOn(global, 'fetch');
-		mockFetch.mockImplementation(async () => new Response('失敗しました', { status: 500 }));
+	// it('登録失敗時に、イベントを発信できること', async () => {
+	// 	let mockFetch = vi.spyOn(global, 'fetch');
+	// 	mockFetch.mockImplementation(async () => new Response('失敗しました', { status: 500 }));
 
-		const { component } = render(ItemModal, { isDisplay: true, bookSearch });
-		const mockFailure = vitest.fn();
-		component.$on('failed', mockFailure);
+	// 	const { component } = render(ItemModal, {
+	// 		isDisplay: true,
+	// 		bookSearch,
+	// 		onSuccess,
+	// 		onFailed
+	// 	});
+	// 	const mockFailure = vitest.fn();
+	// 	component.$on('failed', mockFailure);
 
-		const btnRegister = screen.getByText('登録');
-		await fireEvent.click(btnRegister);
+	// 	const btnRegister = screen.getByText('登録');
+	// 	await fireEvent.click(btnRegister);
 
-		await waitFor(
-			() => {
-				expect(mockFailure).toHaveBeenCalled();
-			},
-			{ timeout: 3000 }
-		);
-	});
+	// 	await waitFor(
+	// 		() => {
+	// 			expect(mockFailure).toHaveBeenCalled();
+	// 		},
+	// 		{ timeout: 3000 }
+	// 	);
+	// });
 });
