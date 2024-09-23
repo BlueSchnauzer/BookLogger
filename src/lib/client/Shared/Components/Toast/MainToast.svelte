14c37a7ca622f5b610b1c46644eaf3ed91399d68
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-23 18:26:14 +0900

    create shared toast components

---
+++ src/lib/client/Shared/Components/Toast/MainToast.svelte
@@ -0,0 +1,17 @@
+<script src="ts">
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import { mainToastTarget } from '$lib/client/Shared/Helpers/Toast';
+</script>
+
+<div class="wrap-bottom">
+	<SvelteToast target={mainToastTarget} />
+</div>
+
+<style>
+	.wrap-bottom {
+		--toastContainerTop: auto;
+		--toastContainerRight: auto;
+		--toastContainerBottom: 4rem;
+		--toastContainerLeft: calc(50vw - 8rem);
+	}
+</style>
---
+++ src/lib/client/Shared/Components/Toast/ModalToast.svelte
@@ -0,0 +1,8 @@
+<script src="ts">
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import { modalToastTarget } from '$lib/client/Shared/Helpers/Toast';
+</script>
+
+<div class="wrap-default">
+	<SvelteToast target={modalToastTarget} />
+</div>
