6e7c1ecd50d88a48f73ab95b386df930296fe16d
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-24 18:29:48 +0900

    Use entity in grid imte

--- src/lib/client/UI/Contents/ContentsGrid/GridItem.svelte
+++ src/lib/client/UI/Contents/ContentsGrid/GridItem.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import type { BookInfoResponseItem } from '$lib/client/Application/Interface';
+	import type { BookInfo } from '$lib/client/Domain/Entities/BookInfo';
 	import StatusLabel from '$lib/client/UI/Contents/ContentsGrid/StatusLabel.svelte';
 
-	export let item: BookInfoResponseItem;
+	export let bookInfo: BookInfo;
 	/**画面サイズが小さくなった際にテキストを非表示にするか*/
 	export let isResponsiveText = true;
 </script>
 
-{#if item.entity.thumbnail}
+{#if bookInfo.thumbnail}
 	<img
 		class="justify-self-center self-center w-[128px] h-[182px] bg-slate-300"
-		src={item.entity.thumbnail}
+		src={bookInfo.thumbnail}
 		alt="test"
 	/>
 {:else}
@@ -26,13 +26,13 @@
 			? 'max-sm:flex'
 			: ''} justify-self-center self-center flex-col items-center justify-center w-[128px] h-[182px] bg-slate-300"
 	>
-		<span class="p-1 break-all collapseTitle">{item.entity.title}</span>
+		<span class="p-1 break-all collapseTitle">{bookInfo.title}</span>
 	</div>
 {/if}
 <div class={isResponsiveText ? 'max-sm:hidden' : ''}>
-	<span class="text-left px-2 text-lime-700 break-all collapseTitle">{item.entity.title}</span>
+	<span class="text-left px-2 text-lime-700 break-all collapseTitle">{bookInfo.title}</span>
 </div>
-<StatusLabel view={item.view} {isResponsiveText} />
+<StatusLabel {bookInfo} {isResponsiveText} />
 
 <style>
 	.collapseTitle {
