8e0cd75f6f46942cc88bd1eef0bedd74018eb626
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-28 00:21:00 +0900

    Remove useless mock data

--- src/lib/mock/Data.ts
+++ src/lib/mock/Data.ts
@@ -1,15 +1,10 @@
-import type { BookInfoResponseItem } from '$lib/client/Application/Interface';
-import { bookInfoView } from '$lib/client/Application/Views/BookInfo';
 import type { BookInfo } from '$lib/client/Feature/Contents/Domain/Entities/BookInfo';
-import {
-	convertResponseToBookSearch,
-	type BookSearch
-} from '$lib/client/Feature/Search/BookSearch';
 import { Id } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/Id';
 import { Identifiers } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/Identifier';
 import { PageHistory } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/PageHistory';
 import { Status } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/Status';
 import { UserId } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/UserId';
+import { type BookSearch } from '$lib/client/Feature/Search/BookSearch';
 import type { books_v1 } from 'googleapis';
 
 const firstId_test = '651451ed67241f439ce8a1af';
@@ -19,144 +14,6 @@ const thirdId_test = '651451ed67241f439ce8a1b1';
 export const testUserId1 = 'testUserId1';
 export const testUserId2 = 'testUserId2';
 export const testUserId3 = 'testUserId3';
-
-// /**テスト用書誌データのEntity(1件) */
-// export const getEntityTestData = (userId = testUserId1) => {
-// 	return new BookInfoEntity({ ...bookInfoPropertiesMock, userId });
-// };
-
-// /**テスト用書誌データのEntity(3件)
-//  * デフォルトでは全て同じUserIdを持つ
-//  */
-// export const getEntityTestDatas = (
-// 	userId1 = testUserId1,
-// 	userId2 = testUserId1,
-// 	userId3 = testUserId1
-// ): BookInfoEntity[] => {
-// 	bookInfoPropertiesMocks[0].userId = userId1;
-// 	bookInfoPropertiesMocks[1].userId = userId2;
-// 	bookInfoPropertiesMocks[2].userId = userId3;
-
-// 	return bookInfoPropertiesMocks.map((item) => new BookInfoEntity(item));
-// };
-
-// /**書誌データのEntityの作成プロパティ(1件) */
-// export const bookInfoPropertiesMock: bookInfoProperties = {
-// 	id: firstId_test,
-// 	userId: testUserId1,
-// 	title: 'わたしを離さないで',
-// 	author: ['イシグロカズオ'],
-// 	thumbnail: '',
-// 	createDate: new Date(),
-// 	updateDate: new Date(),
-// 	pageCount: 300,
-// 	isFavorite: false,
-// 	status: 'wish',
-// 	memorandum: 'memo1',
-// 	isVisible: true,
-// 	completeDate: undefined,
-// 	pageHistories: [
-// 		{
-// 			id: crypto.randomUUID(),
-// 			date: new Date(),
-// 			pageCount: 0
-// 		},
-// 		{
-// 			id: crypto.randomUUID(),
-// 			date: new Date(),
-// 			pageCount: 10
-// 		}
-// 	],
-// 	identifiers: {
-// 		isbn_13: '978-4-15-120051-9'
-// 	},
-// 	shelfCategories: undefined,
-// 	gapiId: 'firstData'
-// };
-
-// /**書誌データのEntityの作成プロパティ(3件) */
-// export const bookInfoPropertiesMocks: bookInfoProperties[] = [
-// 	{
-// 		id: firstId_test,
-// 		userId: testUserId1,
-// 		title: 'わたしを離さないで',
-// 		author: ['イシグロカズオ'],
-// 		thumbnail: '',
-// 		createDate: new Date(),
-// 		updateDate: new Date(),
-// 		pageCount: 300,
-// 		isFavorite: false,
-// 		status: 'wish',
-// 		memorandum: 'memo1',
-// 		isVisible: true,
-// 		completeDate: undefined,
-// 		pageHistories: [
-// 			{
-// 				id: crypto.randomUUID(),
-// 				date: new Date(),
-// 				pageCount: 0
-// 			},
-// 			{
-// 				id: crypto.randomUUID(),
-// 				date: new Date(),
-// 				pageCount: 10
-// 			}
-// 		],
-// 		identifiers: {
-// 			isbn_13: '978-4-15-120051-9'
-// 		},
-// 		shelfCategories: undefined,
-// 		gapiId: 'firstData'
-// 	},
-// 	{
-// 		id: secondId_test,
-// 		userId: testUserId2,
-// 		title: 'エピローグ',
-// 		author: ['円城塔'],
-// 		thumbnail: '',
-// 		createDate: new Date(),
-// 		updateDate: new Date(),
-// 		pageCount: -1,
-// 		isFavorite: false,
-// 		status: 'reading',
-// 		memorandum: 'memo2',
-// 		isVisible: true,
-// 		completeDate: undefined,
-// 		pageHistories: [
-// 			{
-// 				id: crypto.randomUUID(),
-// 				date: new Date(),
-// 				pageCount: 0
-// 			}
-// 		],
-// 		identifiers: {
-// 			isbn_13: '978-4-15-031316-6'
-// 		},
-// 		shelfCategories: undefined,
-// 		gapiId: 'secondData'
-// 	},
-// 	{
-// 		id: thirdId_test,
-// 		userId: testUserId3,
-// 		title: 'プロローグ',
-// 		author: ['円城塔'],
-// 		thumbnail: '',
-// 		createDate: new Date(),
-// 		updateDate: new Date(),
-// 		pageCount: -1,
-// 		isFavorite: false,
-// 		status: 'complete',
-// 		memorandum: 'memo3',
-// 		isVisible: true,
-// 		completeDate: undefined,
-// 		pageHistories: [],
-// 		identifiers: {
-// 			isbn_13: '978-4-16-791019-8'
-// 		},
-// 		shelfCategories: undefined,
-// 		gapiId: 'thirdData'
-// 	}
-// ];
 
 /**書誌データEntityのテストデータ(1件) */
 export const bookInfoInterfaceMock: BookInfo = {
@@ -362,22 +219,6 @@ export const bookInfoInterfaceMocksWithUserIds = (
 		gapiId: 'thirdData'
 	}
 ];
-
-export const bookInfoResponseItemMock = (): BookInfoResponseItem => {
-	const entity = bookInfoInterfaceMock;
-	const view = bookInfoView(entity);
-
-	return { entity, view };
-};
-
-export const bookInfoResponseItemsMock = (): BookInfoResponseItem[] => {
-	const bookInfoResponseItems = bookInfoInterfaceMocks.map((entity) => {
-		const view = bookInfoView(entity);
-		return { entity, view };
-	});
-
-	return bookInfoResponseItems;
-};
 
 /**GAPIのテスト用レスポンスデータ */
 export const gapiTestDatas: books_v1.Schema$Volumes = {
