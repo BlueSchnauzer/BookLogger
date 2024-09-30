ca20903925173e1529f0acbee6c0b478017afe3d
BlueSchnauzer <kbrucke08@gmail.com>
2024-09-30 15:04:44 +0900

    use verification proccess for svelte api route

--- src/routes/(app)/api/bookinfo/+server.ts
+++ src/routes/(app)/api/bookinfo/+server.ts
@@ -11,20 +11,21 @@ import { verifyAndCreateUserId } from '$lib/server/Feature/Auth/idManager';
 import { BookInfoMongoDBResource } from '$lib/server/Feature/Contents/MongoDB/BookInfoDBResource';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { verifyTokenAndCollections } from '../verification';
 
 /**書誌データを取得する
  * クエリパラメータに応じて返却するデータを変更する。
  */
 export const GET: RequestHandler = async ({ url, cookies }) => {
-	const userId = await verifyAndCreateUserId(cookies.get('idToken')!);
-	if (!userId) {
-		return new Response('ログイン情報が不正です', { status: 400 });
-	}
-	if (!collections) {
-		return new Response('サーバーエラー', { status: 500 });
+	const { userId, response } = await verifyTokenAndCollections(
+		cookies.get('idToken')!,
+		collections!
+	);
+	if (!response.ok) {
+		return response;
 	}
 
-	const repos = new BookInfoMongoDBResource(collections.bookInfos!, userId!);
+	const repos = new BookInfoMongoDBResource(collections?.bookInfos!, userId!);
 
 	const param = url.searchParams;
 	const getType = param.get('type');
@@ -48,48 +49,47 @@ export const GET: RequestHandler = async ({ url, cookies }) => {
 
 /**DBに書誌データを保存する */
 export const POST: RequestHandler = async ({ request, cookies }) => {
-	const userId = await verifyAndCreateUserId(cookies.get('idToken')!);
-	if (!userId) {
-		return new Response('ログイン情報が不正です', { status: 400 });
+	const { userId, response } = await verifyTokenAndCollections(
+		cookies.get('idToken')!,
+		collections!
+	);
+	if (!response.ok) {
+		return response;
 	}
-	if (!collections) {
-		return new Response('サーバーエラー', { status: 500 });
-	}
-
-	const repos = new BookInfoMongoDBResource(collections.bookInfos!, userId!);
 
 	const bookSearch = (await request.json()) as BookSearch;
-	return await repos.insert(convertBookSearchToDBModel(userId.value, bookSearch));
+	const repos = new BookInfoMongoDBResource(collections?.bookInfos!, userId!);
+	return await repos.insert(convertBookSearchToDBModel(userId?.value!, bookSearch));
 };
 
 /**DBの書誌データを更新する */
 export const PUT: RequestHandler = async ({ request, cookies }) => {
-	const userId = await verifyAndCreateUserId(cookies.get('idToken')!);
-	if (!userId) {
-		return new Response('ログイン情報が不正です', { status: 400 });
-	}
-	if (!collections) {
-		return new Response('サーバーエラー', { status: 500 });
+	const { userId, response } = await verifyTokenAndCollections(
+		cookies.get('idToken')!,
+		collections!
+	);
+	if (!response.ok) {
+		return response;
 	}
 
 	//Postされたデータの型はモデルではなくEntity
 	const item = (await request.json()) as { bookInfo: BookInfo; isCompleteReading: boolean };
-	const repos = new BookInfoMongoDBResource(collections.bookInfos!, userId);
-
 	if (!validatePutBookInfo(item.bookInfo, item.isCompleteReading)) {
 		return new Response('データが不正です', { status: 400 });
 	}
+
+	const repos = new BookInfoMongoDBResource(collections?.bookInfos!, userId!);
 	return await repos.update(convertBookInfoToDBModel(item.bookInfo), item.isCompleteReading);
 };
 
 /**DBの書誌データを削除する */
 export const DELETE: RequestHandler = async ({ request, cookies }) => {
-	const userId = await verifyAndCreateUserId(cookies.get('idToken')!);
-	if (!userId) {
-		return new Response('ログイン情報が不正です', { status: 400 });
-	}
-	if (!collections) {
-		return new Response('サーバーエラー', { status: 500 });
+	const { userId, response } = await verifyTokenAndCollections(
+		cookies.get('idToken')!,
+		collections!
+	);
+	if (!response.ok) {
+		return response;
 	}
 
 	const id: string = await request.json();
@@ -97,7 +97,6 @@ export const DELETE: RequestHandler = async ({ request, cookies }) => {
 		return new Response('データが不正です', { status: 400 });
 	}
 
-	const repos = new BookInfoMongoDBResource(collections.bookInfos!, userId);
-
+	const repos = new BookInfoMongoDBResource(collections?.bookInfos!, userId!);
 	return await repos.delete(id);
 };
