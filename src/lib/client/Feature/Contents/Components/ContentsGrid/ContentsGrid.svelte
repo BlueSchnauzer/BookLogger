7b06ee8e4b54f8e8c361bc63e7ce51927104c019
BlueSchnauzer <kbrucke08@gmail.com>
2024-11-13 19:52:37 +0900

    feat: add paging button ui

--- src/lib/client/Feature/Contents/Components/ContentsGrid/ContentsGrid.svelte
+++ src/lib/client/Feature/Contents/Components/ContentsGrid/ContentsGrid.svelte
@@ -5,6 +5,8 @@
 	import SimpleBar from 'simplebar';
 	import 'simplebar/dist/simplebar.css';
 	import { onMount } from 'svelte';
+	import Icon from '@iconify/svelte';
+	import { colorLime800 } from '$lib/client/Shared/Constants/DisplayValues';
 
 	export let bookInfos: BookInfo[] | undefined;
 	/**表示する本が無い場合のメッセージ*/
@@ -21,8 +23,8 @@
 	});
 </script>
 
-{#if bookInfos && bookInfos.length}
-	<div bind:this={contentGrid} class="p-1 contentHeight">
+<div bind:this={contentGrid} class="flex flex-grow p-1 relative contentHeight">
+	{#if bookInfos && bookInfos.length}
 		<ul
 			class="grid gap-2 grid-cols-BookContentAutoFill max-sm:grid-cols-smBookContentAutoFit max-sm:place-items-center"
 		>
@@ -37,12 +39,32 @@
 				</li>
 			{/each}
 		</ul>
-	</div>
-{:else}
-	<div class="p-1 contentHeight">
+		<div
+			class="flex justify-between items-center sticky bottom-2 m-auto mt-4 py-1 px-4 gap-1 size-fit rounded-full bg-gray-100
+    border border-stone-300 shadow"
+		>
+			<button class="hover:bg-stone-300 rounded">
+				<Icon icon="ph:caret-line-left" width="32" height="32" color={colorLime800} />
+			</button>
+			<button class="hover:bg-stone-300 rounded">
+				<Icon icon="ph:caret-left" width="32" height="32" color={colorLime800} />
+			</button>
+			<div class="flex px-4">
+				<p>1</p>
+				<p>/</p>
+				<p>10</p>
+			</div>
+			<button class="hover:bg-stone-300 rounded">
+				<Icon icon="ph:caret-right" width="32" height="32" color={colorLime800} />
+			</button>
+			<button class="hover:bg-stone-300 rounded">
+				<Icon icon="ph:caret-line-right" width="32" height="32" color={colorLime800} />
+			</button>
+		</div>
+	{:else}
 		<p class="px-1 font-medium text-lime-700">{@html emptyMessage}</p>
-	</div>
-{/if}
+	{/if}
+</div>
 
 <style>
 	.contentHeight {
