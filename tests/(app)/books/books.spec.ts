bd2ee7dd8d68dcb391bbbd4b70d3ace92a2ed7c5
BlueSchnauzer <kbrucke08@gmail.com>
2024-11-20 00:53:32 +0900

    test: fix expect statement in books e2e

--- tests/(app)/books/books.spec.ts
+++ tests/(app)/books/books.spec.ts
@@ -4,6 +4,6 @@ test.describe('ライブラリ', () => {
 	test('ライブラリ画面が表示できること', async ({ loginedPage }) => {
 		const pageName = 'ライブラリ';
 		await loginedPage.page.locator('[href="/books"]', { hasNotText: pageName }).click();
-		await expect(loginedPage.page.waitForURL('/books')).toBeTruthy();
+		await loginedPage.page.waitForURL('/books');
 	});
 });
