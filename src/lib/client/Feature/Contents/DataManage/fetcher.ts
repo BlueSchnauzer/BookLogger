ba00619ec85d4c13a32f60aac6a4ca33c48668a9
BlueSchnauzer <kbrucke08@gmail.com>
2024-11-28 18:42:33 +0900

    feat: change to use params in get books fetcher

--- src/lib/client/Feature/Contents/DataManage/fetcher.ts
+++ src/lib/client/Feature/Contents/DataManage/fetcher.ts
@@ -13,6 +13,7 @@ import { createUrlWithParams } from '$lib/client/Shared/Helpers/Urls';
 import type { FetchInterface } from '$lib/client/Shared/interface';
 import { getPageHistoryMapInCurrentWeek } from '$lib/client/Shared/Utils/PageHistory';
 import type { BookInfoDBModel } from '$lib/server/Feature/Contents/MongoDB/BookInfoModel';
+import type { OrderFilters } from '$lib/client/Feature/Contents/interface';
 
 export const getBookInfoById = async (fetch: FetchInterface, id: id) => {
 	const response = await fetch(`${APIRouteURLs.bookInfo}/${id}`);
@@ -21,8 +22,17 @@ export const getBookInfoById = async (fetch: FetchInterface, id: id) => {
 	return model ? convertDBModelToBookInfo(model) : undefined;
 };
 
-export const getBookInfos = async (fetch: FetchInterface, page: number) => {
-	const param = { page: page.toString() };
+export const getBookInfos = async (
+	fetch: FetchInterface,
+	page: number,
+	options?: { status?: status; query?: string; order?: OrderFilters }
+) => {
+	const param = {
+		page: page.toString(),
+		type: options?.status ?? '',
+		query: options?.query ?? '',
+		order: options?.order ?? ''
+	};
 
 	const response = await fetch(createUrlWithParams(APIRouteURLs.bookInfo, param));
 	const { totalCount, lastPageCount, bookInfoDBModels } = await parseListResponse(response);
