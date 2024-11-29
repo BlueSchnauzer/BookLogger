d71bd2fe20c88272fdfe295fbd98f1f56eb2c4c6
BlueSchnauzer <kbrucke08@gmail.com>
2024-11-29 21:04:43 +0900

    feat: get query params and fix arguments

--- src/routes/(app)/books/+page.ts
+++ src/routes/(app)/books/+page.ts
@@ -1,10 +1,26 @@
 import { getBookInfos } from '$lib/client/Feature/Contents/DataManage/fetcher';
-import { getPageCount } from '$lib/client/Shared/Helpers/Urls';
+import { getPageCount, getParamValue } from '$lib/client/Shared/Helpers/Urls';
 import type { PageLoad } from './$types';
+import type { status } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/Status';
+import type { OrderFilters } from '$lib/client/Feature/Contents/interface';
 
 export const load = (async ({ fetch, url }) => {
-	const pageCount = getPageCount(url.searchParams);
-	const { totalCount, lastPageCount, bookInfos } = await getBookInfos(fetch, pageCount);
+	const { page, status, query, order } = getSearchConditions(url.searchParams);
 
-	return { pageCount, totalCount, lastPageCount, bookInfos };
+	const { totalCount, lastPageCount, bookInfos } = await getBookInfos(fetch, page, {
+		status,
+		query,
+		order
+	});
+
+	return { page, totalCount, lastPageCount, bookInfos };
 }) satisfies PageLoad;
+
+const getSearchConditions = (params: URLSearchParams) => {
+	const page = getPageCount(params);
+	const status = getParamValue(params, 'status') as status;
+	const query = getParamValue(params, 'query') ?? '';
+	const order = getParamValue(params, 'order') as OrderFilters;
+
+	return { page, status, query, order };
+};
