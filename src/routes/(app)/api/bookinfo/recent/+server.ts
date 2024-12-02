f001a810300599aebe333ee5a2ecc59c63ccf89f
BlueSchnauzer <kbrucke08@gmail.com>
2024-12-02 18:38:56 +0900

    feat: implement get recent book endpoint

--- src/routes/(app)/api/bookinfo/recent/+server.ts
+++ src/routes/(app)/api/bookinfo/recent/+server.ts
@@ -1,5 +1,20 @@
 import type { RequestHandler } from '../../bookinfo/$types';
+import { verifyTokenAndCollections } from '../../verification';
+import collections from '$lib/server/Feature/Contents/MongoDB/MongoDBHelper';
+import { BookInfoMongoDBResource } from '$lib/server/Feature/Contents/MongoDB/BookInfoDBResource';
+import { json } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async () => {
-	return new Response();
+export const GET: RequestHandler = async ({ cookies }) => {
+	const { userId, response } = await verifyTokenAndCollections(
+		cookies.get('idToken')!,
+		collections!
+	);
+	if (!response.ok) {
+		return response;
+	}
+
+	const repos = new BookInfoMongoDBResource(collections?.bookInfos!, userId!);
+
+	const recentData = await repos.getRecentBookInfo();
+	return json(recentData ?? null, { status: 200 });
 };
