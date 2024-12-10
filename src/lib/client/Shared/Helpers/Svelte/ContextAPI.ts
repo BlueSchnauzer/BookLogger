f767a936617479c6a670c8276a2cda2ef16ff2e1
BlueSchnauzer <kbrucke08@gmail.com>
2024-12-10 22:36:25 +0900

    refactor: refactoring context api functions

--- src/lib/client/Shared/Helpers/Svelte/ContextAPI.ts
+++ src/lib/client/Shared/Helpers/Svelte/ContextAPI.ts
@@ -1,9 +1,4 @@
 import { setContext, getContext } from 'svelte';
 
-export const setPathNameContext = (pathName: string) => {
-	setContext('pathName', pathName);
-};
-
-export const getPathNameContext = () => {
-	return getContext<string>('pathName');
-};
+export const setPathNameContext = (pathName: string) => setContext('pathName', pathName);
+export const getPathNameContext = () => getContext<string>('pathName');
