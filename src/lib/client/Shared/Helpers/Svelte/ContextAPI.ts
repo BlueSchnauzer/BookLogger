1ee70d3cab589e8089efb674762a058e13f3aa8c
BlueSchnauzer <kbrucke08@gmail.com>
2024-12-13 01:48:41 +0900

    feat: add setter and getter books url infos

--- src/lib/client/Shared/Helpers/Svelte/ContextAPI.ts
+++ src/lib/client/Shared/Helpers/Svelte/ContextAPI.ts
@@ -2,3 +2,15 @@ import { setContext, getContext } from 'svelte';
 
 export const setPathNameContext = (pathName: string) => setContext('pathName', pathName);
 export const getPathNameContext = () => getContext<string>('pathName');
+
+interface BooksUrlInfo {
+	pathName: string;
+	page: string;
+	status: string;
+	query: string;
+	order: string;
+}
+
+export const setBooksUrlInfo = (booksUrlInfo: BooksUrlInfo) =>
+	setContext('booksUrlInfo', booksUrlInfo);
+export const getBooksUrlInfo = () => getContext<BooksUrlInfo>('booksUrlInfo');
