d587528e31eb140a4beb356169a5894b5db02c58
BlueSchnauzer <kbrucke08@gmail.com>
2024-12-27 20:38:25 +0900

    test: fix ui test error

--- tests/(app)/books/reading/reading.spec.ts
+++ tests/(app)/books/reading/reading.spec.ts
@@ -1,11 +1,11 @@
-import { test, expect } from '../../../fixtures/loginAndLogout';
+import { BooksURLs } from '$lib/client/Shared/Constants/urls';
+import { test } from '../../../fixtures/loginAndLogout';
 
 test.describe('読んでいる本', () => {
 	test('読んでいる本の画面が表示できること', async ({ loginedPage }) => {
 		await loginedPage.gotoLibrary();
 
-		const pageName = '読んでいる';
-		await loginedPage.page.getByText(pageName).click();
-		await loginedPage.page.waitForURL('/books/reading');
+		await loginedPage.page.locator(`[href="${BooksURLs.reading}"]`).click();
+		await loginedPage.page.waitForURL(BooksURLs.reading);
 	});
 });
