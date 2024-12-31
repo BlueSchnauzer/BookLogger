98828923ff2ba9fb9f6abb37fedea9b512a97a21
BlueSchnauzer <kbrucke08@gmail.com>
2024-12-31 19:55:16 +0900

    change: 本棚用interfaceを修正

--- src/lib/client/Feature/Collections/Domain/Entities/BookShelf.ts
+++ src/lib/client/Feature/Collections/Domain/Entities/BookShelf.ts
@@ -1,1 +1,11 @@
-export class BookShelf {}
+import type { Id } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/Id';
+import type { UserId } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/UserId';
+
+export interface BookShelf {
+	id?: Id;
+	userId: UserId;
+	shelfName: string;
+	contentsIds: Id[];
+	createDate: Date;
+	updateDate: Date;
+}
