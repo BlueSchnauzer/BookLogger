7892e3f6137c96dd267c2d3325ddd1a6d35e7b4c
BlueSchnauzer <kbrucke08@gmail.com>
2025-01-06 21:02:41 +0900

    chnage: ページング処理用の引数を削除

--- src/lib/server/Feature/Collections/IBookShelfDB.ts
+++ src/lib/server/Feature/Collections/IBookShelfDB.ts
@@ -4,10 +4,10 @@ import type { BookShelfDBModel } from './MongoDB/BookShelfModel';
 
 export interface IBookShelfDBRepositories {
 	getBookShelf(id: id): Promise<BookShelfDBModel | undefined>;
-	getBookShelves(
-		page: number,
-		options: { query?: string; order?: OrderFilters }
-	): Promise<{ lastPageCount: number; totalCount: number; bookShelfDBModels: BookShelfDBModel[] }>;
+	getBookShelves(options: {
+		query?: string;
+		order?: OrderFilters;
+	}): Promise<{ lastPageCount: number; totalCount: number; bookShelfDBModels: BookShelfDBModel[] }>;
 	insert(bookShelf: BookShelfDBModel): Promise<Response>;
 	update(bookShelf: BookShelfDBModel): Promise<Response>;
 	delete(id: id): Promise<Response>;
--- src/lib/server/Feature/Collections/MongoDB/BookShelfDBResource.ts
+++ src/lib/server/Feature/Collections/MongoDB/BookShelfDBResource.ts
@@ -1,17 +1,25 @@
 import type { id } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/Id';
+import type { UserId } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/UserId';
 import type { OrderFilters } from '$lib/client/Feature/Contents/interface';
+import type { bookShelvesCollection } from '$lib/server/Shared/Helpers/MongoDBHelper';
 import type { IBookShelfDBRepositories } from '../IBookShelfDB';
 import type { BookShelfDBModel } from './BookShelfModel';
 
 export class BookShelfMongoDBResource implements IBookShelfDBRepositories {
+	constructor(
+		private readonly _collection: bookShelvesCollection,
+		private readonly _userId: UserId
+	) {}
+
 	getBookShelf(id: id): Promise<BookShelfDBModel | undefined> {
 		throw new Error('Method not implemented.');
 	}
 
-	getBookShelves(
-		page: number,
-		options: { query?: string; order?: OrderFilters }
-	): Promise<{ lastPageCount: number; totalCount: number; bookShelfDBModels: BookShelfDBModel[] }> {
+	getBookShelves(options: { query?: string; order?: OrderFilters }): Promise<{
+		lastPageCount: number;
+		totalCount: number;
+		bookShelfDBModels: BookShelfDBModel[];
+	}> {
 		throw new Error('Method not implemented.');
 	}
 
