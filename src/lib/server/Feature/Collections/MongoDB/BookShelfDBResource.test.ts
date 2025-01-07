5821091b455169f9122d43b2e066f5f1a63b9314
BlueSchnauzer <kbrucke08@gmail.com>
2025-01-07 23:02:33 +0900

    test: 書棚データ取得処理のテストを実装

--- src/lib/server/Feature/Collections/MongoDB/BookShelfDBResource.test.ts
+++ src/lib/server/Feature/Collections/MongoDB/BookShelfDBResource.test.ts
@@ -1,8 +1,57 @@
-import { describe, it } from 'vitest';
+import { MongoClient, type Db, type Collection, ObjectId } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import * as env from '$env/static/private';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import {
+	convertBookShelfToDBModel,
+	type BookShelfDBModel
+} from '$lib/server/Feature/Collections/MongoDB/BookShelfModel';
+import { bookShelfInterfaceMock, testUserId1 } from '$lib/mock/Data';
+import { BookShelfMongoDBResource } from './BookShelfDBResource';
+import { UserId } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/UserId';
+
+//共通で使用する接続データと、その初期化・破棄用の処理
+let client: MongoClient;
+let mongoServer: MongoMemoryServer;
+let db: Db;
+let collection: Collection<BookShelfDBModel>;
+
+beforeEach(async () => {
+	mongoServer = await MongoMemoryServer.create();
+	client = await MongoClient.connect(mongoServer.getUri(), {});
+	db = client.db(mongoServer.instanceInfo?.dbName);
+	collection = db.collection<BookShelfDBModel>(env.BOOKSHELVES_COLLECTION_NAME);
+});
+afterEach(async () => {
+	if (client) {
+		await client.close();
+	}
+	if (mongoServer) {
+		await mongoServer.stop();
+	}
+});
 
 describe('getBookShelf', () => {
-	it('ユーザIDとIDに一致するデータを取得できること', () => {});
-	it('一致するデータが無い時にUndefinedが返ること', () => {});
+	it('ユーザIDとIDに一致するデータを取得できること', async () => {
+		const testData = bookShelfInterfaceMock;
+		const preData = await collection.insertOne(convertBookShelfToDBModel(testData));
+		expect(preData.acknowledged).toBeTruthy();
+
+		const repos = new BookShelfMongoDBResource(collection, testData.userId);
+		const response = await repos.getBookShelf(testData.id?.value!);
+
+		expect(response).toBeDefined();
+		expect(response?.userId).toEqual(testData.userId.value);
+		expect(response?.shelfName).toEqual(testData.shelfName);
+		expect(response?.contentsIds).toEqual(testData.contentsIds.map((id) => new ObjectId(id.value)));
+	});
+
+	it('一致するデータが無い時にUndefinedが返ること', async () => {
+		const repos = new BookShelfMongoDBResource(collection, new UserId(testUserId1));
+		const response = await repos.getBookShelf(new ObjectId().toHexString());
+
+		expect(response).toBeUndefined();
+	});
 });
 
 describe('getBookShelves', () => {
