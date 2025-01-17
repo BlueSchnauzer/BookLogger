669371a7f9a5e2423173dd8465d7d4cb2879aabc
BlueSchnauzer <kbrucke08@gmail.com>
2025-01-17 20:27:40 +0900

    test: 書棚データのフィルター処理のテストを実装

--- src/lib/server/Feature/Collections/MongoDB/BookShelfDBResource.test.ts
+++ src/lib/server/Feature/Collections/MongoDB/BookShelfDBResource.test.ts
@@ -6,7 +6,7 @@ import {
 	convertBookShelfToDBModel,
 	type BookShelfDBModel
 } from '$lib/server/Feature/Collections/MongoDB/BookShelfModel';
-import { bookShelfInterfaceMock, testUserId1 } from '$lib/mock/Data';
+import { bookShelfInterfaceMock, bookShelfInterfaceMocks, testUserId1 } from '$lib/mock/Data';
 import { BookShelfMongoDBResource } from './BookShelfDBResource';
 import { UserId } from '$lib/client/Feature/Contents/Domain/ValueObjects/BookInfo/UserId';
 
@@ -61,17 +61,76 @@ describe('getBookShelves', () => {
 		expect(await preData.acknowledged).toBeTruthy();
 
 		const repos = new BookShelfMongoDBResource(collection, new UserId(testUserId1));
-		const response = await repos.getBookShelves({});
+		const response = await repos.getBookShelves();
 		expect(response).toBeDefined();
 		expect(response.length).toBe(1);
 		expect(response[0].userId).toEqual(testUserId1);
 	});
-	it('一致するデータが無い場合に空のデータが返ること', () => {});
+	it('一致するデータが無い場合に空のデータが返ること', async () => {
+		const repos = new BookShelfMongoDBResource(collection, new UserId(testUserId1));
+		const response = await repos.getBookShelves();
+		expect(response).toBeDefined();
+		expect(response.length).toBe(0);
+	});
 
 	describe('queryもしくはorderの値に一致するデータを取得できること', () => {
-		it('query引数の値に部分一致する、shelfNameを持つ書棚情報を取得できること', () => {});
-		it('order引数の条件で、取得するデータの並び替えが行えること。', () => {});
-		it('queryとorderの組み合わせ条件で、書誌データを取得できること', () => {});
+		it('query引数の値に部分一致する、shelfNameを持つ書棚情報を取得できること', async () => {
+			const testData = bookShelfInterfaceMock;
+			const preData = await collection.insertOne(convertBookShelfToDBModel(testData));
+			expect(preData.acknowledged).toBeTruthy();
+
+			const repos = new BookShelfMongoDBResource(collection, testData.userId);
+			const response = await repos.getBookShelves({ query: testData.shelfName });
+
+			expect(response).toBeDefined();
+			expect(response.length).toBe(1);
+			expect(response[0].shelfName).toEqual(testData.shelfName);
+		});
+		it('order引数の条件で、取得するデータの並び替えが行えること。', async () => {
+			const testDatas = bookShelfInterfaceMocks;
+			//並び替えのために最新化する
+			const date = new Date();
+			testDatas[0].createDate.setDate(date.getDate() + 1);
+			testDatas[1].createDate.setDate(date.getDate() - 1);
+			testDatas[2].updateDate.setDate(date.getDate() + 1);
+
+			const preData = await collection.insertMany(
+				testDatas.map((data) => convertBookShelfToDBModel(data))
+			);
+			expect(preData.acknowledged).toBeTruthy();
+
+			const repos = new BookShelfMongoDBResource(collection, testDatas[0].userId);
+			const filteredByDesc = await repos.getBookShelves({ order: 'createDateDesc' });
+			const filteredByAsc = await repos.getBookShelves({ order: 'createDateAsc' });
+			const filteredByUpdateDate = await repos.getBookShelves({ order: 'updateDate' });
+
+			expect(filteredByDesc.length).toBe(3);
+			expect(filteredByDesc[0].shelfName).toBe(testDatas[0].shelfName);
+
+			expect(filteredByAsc.length).toBe(3);
+			expect(filteredByAsc[0].shelfName).toBe(testDatas[1].shelfName);
+
+			expect(filteredByUpdateDate.length).toBe(3);
+			expect(filteredByUpdateDate[0].shelfName).toBe(testDatas[2].shelfName);
+		});
+		it('queryとorderの組み合わせ条件で、書誌データを取得できること', async () => {
+			const testDatas = bookShelfInterfaceMocks;
+
+			testDatas[2].updateDate.setDate(new Date().getDate() + 1);
+			const preData = await collection.insertMany(
+				testDatas.map((data) => convertBookShelfToDBModel(data))
+			);
+			expect(preData.acknowledged).toBeTruthy();
+
+			const repos = new BookShelfMongoDBResource(collection, testDatas[0].userId);
+			const filteredByConditions = await repos.getBookShelves({
+				query: 'test',
+				order: 'updateDate'
+			});
+
+			expect(filteredByConditions.length).toBe(3);
+			expect(filteredByConditions[0].shelfName).toBe(testDatas[2].shelfName);
+		});
 	});
 });
 
