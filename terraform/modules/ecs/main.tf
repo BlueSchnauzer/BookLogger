504c9970fffc5a7d3abcc9e2e31ea23da66aceae
BlueSchnauzer <kbrucke08@gmail.com>
2025-11-12 22:05:08 +0900

    change: add tags to ecs resource

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,4 +1,9 @@
 resource "aws_ecs_task_definition" "this" {
     family                = "${var.appname}-${var.environment}"
     container_definitions = jsondecode("")
+    
+  tags = {
+    Application = var.appname
+    Environment = var.environment
+  }
 }
