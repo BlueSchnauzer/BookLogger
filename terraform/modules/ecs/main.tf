a0569be573b26cf3fcd7a3a41c91c7fdd9035c6d
BlueSchnauzer <kbrucke08@gmail.com>
2025-11-14 20:57:46 +0900

    change: add ecs task definition resource

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,3 +1,4 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition
 resource "aws_ecs_task_definition" "this" {
   family                = "${var.appname}-${var.environment}"
   container_definitions = jsondecode("")
