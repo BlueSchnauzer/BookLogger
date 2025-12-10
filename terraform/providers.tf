1e4a5803ee0a2c22b9ffab62b8269ade984d83c3
BlueSchnauzer <kbrucke08@gmail.com>
2025-12-10 20:01:10 +0900

    change: add default tags

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,3 +7,13 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      Application = var.appname
+      Environment = var.environment
+    }
+  }
+}
