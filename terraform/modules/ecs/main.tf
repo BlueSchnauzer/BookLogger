f04bebd75261d577a306763f062501094e062ed2
BlueSchnauzer <kbrucke08@gmail.com>
2025-12-22 20:32:26 +0900

    change: change target group health check interval

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -75,8 +75,8 @@ resource "aws_lb_target_group" "this" {
 
   health_check {
     path                = "/health-check"
-    interval            = 30
-    timeout             = 5
+    interval            = 15
+    timeout             = 10
     healthy_threshold   = 2
     unhealthy_threshold = 2
     matcher             = "200"
