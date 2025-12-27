f8f6961d8c76f9eee824e6e2b70a9fe6925c2fe2
BlueSchnauzer <kbrucke08@gmail.com>
2025-12-27 20:13:40 +0900

    change: add DNS record variables for Route 53 module

--- terraform/modules/route53/variables.tf
+++ terraform/modules/route53/variables.tf
@@ -2,3 +2,19 @@ variable "zone_id" {
   description = "The ID of the Route 53 hosted zone."
   type        = string
 }
+
+variable "record_name" {
+  description = "The name of the DNS record."
+  type        = string
+}
+
+variable "record_type" {
+  description = "The type of the DNS record (e.g., A, CNAME, TXT)."
+  type        = string
+}
+
+variable "record_ttl" {
+  description = "The TTL (time to live) of the DNS record."
+  type        = number
+  default     = 300
+}
