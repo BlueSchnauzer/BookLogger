455aeb40817aa8f6d242c2243592506be508e2c8
BlueSchnauzer <kbrucke08@gmail.com>
2026-01-18 21:16:17 +0900

    change: set lb setting

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -8,6 +8,9 @@ resource "aws_ecs_cluster" "this" {
   }
 }
 
+//クラスターを別ファイルのecsに分けて、それ以外をecs_serviceにまとめる
+
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition
 resource "aws_ecs_task_definition" "this" {
   family = local.name
@@ -58,27 +61,16 @@ resource "aws_ecs_task_definition" "this" {
   }])
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
-resource "aws_ecs_service" "this" {
-  name            = local.name
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.this.arn
-  desired_count   = var.desired_count
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = var.subnet_ids
-    security_groups  = var.security_group_ids
-    assign_public_ip = var.assign_public_ip
-  }
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
 resource "aws_lb" "this" {
   name               = local.name
-  internal           = false
+  internal           = false //環境に応じて切り替える？
   load_balancer_type = "application"
   security_groups    = var.lb_security_group_ids
   subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
+
   access_logs {
     bucket  = var.lb_access_logs_bucket
     prefix  = local.name
@@ -86,30 +78,117 @@ resource "aws_lb" "this" {
   }
 }
 
-resource "aws_lb_listener" "this" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = var.lb_listener_port
-  protocol          = var.lb_listener_protocol
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
-  }
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
 resource "aws_lb_target_group" "this" {
-  name     = local.name
-  port     = var.container_port
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = local.name
+  target_type = "ip"
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
 
   health_check {
     path                = "/health-check"
+    port                = var.container_health_port
     interval            = 15
     timeout             = 10
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    matcher             = "200"
+    matcher             = 200
   }
+
+  //要チェック
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = var.lb_listener_port
+  protocol          = var.lb_listener_protocol
+  ssl_policy        = var.lb_ssl_policy
+  certificate_arn   = var.lb_certificate_arn
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = "404"
+    }
+  }
+
+  //要チェック
+  lifecycle {
+    ignore_changes = [default_action]
+  }
+}
+
+//要チェック
+resource "aws_lb_listener_rule" "this" {
+  listener_arn = aws_lb_listener.this.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [action]
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
+resource "aws_ecs_service" "this" {
+  name            = local.name
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  //要チェック
+  health_check_grace_period_seconds = 360
+  propagate_tags                    = "SERVICE"
+  platform_version                  = "1.4.0"
+
+  deployment_controller {
+    type = "ECS"
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.this.arn
+    container_name   = local.name
+    container_port   = var.container_port
+  }
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_ids
+    assign_public_ip = false
+  }
+
+  depends_on = [aws_lb.this, aws_lb_target_group.this, aws_lb_listener.this, aws_lb_listener_rule.this]
+}
+
+
+//別ファイルにしない方が良い？
+resource "aws_route53_record" "this" {
+  zone_id = var.lb_host_zone
+  name    = var.lb_sub_domain
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_lb.this.dns_name]
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/ecs/${local.name}"
+  retention_in_days = 14 //こんなに要らない
 }
